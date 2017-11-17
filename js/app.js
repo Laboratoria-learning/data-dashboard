0e648c69c708dac2acfda69c4bba5291704cc4ed
kuveee <kamilavillablanca23@gmail.com>
2017-11-17 12:10:19 -0300

    subiendo graficos con funcionalidad

--- js/app.js
+++ js/app.js
@@ -9,3 +9,81 @@ function myFunction() {
 	var menu = document.getElementsByClassName("Mexico")[0];
 	menu.style.display = '';
 }
+
+function generacionI() {
+	var container= document.getElementById('container');
+	var parrafo=document.createElement('p');
+	var activas= '';
+	for (var i=0; i < data.CDMX['2017-1'].students.length; i++){
+		if (data.CDMX['2017-1'].students[i].active ===true){
+			activas++;
+		}
+	}
+	var estudiantesActivas= document.createTextNode('Alumnas presentes en CDMX generación 2017-1: ' + activas);
+	var totalEstudiantes= data.CDMX['2017-1'].students.length;
+	var decersion= document.createTextNode('Desertoras ' + (totalEstudiantes - activas));
+	parrafo.appendChild(decersion);
+	container.appendChild(parrafo);
+
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Alumnas', 'Estudian'],
+          ['Activas', activas],
+          ['Inactivas', (totalEstudiantes - activas)],
+
+        ]);
+
+        var options = {
+          title: 'Estudiantes activas e inactivas'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+
+
+}
+
+function generacionII() {
+	var container= document.getElementById('container');
+	var parrafo=document.createElement('p');
+	var activas= '';
+	for (var i=0; i < data.CDMX['2017-2'].students.length; i++){
+		if (data.CDMX['2017-2'].students[i].active ===true){
+			activas++;
+		}
+	}
+	var estudiantesActivas= document.createTextNode('Alumnas presentes en CDMX generación 2017-2: ' + activas);
+	var totalEstudiantes= data.CDMX['2017-2'].students.length;
+	var decersion= document.createTextNode('Desertoras ' + (totalEstudiantes - activas));
+	parrafo.appendChild(decersion);
+	container.appendChild(parrafo);
+
+
+	 google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart2);
+
+      function drawChart2() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Alumnas', 'Estudian'],
+          ['Activas', activas],
+          ['Inactivas', (totalEstudiantes - activas)],
+
+        ]);
+
+        var options = {
+          title: 'Estudiantes activas e inactivas'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+}
+
