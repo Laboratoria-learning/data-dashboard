2b8ad333d18ebcf5021912db7db34438fdcc1305
Natalia Cancino <nataliacancino@mbp-de-natalia.home>
2017-11-17 17:15:37 -0300

    Actualización Dashboard

--- js/app.js
+++ js/app.js
@@ -3,4 +3,12 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//console.log(data);
+
+function generation(sel){
+
+         
+		 var text= sel.options[sel.selectedIndex].text;
+		 document.getElementById("title").innerHTML = text;
+    }
+
