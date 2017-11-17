84e0509c47388f4603223acd04a214219d6cbd78
kuveee <kamilavillablanca23@gmail.com>
2017-11-17 09:45:30 -0300

    actualización mapa navegación

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+function myFunction() {
+	var menu = document.getElementsByClassName("Mexico")[0];
+	menu.style.display = '';
+}
