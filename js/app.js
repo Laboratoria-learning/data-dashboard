5386885ea8c5cd849c9e4d60d51f0a9649e5049b
lpalominosf <lorena.palominos@gmail.com>
2017-11-20 10:49:52 -0300

    subiendo cambios y avances en maqueta (menus)

--- js/app.js
+++ js/app.js
@@ -12,11 +12,13 @@ var job = document.getElementsByClassName("job");
 	function showMenu(){
 		var listMenu = this.getElementsByClassName("itemList")[0];
 		console.log(listMenu);
-		/*Al clickear muestra el menú*/
+		/*Al clickear muestra el menú (bajo el cargo del 
+		usuario)*/
 		if (listMenu.classList.contains("hide")){
 			listMenu.classList.remove("hide");
 			listMenu.classList.add("show");
-		/*Al clickear esconde el mené*/	
+		/*Al clickear esconde el menú (bajo el cargo del 
+		usuario)*/
 		} else {
 			listMenu.classList.remove("show");
 			listMenu.classList.add("hide");
@@ -24,12 +26,9 @@ var job = document.getElementsByClassName("job");
 		}
 	}
 
-    var menu = document.getElementsByClassName("fa-bars");
-	function showSeatsMenu() {
-		console.log("function");
-        menu.addEventListener("click", showSeatsMenu);
-    }
-
 }
+
+
+
 
 /*funcion con parametros para que se reemplace en cada for*/
