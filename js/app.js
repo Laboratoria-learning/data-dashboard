8386106f63e0debc185f42e136efdeebc0b391d3
brendamesias <brendamesiasvillar@gmail.com>
2017-11-27 18:29:47 -0500

    obteniendo total de estudiantes por sede y genreacion

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-/*
- * Funcionalidad de tu producto
- */
-
+window.addEventListener('load', function(event) {
+  console.log(event);
+  console.log(event.target);
+});
+//data.AQP["2016-2"].students.length
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
