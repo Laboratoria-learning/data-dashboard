f78c6218f1faeb4d1235fabc9d5e3973afbb2bad
adayc <adayc@hotmail.com>
2017-11-28 18:23:53 -0500

    Agregando opciones de sprint con js y eslint

--- js/app.js
+++ js/app.js
@@ -8,21 +8,18 @@ optionSpecialization = document.getElementById('specialization');
 // Puedes hacer uso de la base de datos a través de la variable `data`
 // console.log(data);
 window.addEventListener('load', function(event) {
-  //Ocultando especializacion de las estudiantes
+  // Ocultando especializacion de las estudiantes
   optionSpecialization.classList.add('hidden');
-  //Evento para elegir el Sprint
+  // Evento para elegir el Sprint
   optionSprint.addEventListener('change', function(event) {
-    if (optionSprint.selectedIndex === 5 ||optionSprint.selectedIndex === 6 ||optionSprint.selectedIndex === 7 ||optionSprint.selectedIndex === 8) {
+    if (optionSprint.selectedIndex === 5 || optionSprint.selectedIndex === 6 || optionSprint.selectedIndex === 7 || optionSprint.selectedIndex === 8) {
       optionSpecialization.classList.add('show');
       optionSpecialization.classList.remove('hidden');
       console.log(optionSprint.selectedIndex);
       console.log(optionSpecialization);
+    } else {
+      optionSpecialization.classList.add('hidden');
+      optionSpecialization.classList.remove('show');
     }
-    else{
-        optionSpecialization.classList.add('hidden');
-        optionSpecialization.classList.remove('show');
-    }
-  
-});
-
+  });
 });
