d4c448b480a264304d94ae5ad86881869436ad30
ThaliaDiaz <thalidiaz24@gmail.com>
2017-11-28 14:38:22 -0500

    calculando el porcentaje de dropout

--- js/app.js
+++ js/app.js
@@ -2,26 +2,32 @@
  * Funcionalidad de tu producto
  */
 console.log(data);
-console.log(Object.keys(Object.getOwnPropertyNames(data)[1]));
+//console.log(Object.keys(Object.getOwnPropertyNames(data)[1]));
 // console.log(data.AQP['2016-2']['students'][0]['active']);
-/*var studemts = data.AQP['2016-2']['students'];
-var studemtsActive = [];
-var studemtsDeserte = [];
-
-for (var i = 0; i <= studemts.length; i++) {
-  debugger;
-  if (studemts[i].active === true) {
-    studemtsActive.push(studemts[i].active);
-    console.log('activos estudiamtes' + studemtsActive);
-  } else {
-    studemtsDeserte.push(studemts[i].active);
-    console.log('desertores' + studemtsDeserte);
+//function desercion(){
+  var students = data.AQP['2016-2']['students'];
+  var studentsActive = [];
+  var studentsDeserte = [];
+  
+  for (var i = 0; i <= students.length-1; i++) {
+   
+    if (students[i].active === true) {
+      studentsActive.push(students[i].active);
+      console.log('activos estudiantes: ' + studentsActive);
+    } else {
+      studentsDeserte.push(students[i].active);
+      console.log('desertores: ' + studentsDeserte);
+    }
   }
-}
+  debugger;
+  var porcentajeDesercion = ((studentsDeserte.length * 100/ students.length));
+  console.log('STUDENTS CURRENTLY ENROLLMENT'+ '\t'+  studentsActive.length);
+  console.log('STUDENTS DROPOUT'+ '\t'+ studentsDeserte.length);
+  console.log('PERCENTAGE OF DESERTION'+ '\t' + porcentajeDesercion+'%');
+//}
 
-var porcentajeDesercion = (studemtsDeserte / studemts.length * 100);
-console.log(studemtsDeserte.length);
-console.log(porcentajeDesercion);*/
+//console.log(desercion());
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
 //
 
