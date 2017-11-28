ba7f5e67343d5d1340f3839d24c4cf62ee79da75
Maythe <maytheinfante96@gmail.com>
2017-11-28 17:46:46 -0500

    agregando variables estudiantes INACTIVAS(Desertoras)

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@ window.addEventListener('load', function(event) {
   console.log(event);
   console.log(event.target);
 });
-//data.AQP["2016-2"].students.length
+//  data.AQP["2016-2"].students.length
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+var students = data.AQP['2016-2'].students;
+
+// calculando el total de estudiantes ACTIVAS AQP
+var activeAqp = 0;
+for (i = 0; i < students.length; i++) {
+  if (students[i].active === false) {
+    var activeAqp = (activeAqp + 1);
+    console.log(activeAqp);
+  }
+};
+
+// calculando el total de estudiantes INACTIVAS AQP
+var inactiveAqp = students.length - activeAqp;
+console.log(inactiveAqp);
