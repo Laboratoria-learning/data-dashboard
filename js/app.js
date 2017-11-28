2f8b6404674906283e29f60f429d6b6ede55e68a
JessicaMP <jessica95.mp@gmail.com>
2017-11-28 11:35:11 -0500

    Estructurando el nav

--- js/app.js
+++ js/app.js
@@ -3,4 +3,10 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// console.log(data);
+var countries = document.getElementById('countries').children[0];
+
+countries.addEventListener('click', function(event) {
+  var list = document.getElementById('countries').children[1];
+  list.classList.toggle('hide');
+});
