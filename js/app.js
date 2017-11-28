40b3833c2e096ac5002e71533063a76ae7c501cb
AndreEG <andrea_206_93@hotmail.com>
2017-11-28 16:19:48 -0500

    añade estructura

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
-/*
- * Funcionalidad de tu producto
- */
+/* guarda en un array las sedes */
+var sede = Object.keys(data);
+console.log(sede);
+/* guarda en un array los valores de cada sede es decir las generaciones */
+var generation = Object.values(data);
+console.log(generation);
+
+var lima = sede[2];
+var otro = data[lima]['2016-2'];
+
+window.addEventListener('load', function() {
+  var selectSede = document.getElementById('sede');
+  var selectGeneration = document.getElementById('generation');
+  selectSede.addEventListener('change', function() {
+    switch (true) {
+    case event.target.value === 'lima':
+      
+    };
+  });
+});
+
+function showGeneration(sede, generacion) {
+
+}
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// console.log(data);
