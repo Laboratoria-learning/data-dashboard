9445fddc5463ab3b336f9aabb320c7e543415942
Kamaly Zapana Lorenzo <32285958+kamalyzl@users.noreply.github.com>
2017-11-28 11:44:15 -0500

    Añadiendo imagenes a la seccion

--- js/app.js
+++ js/app.js
@@ -6,12 +6,30 @@
 console.log(data);
 
 
-var select = document.getElementById('select');
-var alumn = document.getElementById('alumn');
+window.onload('load', function() {
+  var select = document.getElementById('select');
+  var alumn = document.getElementById('alumn');
+  
+  select.addEventListener('click', function() {
+    var image = document.createElement('img');
+    alumn.appendChild(image);
+    debugger;
+    image.src = data.LIM['2016-2']['students'][0]['photo'];
+  });
 
-select.addEventListener('click', function() {
-  var image = document.createElement('img');
-  alumn.appendChild(image);
-  debugger;
-  image.src = data.LIM['2016-2']['students'][0]['photo'];
+
+selec.addEventListener('click', function(){
+
+
+ 
+
+});
+
+
+
+
+
+
+
+
 });
