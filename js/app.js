2d3dc19a528005a990ab1eb4304e4f26d8155529
ThaliaDiaz <thalidiaz24@gmail.com>
2017-11-28 02:30:54 -0500

    obteniendo el total de desertores

--- js/app.js
+++ js/app.js
@@ -2,5 +2,26 @@
  * Funcionalidad de tu producto
  */
 
+// console.log(data.AQP['2016-2']['students'][0]['active']);
+var studemts = data.AQP['2016-2']['students'];
+var studemtsActive = [];
+var studemtsDeserte = [];
+
+for (var i = 0; i <= studemts.length; i++) {
+  debugger;
+  if (studemts[i].active === true) {
+    studemtsActive.push(studemts[i].active);
+    console.log('activos estudiamtes' + studemtsActive);
+  } else {
+    studemtsDeserte.push(studemts[i].active);
+    console.log('desertores' + studemtsDeserte);
+  }
+}
+
+console.log(studemtsDeserte.length);
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// console.log(data);
+
+// console.log(Object.keys(data)[0]);
+
+
