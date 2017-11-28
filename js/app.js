a39e02a92df1d6a97766acc5fc188d66fe6f6c43
LizbethFP <lizbeth.fp@gmail.com>
2017-11-28 12:52:21 -0500

    agregando desplegable menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-/*
- * Funcionalidad de tu producto
- */
+/* Dropdwon de menu*/
+var submenu = document.getElementsByClassName('country');
+for (var i = 0; i < submenu.length; i++) {
+  submenu[i].addEventListener('click', showMenu);
+}
+
+function showMenu() {
+  var listMenu = this.getElementsByClassName('item-list')[0];
+  if (listMenu.classList.contains('hide')) {
+    listMenu.classList.remove('hide');
+    listMenu.classList.add('show');
+  } else {
+    listMenu.classList.remove('show');
+    listMenu.classList.add('hide');
+  }
+}
+
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
