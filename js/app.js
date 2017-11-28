802a134b7a562b96f10718017f81e01411856aa5
Yelitza3125 <deyel_99@hotmail.com>
2017-11-28 00:54:02 -0500

    Estructura, estilo y función del modal sedes

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+window.addEventListener('load', function() {
+    
+  arrow.addEventListener('click', function showSedes(event) {
+    var arrow = document.getElementById('arrow');  
+    var ventana = document.getElementById('sedes-container');
+    ventana.style.marginTop = '170px';
+    ventana.style.marginLeft = '170px';
+    // ventana.style.left = ((document.body.clientWidth - 350) / 2) + 'px';
+    ventana.style.display = 'block';
+
+  });
+
+
+});
+
+/* var ventana = document.getElementById(‘miVentana’);
+    ventana.style.marginTop = “100px”;
+    ventana.style.left = ((document.body.clientWidth-350) / 2) +  “px”;
+    ventana.style.display = ‘block’; */
