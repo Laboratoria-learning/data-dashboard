ccef9da446c86e1dbcbb9969eb82bf0e61637560
brendamesias <brendamesiasvillar@gmail.com>
2017-11-28 08:45:11 -0500

    estructura báscia html y creción d eliestas deplegables de sedes y generaciones

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 window.addEventListener('load', function(event) {
   console.log(event);
   console.log(event.target);
+
+  var sede_generation = document.getElementById('sede_generation');
+  console.log(sede_generation)
+
+
+  sede_generation.addEventListener('click', function(event) {
+
+    /*if (characters.value) {
+      var tweet = document.createElement('p');
+      var textTweet = document.createTextNode('');
+      textTweet.textContent = characters.value;
+      tweet.appendChild(textTweet);
+      tweetsPublicados.appendChild(tweet);
+      tweetsPublicados.insertBefore(tweet, tweetsPublicados.firstElementChild);
+      characters.value = '';
+      */
+
+  });
+
 });
-//data.AQP["2016-2"].students.length
+// data.AQP["2016-2"].students.length
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
