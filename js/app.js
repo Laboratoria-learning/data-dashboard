bc6adbd0f1a069c42994243a6d33f389d00c077e
betsyvies <ing.betsyvies@gmail.com>
2017-11-28 09:27:51 -0500

    añadiendo boton sedes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
-/*
- * Funcionalidad de tu producto
- */
+/* Iniciamos extrayendo los datos de las sedes */
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+window.addEventListener('load', function() {
+  var containerSedes = document.getElementById('container-sedes');
+  containerSedes.addEventListener('click', function() {
+    var containerGenerations = document.getElementsByClassName('container-generations')[0];
+    if (containerGenerations.classList.contains('hide')) {
+      containerGenerations.classList.remove('hide');
+      containerGenerations.classList.add('show');
+    } else {
+      containerGenerations.classList.add('show');
+      
+      for (var i = 0; i < Object.keys(data).length; i++) {
+        var containerList = document.createElement('ul');
+        var list = document.createElement('li');
+        var text = document.createTextNode('local ' + Object.keys(data)[i]);
+        console.log(text);
+        list.appendChild(text);
+        containerList.appendChild(list);
+        containerGenerations.appendChild(containerList);
+        // console.log(data[Object.keys(data)[i]]);  
+      } 
+    }  
+  });
+});
