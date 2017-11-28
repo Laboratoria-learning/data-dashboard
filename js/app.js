84508e1b73db1bec0b5f7f3d3e3323495cded80d
lucerohospina <lucerojhp@gmail.com>
2017-11-28 17:22:49 -0500

    haciendo cambios en index y js

--- js/app.js
+++ js/app.js
@@ -18,8 +18,15 @@ window.addEventListener('load', function() {
   var promoS120162 = data.AQP['2016-2'].ratings[0].nps.promoters;
   var detracS120162 = data.AQP['2016-2'].ratings[0].nps.detractors;
 
+  var parag = document.createElement("p");
   var text = document.createTextNode(promoS120162);
-  box2.appendChild(text);
+  parag.appendChild(text);
+  box2.appendChild(parag);
+
+  var parag2 = document.createElement("p");
+  var text2 = document.createTextNode(detracS120162);
+  parag2.appendChild(text2);
+  box2.appendChild(parag2);
 });
 
 
