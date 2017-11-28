3927ef6806c1cd0688168ea7e407e65f5d70e787
Yelitza3125 <deyel_99@hotmail.com>
2017-11-28 14:08:57 -0500

    función showSedes, modificada con toggle

--- js/app.js
+++ js/app.js
@@ -5,17 +5,13 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
 
-window.addEventListener('load', function() {
-    
+window.addEventListener('load', function() {  
   arrow.addEventListener('click', function showSedes(event) {
     var arrow = document.getElementById('arrow');  
-    var ventana = document.getElementById('sedes-container');
-    ventana.style.marginTop = '170px';
-    ventana.style.marginLeft = '170px';
+    var sedes = document.getElementById('sedes-container');
+    sedes.classList.toggle('hidden');
+    sedes.style.marginTop = '170px';
+    sedes.style.marginLeft = '170px';
     // ventana.style.left = ((document.body.clientWidth - 350) / 2) + 'px';
-    ventana.style.display = 'block';
-
   });
-
-
 });
