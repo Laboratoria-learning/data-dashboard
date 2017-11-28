782305a80698cd5555e96b6f4dfb192d9e729358
Rocciescobar <roccisen.88@gmail.com>
2017-11-28 17:23:25 -0500

    borrador en app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,18 @@
-/*
- * Funcionalidad de tu producto
- */
 
-window.addEventListener("load", function(){
-    var openAnimatedMenu = document.getElementById("open-animated-menu");
-    var closeAnimatedMenu = document.getElementById("close-animated-menu");
-    
-     openAnimatedMenu.addEventListener("click", function(){
-        document.getElementById("animated-menu").style.width = "250px";
-     })
+window.addEventListener('load', function() {
+  var openAnimatedMenu = document.getElementById('open-animated-menu');
+  var closeAnimatedMenu = document.getElementById('close-animated-menu');
 
-     closeAnimatedMenu.addEventListener("click", function(){
-        document.getElementById("animated-menu").style.width = "0";
-     })
-})
+  openAnimatedMenu.addEventListener('click', function() {
+    document.getElementById('animated-menu').style.width = '250px';
+  });
 
- 
-
+  closeAnimatedMenu.addEventListener('click', function() {
+    document.getElementById('animated-menu').style.width = '0';
+  });
+});
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+
