a73aa86ef67664218b83786f806184c00b72d8c6
adrianarojas29 <adriana_29-98@hotmail.com>
2017-11-28 00:21:04 -0500

    subiendo la primera parte de la estructura

--- js/app.js
+++ js/app.js
@@ -3,4 +3,21 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+
+var boton = document.getElementById("btn");
+
+function showMenu() {
+    var menu = document.getElementById("menu-princ");
+
+    if(menu.classList.contains("disabled-menu")){
+        menu.classList.remove("disabled-menu");
+        menu.classList.add("menu");
+    }
+    else{
+        menu.classList.remove("menu");
+        menu.classList.add("disabled-menu");
+
+    }
+}
+
+boton.addEventListener("click", showMenu);
