2c91fd9a1fafae441b6a57fc27e1af7d37728611
betsyvies <ing.betsyvies@gmail.com>
2017-11-28 11:00:21 -0500

    añadiendo generaciones

--- js/app.js
+++ js/app.js
@@ -3,23 +3,39 @@
 window.addEventListener('load', function() {
   var containerSedes = document.getElementById('container-sedes');
   containerSedes.addEventListener('click', function() {
-    var containerGenerations = document.getElementsByClassName('container-generations')[0];
-    if (containerGenerations.classList.contains('hide')) {
-      containerGenerations.classList.remove('hide');
-      containerGenerations.classList.add('show');
+    var sedes = document.getElementsByClassName('container-generations')[0];
+    if (sedes.classList.contains('hide')) {
+      sedes.classList.remove('hide');
+      sedes.classList.add('show');
     } else {
-      containerGenerations.classList.add('show');
+      sedes.classList.add('show');
       
       for (var i = 0; i < Object.keys(data).length; i++) {
         var containerList = document.createElement('ul');
         var list = document.createElement('li');
         var text = document.createTextNode('local ' + Object.keys(data)[i]);
-        console.log(text);
+
         list.appendChild(text);
         containerList.appendChild(list);
-        containerGenerations.appendChild(containerList);
-        // console.log(data[Object.keys(data)[i]]);  
-      } 
+        sedes.appendChild(containerList);
+
+        text.addEventListener('click', function() {
+          var containerGenerations = document.getElementsByClassName('container-generations')[0];
+          for (var i = 0; i < Object.keys(data).length; i++) {
+            for (var a = 0; a < Object.keys(data[Object.keys(data)[i]]).length; a++) {
+              var containerListGenerations = document.createElement('ul');
+              var listGenerations = document.createElement('li');
+              var textGenerations = document.createTextNode('generacion ' + Object.keys(data[Object.keys(data)[i]])[a]);
+
+              listGenerations.appendChild(textGenerations);
+              containerListGenerations.appendChild(listGenerations);
+              containerGenerations.appendChild(containerListGenerations);
+              // console.log(data[Object.keys(data)[i]]);  
+            } 
+          }
+        }); 
+      }
     }  
   });
 });
+
