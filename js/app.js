48de400b65f9678ff1f4fc3b8634f7fd472e3a8d
ThaliaDiaz <thalidiaz24@gmail.com>
2017-11-28 15:05:50 -0500

    calculando el porcentaje de cualquier sede y cualquier promoción

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-/*
- * Funcionalidad de tu producto
- */
 console.log(data);
 //console.log(Object.keys(Object.getOwnPropertyNames(data)[1]));
 // console.log(data.AQP['2016-2']['students'][0]['active']);
-//function desercion(){
-  var students = data.AQP['2016-2']['students'];
+
+/* DATOS QUE MUESTRA EL ENROLLMENT */ 
+function desercion(sede, generacion){
+  var students = data[sede][generacion]['students'];
   var studentsActive = [];
   var studentsDeserte = [];
   
@@ -13,24 +12,17 @@ console.log(data);
    
     if (students[i].active === true) {
       studentsActive.push(students[i].active);
-      console.log('activos estudiantes: ' + studentsActive);
     } else {
       studentsDeserte.push(students[i].active);
-      console.log('desertores: ' + studentsDeserte);
     }
   }
-  debugger;
+ 
   var porcentajeDesercion = ((studentsDeserte.length * 100/ students.length));
   console.log('STUDENTS CURRENTLY ENROLLMENT'+ '\t'+  studentsActive.length);
   console.log('STUDENTS DROPOUT'+ '\t'+ studentsDeserte.length);
   console.log('PERCENTAGE OF DESERTION'+ '\t' + porcentajeDesercion+'%');
-//}
+}
 
-//console.log(desercion());
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
-//
-
-// console.log(Object.keys(data)[0]);
+console.log(desercion('LIM', '2017-2'));
 
 
