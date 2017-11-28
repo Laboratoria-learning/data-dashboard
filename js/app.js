39e692b07f94f3e7ec588e7b686b0e2ae9d92954
AndreEG <andrea_206_93@hotmail.com>
2017-11-28 18:07:57 -0500

    añade evento change

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-/* guarda en un array las sedes */
-var sede = Object.keys(data);
+// guarda en un array las sedes
+var sede = Object.keys(data); // ["AQP", "CDMX", "LIM", "SCL"]
 console.log(sede);
-/* guarda en un array los valores de cada sede es decir las generaciones */
-var generation = Object.values(data);
+// guarda en un array los objetos de cada sede es decir las generaciones
+var generation = Object.values(data); // [{…}, {…}, {…}, {…}] tiene el valor de AQP, CDMX, LIM, SCL
 console.log(generation);
+// guarda las generaciones en un array
+var generationAarequipa = Object.keys(generation[0]); // ["2016-2", "2017-1"]
+console.log(generationAarequipa);
+// guarda en un array los objetos de las generaciones que guardan students y ratings
+var genArequipaObject = Object.values(generation[0]); // [{…}, {…}] tiene el valor 2016-2, 2017-1
+console.log(genArequipaObject);
 
-var lima = sede[2];
-var otro = data[lima]['2016-2'];
+
+var otro = data[sede[2]]['2016-2'];
+// console.log(otro);
 
 window.addEventListener('load', function() {
   var selectSede = document.getElementById('sede');
@@ -14,7 +21,18 @@ window.addEventListener('load', function() {
   selectSede.addEventListener('change', function() {
     switch (true) {
     case event.target.value === 'lima':
-      
+      console.log(data[sede[2]]);
+      // deberia tener aca alguna funcion que imprima todos los datos de lima
+      break;
+    case event.target.value === 'arequipa':
+      console.log(data[sede[0]]);
+      break;
+    case event.target.value === 'chile':
+      console.log(data[sede[3]]);
+      break;
+    case event.target.value === 'mexico':
+      console.log(data[sede[1]]);
+      break;
     };
   });
 });
