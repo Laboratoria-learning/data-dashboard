533dc2b0bb034d69cfe63e1ea6d115c557fea4ac
brendamesias <brendamesiasvillar@gmail.com>
2017-11-28 14:52:51 -0500

    creando funcion showListSedes

--- js/app.js
+++ js/app.js
@@ -1,24 +1,17 @@
-// variable que almacena  sede y generacion
-var sedeGeneration = document.getElementById('sede_generation').textContent;
-var listSedes = document.getElementById('list_sedes');
+// variable que almacena  el texto junto de sede y generacion
+var sedeGeneration = document.getElementById('sede_generation');
+// variable que almacena la lista de sedes
+var listSedes = document.getElementById('list_sedes').childElements;
 var arequipaGenerations = document.getElementById('arequipa_generations');
 var mexicoGenerations = document.getElementById('mexico_generations');
 var limaGenerations = document.getElementById('lima_generations');
 var chileGenerations = document.getElementById('chile_generations');
 
-/*
-sedeGeneration.addEventListener('click', function(event) {
-   if (characters.value) {
-      var tweet = document.createElement('p');
-      var textTweet = document.createTextNode('');
-      textTweet.textContent = characters.value;
-      tweet.appendChild(textTweet);
-      tweetsPublicados.appendChild(tweet);
-      tweetsPublicados.insertBefore(tweet, tweetsPublicados.firstElementChild);
-      characters.value = '';
 
-});
-*/
+sedeGeneration.addEventListener('click', showListSedes);
+function showListSedes(event) {
+
+};
 
 
 // data.AQP["2016-2"].students.length --> me da el total de estudiantes de arequipa 2016-2
