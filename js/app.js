eec56e04c72c354f20a63c10b7a36293ba7cc52c
ThaliaDiaz <thalidiaz24@gmail.com>
2017-11-28 16:45:35 -0500

    agregando porcentaje de estudiantes que lograron el objetivo

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 console.log(data);
-//console.log(Object.keys(Object.getOwnPropertyNames(data)[1]));
+// console.log(Object.keys(Object.getOwnPropertyNames(data)[1]));
 // console.log(data.AQP['2016-2']['students'][0]['active']);
 
 /* DATOS QUE MUESTRA EL ENROLLMENT */ 
-function desercion(sede, generacion){
+function desercion(sede, generacion) {
   var students = data[sede][generacion]['students'];
   var studentsActive = [];
   var studentsDeserte = [];
   
-  for (var i = 0; i <= students.length-1; i++) {
-   
+  for (var i = 0; i <= students.length - 1; i++) {
     if (students[i].active === true) {
       studentsActive.push(students[i].active);
     } else {
@@ -17,12 +16,39 @@ function desercion(sede, generacion){
     }
   }
  
-  var porcentajeDesercion = ((studentsDeserte.length * 100/ students.length));
-  console.log('STUDENTS CURRENTLY ENROLLMENT'+ '\t'+  studentsActive.length);
-  console.log('STUDENTS DROPOUT'+ '\t'+ studentsDeserte.length);
-  console.log('PERCENTAGE OF DESERTION'+ '\t' + porcentajeDesercion+'%');
+  var porcentajeDesercion = ((studentsDeserte.length * 100 / students.length));
+  console.log('STUDENTS CURRENTLY ENROLLMENT' + '\t' + studentsActive.length);
+  console.log('STUDENTS DROPOUT' + '\t' + studentsDeserte.length);
+  console.log('PERCENTAGE OF DESERTION' + '\t' + porcentajeDesercion + '%');
 }
 
 console.log(desercion('LIM', '2017-2'));
 
+/* DATOS ACHIEVEMENT*/ 
 
+function logroMetas(sede, generacion, sprint) {
+  // debugger;
+  var students = data[sede][generacion]['students'];
+  // var sprint = data[sede][generacion]['students']['sprints']['number'];
+  var studentsOverTarget = [];
+  var studentsDownTarget = [];
+
+  for (var i = 0; i < students.length; i++) {
+    for (var j = 0; j < students[i].sprints.length; j++) {
+      if (students[i].sprints[j].score['tech'] >= 1260 && students[i].sprints[j].score['hse'] >= 840) {
+        studentsOverTarget.push(students[i].sprints[j]);
+      } else {
+        studentsDownTarget.push(students[i].sprints[j]);
+      }
+    }
+  }
+  
+  var porcentajeLogroMetas = (studentsOverTarget.length * 100) / (studentsOverTarget.length + studentsDownTarget.length);
+  console.log('pasaron la meta: ' + '\t' + studentsOverTarget.length);
+  console.log('no llegaron a la meta: ' + '\t' + studentsDownTarget.length);
+  console.log('porcentaje que logro la meta:' + '\t' + porcentajeLogroMetas);
+}
+
+console.log(logroMetas('LIM', '2017-1', 1));
+
+/* */ 
