83458ea51c4e11e4bcd1409906060de194173376
brendamesias <brendamesiasvillar@gmail.com>
2017-11-28 14:32:23 -0500

    agregando nuevas variables

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
-window.addEventListener('load', function(event) {
-  console.log(event);
-  console.log(event.target);
+// variable que almacena  sede y generacion
+var sedeGeneration = document.getElementById('sede_generation').textContent;
+var listSedes = document.getElementById('list_sedes');
+var arequipaGenerations = document.getElementById('arequipa_generations');
+var mexicoGenerations = document.getElementById('mexico_generations');
+var limaGenerations = document.getElementById('lima_generations');
+var chileGenerations = document.getElementById('chile_generations');
 
-  var sedeGeneration = document.getElementById('sede_generation');
-  var listSedes = document.getElementById('list_sedes');
-  var arequipaGenerations = document.getElementById('arequipa_generations');
-  var mexicoGenerations = document.getElementById('mexico_generations');
-  var limaGenerations = document.getElementById('lima_generations');
-  var chileGenerations = document.getElementById('chile_generations');
-
-  sedeGeneration.addEventListener('click', function(event) {
-  /* if (characters.value) {
+/*
+sedeGeneration.addEventListener('click', function(event) {
+   if (characters.value) {
       var tweet = document.createElement('p');
       var textTweet = document.createTextNode('');
       textTweet.textContent = characters.value;
@@ -18,9 +16,10 @@ window.addEventListener('load', function(event) {
       tweetsPublicados.appendChild(tweet);
       tweetsPublicados.insertBefore(tweet, tweetsPublicados.firstElementChild);
       characters.value = '';
-      */
-  });
+
 });
-// data.AQP["2016-2"].students.length
-// Puedes hacer uso de la base de datos a través de la variable `data`
+*/
+
+
+// data.AQP["2016-2"].students.length --> me da el total de estudiantes de arequipa 2016-2
 console.log(data);
