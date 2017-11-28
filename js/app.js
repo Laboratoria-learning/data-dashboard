ea683a4260aa22348e963e5d79b13d2b3dea274b
SIlviaTivi <skfl.erg@hotmail.com>
2017-11-28 12:38:39 -0500

    data total de estudiantes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
-/*
- * Funcionalidad de tu producto
- */
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+// total de estudiantes presentes por sede y generacion
+
+var studentsLIM20162 = data.LIM['2016-2'].students;
+var studentsLIM20171 = data.LIM['2017-1'].students;
+var studentsLIM20172 = data.LIM['2017-2'].students;
+
+// por generacion:
+
+var numEstudentsLIM20162 = data.LIM['2016-2'].students.length + 1;
+console.log(numEstudentsLIM20162);
+var numEstudentsLIM20171 = data.LIM['2017-1'].students.length + 1;
+console.log(numEstudentsLIM20171);
+var numEstudentsLIM20172 = data.LIM['2017-2'].students.length + 1;
+console.log(numEstudentsLIM20172);
+
+// por sede:
+
+var numberEstudentsSeatLIM = [numEstudentsLIM20162, numEstudentsLIM20171, numEstudentsLIM20172];
+sumEstudentsLIM = 0;
+for (var i = 0; i < numberEstudentsSeat.length; i++) {
+  sumEstudentsLIM = sumEstudentsLIM + numberEstudentsSeatLIM[i];
+};
+console.log(sumEstudentsLIM);// total de alumnas sede LIM
