9afc49dc7b2c407a6c66ef9c6c24d4210084dbc4
lucerohospina <lucerojhp@gmail.com>
2017-11-28 16:27:28 -0500

    agregando DIVS para achievement y net promoter score

--- js/app.js
+++ js/app.js
@@ -2,21 +2,27 @@
  * Funcionalidad de tu producto
  */
 
-window.addEventListener("load", function(){
-    var openAnimatedMenu = document.getElementById("open-animated-menu");
-    var closeAnimatedMenu = document.getElementById("close-animated-menu");
+window.addEventListener('load', function() {
+  var openAnimatedMenu = document.getElementById('open-animated-menu');
+  var closeAnimatedMenu = document.getElementById('close-animated-menu');
     
-     openAnimatedMenu.addEventListener("click", function(){
-        document.getElementById("animated-menu").style.width = "250px";
-     })
+  openAnimatedMenu.addEventListener('click', function() {
+    document.getElementById('animated-menu').style.width = '250px';
+  });
 
-     closeAnimatedMenu.addEventListener("click", function(){
-        document.getElementById("animated-menu").style.width = "0";
-     })
-})
+  closeAnimatedMenu.addEventListener('click', function() {
+    document.getElementById('animated-menu').style.width = '0';
+  });
 
- 
+  var box2 = document.getElementById('aqp-nps-sprint1');
+  var promoS120162 = data.AQP['2016-2'].ratings[0].nps.promoters;
+  var detracS120162 = data.AQP['2016-2'].ratings[0].nps.detractors;
+
+  var text = document.createTextNode(promoS120162);
+  box2.appendChild(text);
+});
 
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
