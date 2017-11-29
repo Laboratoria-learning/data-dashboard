3598eeb6bc0da55057c3227aa21aa76a9828d520
pameam <pamela.anayhuaman@gmail.com>
2017-11-29 02:47:17 -0500

    añadiendo nuevos estilos al body

--- js/app.js
+++ js/app.js
@@ -4,59 +4,47 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 
-var boton = document.getElementById("btn");
+var boton = document.getElementById('btn');
 
 function showMenu() {
-    var menu = document.getElementById("menu-princ");
+  var menu = document.getElementById('menu-princ');
 
-    if(menu.classList.contains("disabled-menu")){
-        menu.classList.remove("disabled-menu");
-        menu.classList.add("menu");
-    }
-    else{
-        menu.classList.remove("menu");
-        menu.classList.add("disabled-menu");
-
-    }
+  if (menu.classList.contains('disabled-menu')) {
+    menu.classList.remove('disabled-menu');
+    menu.classList.add('menu');
+  } else {
+    menu.classList.remove('menu');
+    menu.classList.add('disabled-menu');
+  }
 }
 
-boton.addEventListener("click", showMenu);
+boton.addEventListener('click', showMenu);
 
-var mostrarOcultar = function(e){
-    var tabSeleccionado = e.target.dataset.tabSeleccionado;
-    var OVERVIEW = document.getElementById('OVERVIEW');
-    var STUDENTS = document.getElementById('STUDENTS');
-    var TEACHERS = document.getElementById('TEACHERS');
+var mostrarOcultar = function(event) {
+  var tabSeleccionado = event.target.dataset.tabSeleccionado;
+  var OVERVIEW = document.getElementById('OVERVIEW');
+  var STUDENTS = document.getElementById('STUDENTS');
+  var TEACHERS = document.getElementById('TEACHERS');
 
-    if(tabSeleccionado === 'OVERVIEW'){
-        STUDENTS.style.display = 'none';
-        TEACHERS.style.display = 'none';
-        OVERVIEW.style.display = 'block';
-
-    }else if(tabSeleccionado === 'STUDENTS'){
-        OVERVIEW.style.display = 'none';
-        STUDENTS.style.display = 'block';
-        TEACHERS.style.display = 'none';
-
-    }else if(tabSeleccionado === 'TEACHERS'){
-        OVERVIEW.style.display = 'none';
-        STUDENTS.style.display = 'none';
-        TEACHERS.style.display = 'block';
-    }
-}
-var cargarPagina = function(){
-    var OVERVIEW = document.getElementById('OVERVIEW');
-    var STUDENTS = document.getElementById('STUDENTS');
-    var TEACHERS = document.getElementById('TEACHERS');
-
+  if (tabSeleccionado === 'OVERVIEW') {
     STUDENTS.style.display = 'none';
     TEACHERS.style.display = 'none';
     OVERVIEW.style.display = 'block';
-
-    var elementosTab = document.getElementsByClassName("tab");
-    for(var i = 0; i < elementosTab.length;i++){
-        elementosTab[i].addEventListener("click",mostrarOcultar);
-    }
-}
+  } else if (tabSeleccionado === 'STUDENTS') {
+    OVERVIEW.style.display = 'none';
+    STUDENTS.style.display = 'block';
+    TEACHERS.style.display = 'none';
+  } else if (tabSeleccionado === 'TEACHERS') {
+    OVERVIEW.style.display = 'none';
+    STUDENTS.style.display = 'none';
+    TEACHERS.style.display = 'block';
+  }
+};
+var cargarPagina = function() {
+  var elementosTab = document.getElementsByClassName('tab');
+  for (var i = 0; i < elementosTab.length;i++) {
+    elementosTab[i].addEventListener('click', mostrarOcultar);
+  }
+};
 
 cargarPagina();
