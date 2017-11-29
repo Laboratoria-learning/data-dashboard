573498e37df063b097e4552c995c9f3c4dabc949
AraceliCumen <aycuevam@gmail.com>
2017-11-29 09:40:56 -0500

    Actualizando js

--- js/app.js
+++ js/app.js
@@ -7,27 +7,30 @@
 // Almacenamos el nombre de las sedes dado en el archivo data.js; que nos da un array:
 console.log(data);
 // guardamos los nombres de las sedes
-var nombreofSede = Object.keys(data);
+var dataNameSedes = Object.keys(data);
+
 // guardamos los valores las sedes
 var valuesofData = Object.values(data);
+// console.log(valuesofData);
 
 // Nos aseguramos que la página cargue correctamente:
 window.addEventListener('load', function() {
   // Creamos una variable que va a guardar la lista desplegable
   var selectionSede = document.getElementById('select-sede');
-
   // Recorremos el array de nombre de las sedes que traemos del archivo data.js
-  for (var i = 0; i < nombreofSede.length; i++) {
+  for (var i = 0; i < dataNameSedes.length; i++) {
     // Creamos una variable sedes que va almacenar la grupo de la sede
     var optgroupsedes = document.createElement('optgroup');
     // le asignamos el valor del labol del grupo, de acuerdo al nombre extraido de data.js de cada sede
-    optgroupsedes.label = nombreofSede[i];
+    optgroupsedes.label = dataNameSedes[i];
     // le asignamos cada sede a la lista de sedes
     selectionSede.appendChild(optgroupsedes);
     // guardamos las promociones por sede
     var promsforSede = Object.keys(valuesofData[i]);
+    // console.log(promsforSede);
     // guardamos los valores de los promociones
-    var valuesofSedes = Object.values(valuesofData[i]);
+    var valuesofPromsforSede = Object.values(valuesofData[i]);
+    // console.log(valuesofPromsforSede);
 
     // Recorremos las promociones
     for (var j = 0; j < promsforSede.length; j++) {
@@ -36,23 +39,38 @@ window.addEventListener('load', function() {
       // le asignamos el valor a mostrar
       optionproms.label = promsforSede[j];
       // le asignamos donde lo va a mostrar
-      optgroupsedes.appendChild(optionproms);
-      // recorremos los valores de las sedes
-      for (var l = 0; l < valuesofSedes.length; l++) {
-        // obtenemos el valor de las sedes de la posicion l
-        var valuesofProms = valuesofSedes[l];
-        // obtenemos los keys de las estudiantes
-        var keysforSttudents = Object.keys(valuesofProms);
-        // obtenemos los valores de los keys de las estudiantes
-        var valueskeysforStudents = Object.values(valuesofProms);
-        console.log(valueskeysforStudents);
+      optgroupsedes.appendChild(optionproms); 
 
-        // recorremos los keys de los estudiantes
-        // for (var k = 0; k < keysforSttudents.length; k++){
-        //   var valuesofStudents = valuesofProms[k];
-
-        // }
-      }
+      var nameSede = (searchInArray(dataNameSedes, dataNameSedes[i]));
+      var nameProm = searchInArray(promsforSede, promsforSede[j]);
+    
     }
   }
+  
+  
+
+
 });
+
+// para buscar en un array
+function searchInArray(array, string) {
+  var result;
+  for (var m = 0; m < array.length; m++) {
+    if (string === array[m]) {
+      result = string;
+    }
+  }
+  return result;
+}
+
+// para buscar una key en un objeto
+function searchInObject(obj, string) {
+  var result;
+  var keys = Object.keys(obj);
+  for (var s = 0; s < keys.length; s++) {
+    if (string === keys[s]) {
+      result = string;
+    }
+  }
+  return result;
+}
