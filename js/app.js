76661054c42ca60ef699ba49ede4f15177f98e1f
ZulemaAB <malezu06@hotmail.com>
2017-11-29 17:28:39 -0500

    agregando función student

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
   var tabs = document.querySelectorAll('.tab');
   var contenidos = document.querySelectorAll('.inform');
-  
+
   function toggleTab(elem) {
     for (var i = 0; i < elem.length; i++) {
       elem[i].addEventListener('click', showContainers);
@@ -19,6 +19,40 @@ window.addEventListener('load', function() {
       /* this.classList.add('red');*/
     }
   }
+// -creando funcion para el total de estudiantes activos y desactivos-porcentaje de total de desertores
+  function students(sede, generation, students) {
+    var studentAct = 0;
+    var studentInac = 0;
+    var total = 0;
+    var Student = data[sede][generation][students];
+    for (var i = 0; i < Student.length; i++) {
+      if (Student[i].active) {
+        studentAct = studentAct + 1;
+      } else if (Student[i].active === false) {
+        studentInac = studentInac + 1;
+      }
+
+      total = studentAct + studentInac;
+    }
+
+    var porcentaje = Math.round((studentInac * 100) / total);
+
+    console.log('hay ' + total);
+    console.log(' Las alumnas activas son: ' + studentAct);
+    console.log(' las alumnas retiradas son: ' + studentInac);
+    console.log('Las alumnas retiratas representan el ' + porcentaje + ' %');
+  }
+
+  console.log(students('AQP', '2016-2', 'students'));
+
+  var box1 = document.getElementById('box1');
+  var total = document.createElement('h2');
+  total.innerText = dropoutAll('LIM', '2016-2');
+Añadir comentario Contraer
+
+
+
+
 
   toggleTab(tabs);
 });
