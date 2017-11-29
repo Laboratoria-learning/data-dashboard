48b5aa0e00b0a71083053d98a0840d72011316e2
brendamesias <brendamesiasvillar@gmail.com>
2017-11-29 16:31:21 -0500

    creando funciones que me permitan hallar la cantidad de estudiantes de cualquier sede y generación

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ console.log(objSedes);
 var allSedes = document.querySelectorAll('.sede');
 console.log(allSedes);
 
+// variables que guarda elemtnos que tiene en comun que son generaciones de la misma sede
+var allGenerationAQP = document.querySelectorAll('.generationAQP');
+var allGenerationCDMX = document.querySelectorAll('.generationCDMX');
+var allGenerationLIM = document.querySelectorAll('.generationLIM');
+var allGenerationSCL = document.querySelectorAll('.generationSCL');
+
 // sedes y prefijos
 var arequipa = 'AQP' ;
 var mexico = 'CDMX' ;
@@ -27,27 +33,141 @@ var sede1 = document.getElementById('sede_1');
 var sede2 = document.getElementById('sede_2');
 var sede3 = document.getElementById('sede_3');
 
+// variables que almacenan cada generacion del html en el dom
+var codeGenerationAQP0 = document.getElementById('code_generation_aqp_0');
+var codeGenerationAQP1 = document.getElementById('code_generation_aqp_1');
+var codeGenerationCDMX0 = document.getElementById('code_generation_cdmx_0');
+var codeGenerationCDMX1 = document.getElementById('code_generation_cdmx_1');
+var codeGenerationLIM0 = document.getElementById('code_generation_lim_0');
+var codeGenerationLIM1 = document.getElementById('code_generation_lim_1');
+var codeGenerationLIM2 = document.getElementById('code_generation_lim_2');
+var codeGenerationSCL0 = document.getElementById('code_generation_scl_0');
+var codeGenerationSCL1 = document.getElementById('code_generation_scl_1');
+var codeGenerationSCL2 = document.getElementById('code_generation_scl_2');
 
 sedeGeneration.addEventListener('click', showListSedes);
 function showListSedes(event) {
   listSedes.classList.toggle('list_active');
-  /* console.log(event.target);
-    sedeGeneration.addEventListener('click', showListSedes);
-    function showListSedes(event) {
-    listSedes.classList.toggle('list_active');
-    console.log(event.target);
-    */
+  for (var i = 0; i < allSedes.length; i++) {
+    allSedes[i].addEventListener('click', addVarSede);
+    function addVarSede(event) {
+      var codeSede = event.target.dataset.sedecod;
+      console.log(codeSede);
+      var sede = Object.keys(data)[codeSede];
+      console.log(sede) ;
+      for (var i = 0; i < allSedes.length; i++) {
+        allSedes[i].addEventListener('click', showListGenerations);
+        function showListGenerations(event) {
+        // console.log(typeof event.target.dataset.sedecod)=string;
+          switch (event.target.dataset.sedecod) {
+          case '0' :
+            // activar display block de la lista de generaciones y que se vea desplegable
+            arequipaGenerations.classList.toggle('list_active') ;
+
+            // crear evento o funcion que al dar click me almacene la variable codeGeneration
+            for (var i = 0; i < allGenerationAQP.length; i++) {
+              allGenerationAQP[i].addEventListener('click', addVarGeneration);
+              function addVarGeneration(event) {
+                var codeGeneration = event.target.dataset.codeaqpgeneration;
+                console.log(codeGeneration);
+              }
+            }
+            break;
+          case '1' :
+            mexicoGenerations.classList.toggle('list_active') ;
+            for (var i = 0; i < allGenerationCDMX.length; i++) {
+              allGenerationCDMX[i].addEventListener('click', addVarGeneration);
+              function addVarGeneration(event) {
+                var codeGeneration = event.target.dataset.codecdmxgeneration;
+                console.log(codeGeneration);
+              }
+            }
+            break;
+          case '2' :
+            limaGenerations.classList.toggle('list_active') ;
+            for (var i = 0; i < allGenerationLIM.length; i++) {
+              allGenerationLIM[i].addEventListener('click', addVarGeneration);
+              function addVarGeneration(event) {
+                var codeGeneration = event.target.dataset.codelimgeneration;
+                console.log(codeGeneration);
+              }
+            }
+            break;
+          case '3' :
+            chileGenerations.classList.toggle('list_active') ;
+            for (var i = 0; i < allGenerationSCL.length; i++) {
+              allGenerationSCL[i].addEventListener('click', addVarGeneration);
+              function addVarGeneration(event) {
+                var codeGeneration = event.target.dataset.codesclgeneration;
+                console.log(codeGeneration);
+              }
+            }
+            break;
+          }
+          var generation = Object.keys(Object.values(data)[codeSede])[codeGeneration] ;
+          console.log(generation) ;
+        }
+      }
+    }
+  }
 };
 
-for (var i = 0; i < allSedes.length; i++) {
-  allSedes[i].addEventListener('click', addVarSede);
-  function addVarSede(event) {
-  //  console.log(event.target.dataset.cod);
-    var codeSede = event.target.dataset.sedecod;
-    console.log(codeSede);
-  };
-  allSedes[i].addEventListener('click', showListGenerations);
-  function showListGenerations(event) {
 
-  }
+for (var i = 0; i < allSedes.length; i++) {
+
 }
+
+// var generation = Object.keys(Object.values(data)[])
+
+/* var codeSede = '' ;
+switch (true) {
+case event.target.dataset.cod = 0:
+  codeSede = 0 ;
+  break;
+case event.target.dataset.cod = 1:
+  codeSede = 1 ;
+  break;
+case event.target.dataset.cod = 2:
+  codeSede = 2 ;
+  break;
+case event.target.dataset.cod = 3:
+  codeSede = 3 ;
+  break;
+}
+*/
+//  console.log(codeSede) ;
+
+/*
+sede1.addEventListener('click', addVarSede);
+function addVarSede(event) {
+  var codeSede = 1 ;
+  console.log(codeSede) ;
+}
+
+sede2.addEventListener('click', addVarSede);
+function addVarSede(event) {
+  var codeSede = 2 ;
+  console.log(codeSede) ;
+}
+
+sede3.addEventListener('click', addVarSede);
+function addVarSede(event) {
+  var codeSede = 3 ;
+  console.log(codeSede) ;
+}
+
+/*
+codeSede0.addEventListener('click', addvarcodesede);
+function addvarcodesede(event) {
+  codeSede0.textContent = objSedes.indexOf(event.target.textContent);
+
+  listSedes.classList.toggle('list_active');
+  console.log(event.target);
+};
+*/
+
+// data.AQP["2016-2"].students.length --> me da el total de estudiantes de arequipa 2016-2
+// console.log(data);
+
+// var students = data.AQP['2016-2'].students.length ;
+// console.log(students);
