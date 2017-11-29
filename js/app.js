e789611e41d0248b0259d6b905fa358d16f6ea09
LizbethFP <lizbeth.fp@gmail.com>
2017-11-29 12:49:00 -0500

    tabs

--- js/app.js
+++ js/app.js
@@ -1,60 +1,62 @@
-/* Dropdwon de menu*/
-var menu = document.querySelector('.menu');
-var subMenu = document.querySelectorAll('.country');
-var lowerMenu = document.querySelectorAll('.city');
-// console.log(subMenu);
-// function showCountry(event) {
-//   for (var i = 0; i < subMenu.length; i++) {
-//     subMenu[i].classList.toggle('display-none');
+// window.addEventListener('load', function(event) {
+//   /* Dropdwon de menu*/
+//   var menu = document.querySelector('.menu');
+//   var country = document.querySelector('.country');
+//   var city = document.querySelectorAll('.city');
+//   function showCity() {
+//
+//
+//   }
+// });
+/* Tabs */
+// var tab = document.querySelectorAll('.tab');
+//
+//
+// function showTab(event) {
+//   // console.log(event.target.dataset.selectedTab);
+//   selectedTab = event.target.dataset.selectedTab;
+//   var overView = document.querySelector('#over-view');
+//   var students = document.querySelector('#students');
+//   var teacher = document.querySelector('#teachers');
+//
+//   switch (selectedTab) {
+//   case 'tabOverView':
+//     students.style.displayNone;
+//     // teacher.style.displayNone;
+//     break;
+//
+//   case 'tabStudents':
+//     // overView.style.displayNone;
+//     // teacher.style.displayNone;
+//     break;
+//
+//   case 'tabTeacher':
+//     // overView.style.displayNone;
+//     // students.style.displayNone;
+//     break;
+//   default:
+//     // overView.style.displayNone;
+//     // students.style.displayNone;
+//     // teacher.style.displayNone;
+//     break;
 //   }
 // }
-function showCity(event) {
-  for (var i = 0; i < subMenu.length; i++) {
-    subMenu[i].classList.toggle('display-');
-  }
+// function loadPage() {
+//   var tab = document.querySelectorAll('.tab');
+//   for (var i = 0; i < tab.length; i++) {
+//     tab[i].addEventListener('click', showTab);
+//   }
+// }
+//
+//
+// loadPage();
+
+// Agregar fotos de coders y nombres
+function addCoder() {
+  var coderPhoto;
+  var coderName;
+  var coderTechSkills;
+  var coderLifeSkills;
+  console.log(data[]);
 }
-// menu.addEventListener('click', showCountry);
-subMenu.addEventListener('click', showCity);
-
-/* Tabs */
-var tab = document.querySelectorAll('.tab');
-
-
-function showTab(event) {
-  // console.log(event.target.dataset.selectedTab);
-  selectedTab = event.target.dataset.selectedTab;
-  var overView = document.querySelector('#over-view');
-  var students = document.querySelector('#students');
-  var teacher = document.querySelector('#teachers');
-
-  switch (selectedTab) {
-  case 'tabOverView':
-    students.style.displayNone;
-    // teacher.style.displayNone;
-    break;
-
-  case 'tabStudents':
-    // overView.style.displayNone;
-    // teacher.style.displayNone;
-    break;
-
-  case 'tabTeacher':
-    // overView.style.displayNone;
-    // students.style.displayNone;
-    break;
-  default:
-    // overView.style.displayNone;
-    // students.style.displayNone;
-    // teacher.style.displayNone;
-    break;
-  }
-}
-function loadPage() {
-  var tab = document.querySelectorAll('.tab');
-  for (var i = 0; i < tab.length; i++) {
-    tab[i].addEventListener('click', showTab);
-  }
-}
-
-
-loadPage();
+addCoder();
