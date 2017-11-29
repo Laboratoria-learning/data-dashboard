dfeb8ca3d0345342d21985b56d240e77fc24b1e0
Maythe <maytheinfante96@gmail.com>
2017-11-29 18:19:04 -0500

    puntajes de alumnas de todo los Sprints

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-window.addEventListener('load', function(event) {
-  console.log(event);
-  console.log(event.target);
-});
-//  data.AQP["2016-2"].students.length
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// solo para estudiantes AQP 2016-2
 
 var students = data.AQP['2016-2'].students;
 
 // calculando el total de estudiantes ACTIVAS AQP
 var activeAqp = 0;
 for (i = 0; i < students.length; i++) {
-  if (students[i].active === false) {
+  if (students[i].active === true) {
     var activeAqp = (activeAqp + 1);
-    console.log(activeAqp);
   }
 };
-
 // calculando el total de estudiantes INACTIVAS AQP
 var inactiveAqp = students.length - activeAqp;
-console.log(inactiveAqp);
+
+// calculado la suma de scores(HSE Y TECH) por sprints de cada estudiante
+
+//  suma score hse
+
+for (i = 0; i < students.length; i++) {
+  var studentsSprints = students[i].sprints;
+  console.log(studentsSprints);
+};
