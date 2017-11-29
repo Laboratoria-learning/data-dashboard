6534a29bff06f68e8921b2dac91bf701fc724ce0
jessia2011 <jessicamezarojas2011@gmail.com>
2017-11-29 10:43:18 -0500

    funcionalidad js

--- js/app.js
+++ js/app.js
@@ -4,12 +4,3 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
-
-obj = JSON.parse(data);
-
-var x = document.querySelector('.x');
-x.innerHTML = obj.students[0];
-
-
-
-
