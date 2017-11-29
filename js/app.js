c16edc2c477d5b79a2b3ccbdc100ec2d02eb67ec
gabrielamf <glennygabb@gmail.com>
2017-11-29 11:52:06 -0500

    añade funcion para mostrar estudiantes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', function() {
   var openAnimatedMenu = document.getElementById('open-animated-menu');
   var closeAnimatedMenu = document.getElementById('close-animated-menu');
-    
+
   openAnimatedMenu.addEventListener('click', function() {
     document.getElementById('animated-menu').style.width = '250px';
   });
@@ -11,54 +11,111 @@ window.addEventListener('load', function() {
     document.getElementById('animated-menu').style.width = '0';
   });
 
-});
+// ------------> Aquí termina la animación del menú <-----------------
 
-// ------------> ENROLLMENT - arequipa 2016-II <-----------------
+  var studentsTab = document.getElementById('studentsTab');
+  studentsTab.addEventListener('click', showSectionStudents);
 
-var totalStudentArequipa = data.AQP['2016-2'].students.length;
-var studentsArequipa = data.AQP['2016-2'].students;
-// recorre la longitud de las estudiantes y muestra a las que desertaron en porcentaje
-var desertaron = 0;
-for ( var i = 0; i < studentsArequipa.length; i++) {
-  if(studentsArequipa[i].active == false) {
-    desertaron++;
+  var sectionOverview = document.getElementById('section-overview');
+  var sectionStudents = document.getElementById('section-students');
+
+  function showSectionStudents(event) {
+    console.log(event.target);
+    sectionOverview.classList.add('hide');
+    sectionStudents.classList.remove('hide');
+    sectionStudents.style.cssText = 'display:inline-block; background:white;';
   }
-}
-var desertaron = Math.round((desertaron/totalStudentArequipa)*100) + '%';
 
-// Total estudiantes
-var totalStudents = document.getElementById('total-student');
-var text = document.createElement('p');
-text.textContent = totalStudentArequipa;
-var leyendaTotalStudents = document.createElement('p');
-leyendaTotalStudents.textContent = '# students currently enrolled';
-totalStudents.appendChild(text);
-totalStudents.appendChild(leyendaTotalStudents);
+  // ruta para hallar el value del filtro
+  var promoFilter = document.getElementById('promo-filter');
 
-// porcentaje de alumnas que desertaron
-var desertaronPorcentaje = document.getElementById('desertaron-porcentaje');
-var textPorcentaje = document.createElement('p');
-textPorcentaje.textContent = desertaron;
-var leyendaDesertaronPorcentaje = document.createElement('p');
-leyendaDesertaronPorcentaje.textContent = '% DROPOUT';
-desertaronPorcentaje.appendChild(textPorcentaje);
-desertaronPorcentaje.appendChild(leyendaDesertaronPorcentaje);
+  if (promoFilter.children[1].children[0].value === 'aqp-16-2') {
+    students = data.AQP['2016-2'].students;
+    //console.log(students);
+    showStudents();
+  } else if (promoFilter.children[1].children[1].value === 'aqp-17-1') {
+    students = data.AQP['2017-1'].students;
+    console.log(students);
+    showStudents();
+  }
 
-// ------------> NPS - arequipa 2016-II <-----------------
-var box2 = document.getElementById('aqp-nps-sprint1');
-var promoS120162 = data.AQP['2016-2'].ratings[0].nps.promoters;
-var detracS120162 = data.AQP['2016-2'].ratings[0].nps.detractors;
+  function showStudents() {
+    var container = document.getElementById('students-hidden');
+    for (var i = 0; i < students.length; i++) {
+    //crear un div para una estudiante
+   var profilestudent = document.createElement('div');
+   container.appendChild(profilestudent);
 
-var parag = document.createElement("p");
-var text = document.createTextNode(promoS120162);
-parag.appendChild(text);
-box2.appendChild(parag);
+   //crear la imagen para la foto de perfil
+   var imgprofile= document.createElement('img');
+   imgprofile.classList.add('img-student');
+   imgprofile.setAttribute('src', students[i].photo);
 
-var parag2 = document.createElement("p");
-var text2 = document.createTextNode(detracS120162);
-parag2.appendChild(text2);
-box2.appendChild(parag2);
+   profilestudent.appendChild(imgprofile);
+
+   //agregar nombre de estudiante
+   var information  = document.createElement('p');
+    information.classList.add('datos-studen');
+   var namem = document.createElement('h5');
+   namem.textContent=(students[i].name);
+
+   information.appendChild(namem);
+   profilestudent.appendChild(information);
+    }
+  }
+
+  // ------------> ENROLLMENT - arequipa 2016-II <-----------------
+
+  var totalStudentArequipa = data.AQP['2016-2'].students.length;
+  var studentsArequipa = data.AQP['2016-2'].students;
+
+  // recorre la longitud de las estudiantes y muestra a las que desertaron en porcentaje
+  var desertaron = 0;
+  for (var i = 0; i < studentsArequipa.length; i++) {
+    if (studentsArequipa[i].active === false) {
+      desertaron++;
+    }
+  }
+
+  desertaron = Math.round((desertaron / totalStudentArequipa) * 100) + '%';
+
+  // Total estudiantes
+  var totalStudents = document.getElementById('total-student');
+  var text = document.createElement('p');
+  text.textContent = totalStudentArequipa;
+  var leyendaTotalStudents = document.createElement('p');
+  leyendaTotalStudents.textContent = '# students currently enrolled';
+  totalStudents.appendChild(text);
+  totalStudents.appendChild(leyendaTotalStudents);
+
+  // porcentaje de alumnas que desertaron
+  var desertaronPorcentaje = document.getElementById('desertaron-porcentaje');
+  var textPorcentaje = document.createElement('p');
+  textPorcentaje.textContent = desertaron;
+  var leyendaDesertaronPorcentaje = document.createElement('p');
+  leyendaDesertaronPorcentaje.textContent = '% DROPOUT';
+  desertaronPorcentaje.appendChild(textPorcentaje);
+  desertaronPorcentaje.appendChild(leyendaDesertaronPorcentaje);
+
+  // ------------> NPS - arequipa 2016-II <-----------------
+  var box2 = document.getElementById('aqp-nps-sprint1');
+  var promoS120162 = data.AQP['2016-2'].ratings[0].nps.promoters;
+  var detracS120162 = data.AQP['2016-2'].ratings[0].nps.detractors;
+
+  var parag = document.createElement("p");
+  var text = document.createTextNode(promoS120162);
+  parag.appendChild(text);
+  box2.appendChild(parag);
+
+  var parag2 = document.createElement("p");
+  var text2 = document.createTextNode(detracS120162);
+  parag2.appendChild(text2);
+  box2.appendChild(parag2);
 
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+  // Puedes hacer uso de la base de datos a través de la variable `data`
+  console.log(data);
+
+
+
+});
