e4d9145e19177c5b249c8ce11eb8e09fd77eff58
Pamelaleo96 <delacruzlozano.p@gmail.com>
2017-11-29 13:56:24 -0500

    Añadiendo archivo js y modificando estructura html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-/*
- * Funcionalidad de tu producto
- */
-
+/* Funcionalidad de tu producto*/
+window.addEventListener('load', function() {
+    var currentStudents = document.getElementById('currentStudents');
+    var sede =
+function totalStudents (sede, generacion) {
+    if (sede =='Lima' && generacion == '2017-II'){
+    }
+totalStudents('Lima', '2017-II');
+});
+     
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
