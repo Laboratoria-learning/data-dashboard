eccbbeebfbc27aa6fe8237345118e98adf74f0ee
YasminMattos <yasmin.mattos.conislla@gmail.com>
2017-11-29 00:45:19 -0500

    añade cambio de estilos y gráfica nps

--- js/app.js
+++ js/app.js
@@ -1,51 +1,30 @@
-/*
- * Funcionalidad de tu producto
- */
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
-//console.log(data);
-
-
-// Load the Visualization API and the corechart package.
+// Carga la visualización API y el paquete corechart.
 google.charts.load('current', {'packages':['corechart']});
 
-    // Set a callback to run when the Google Visualization API is loaded.
+    // Establece una llamada que se ejecuta cuando la API es cargada.
     google.charts.setOnLoadCallback(drawChart);
 
-    // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
+      // Dibuja el gráfico
       function drawChart() {
 
-        // Create the data table.
+        // Crea los datos del gráfico.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Topping');
         data.addColumn('number', 'Slices');
         data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
+          ['Promoters', percentProm],
+          ['Detractors', percentDetr],
+          ['Passive', percentPass],
         ]);
 
-        // Set chart options
-        var options = {'title': 'How Much Pizza I Ate Last Night',
+        // Establece opciones del gráfico
+        var options = {'title': 'NET PROMOTER SCORE (NPS)',
                        'width': 400,
                        'height': 300};
-
-        // Instantiate and draw our chart, passing in some options.
+                
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
-
-//el rating es por sprint del 1 al 4
-/*
-console.log(data.AQP["2016-2"].ratings[0].nps["promoters"]);
-console.log(data.AQP["2016-2"].ratings[0].nps["passive"]);
-console.log(data.AQP["2016-2"].ratings[0].nps["detractors"]);
-*/
-
 
 //REQUERIMIENTO #5 solo sede Arequipa, primera generación
 var data = data.AQP["2016-2"].ratings;
@@ -84,3 +63,5 @@ console.log(percentDetr);//10%
 console.log(percentPass);//15%
 console.log(percentNps);//76%
 //Nota: Pienso que si la proporción de promoters es > a la de detractors, el NPS (puntaje neto promotor) es mayor
+// Con este código se puede cambiar tanto de sede como de generación
+// pero lo ideal es hacer una función.
