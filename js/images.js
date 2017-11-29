da432cd8b2da81102fcd44281e0ec28c2de6175e
wendytamara <anacarlavegam@gmail.com>
2017-11-29 08:30:49 -0500

    añadiendo fotos de las estudiantes

--- js/images.js
+++ js/images.js
@@ -1,26 +1,20 @@
 window.addEventListener("load", function(){
 
-
+debugger
 var contentStudentsImg = document.getElementById("content-students-img");
 
 var aqp = data.AQP;
 var students = aqp["2016-2"].students;
 
 for (var i = 0; i < students.length; i++) {
-  var studentsPhoto = students[i].photo;
-  console.log(studentsPhoto);
+ var studentsPhoto = students[i].photo;
+
+var img = document.createElement("img");
+img.setAttribute("src",studentsPhoto);
+
+contentStudentsImg.appendChild(img);
+img.style.width = "200px";
+img.style.height = "200px";
 }
-
-function insertImg(){
-  var img = document.createElement("img");
-  img.setAttribute("src", "studentsPhoto");
-
-
-  contentStudentsImg.appendChild(img);
-}
-
-
-
-
 
 })
