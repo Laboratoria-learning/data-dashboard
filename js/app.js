d81084daf2d546f6cf22e1898b269caa64a64b34
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2017-11-29 15:07:41 -0500

    Creando evento click, al boton de students

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-
-
+window.addEventListener('load',function(){
 var students = document.getElementById('students');
+var studentsBtn = document.getElementById('students-btn')
 
-  var div = document.createElement('div');
-  students.appendChild(div)
-  var addText = document.createTextNode("hola");
-  div.appendChild(addText);
-
+function provando(){
   var div2 = document.createElement('div');
   students.appendChild(div2);
-  var studentsAqp = document.createTextNode(data['AQP']['2016-2']['students'][0])
-  div2.appendChild(studentsAqp);
+  var aqp2016 = data['AQP']['2016-2']['students'][0]
+  div2.innerHTML = aqp2016.name
+ }
+
+studentsBtn.addEventListener('click', function(){
+   provando(students);
+})
+
 //console.log(data['AQP']['2016-2']['students'][0])
+});
