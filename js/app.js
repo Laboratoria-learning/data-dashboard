9515ce62120e7d4c4d84974b878a94f9fa99c62f
llandreall <andrea.telleria@gmail.com>
2017-11-29 12:15:36 -0500

    Archivo README modificado y pruebas de funcionalidad en JS

--- js/app.js
+++ js/app.js
@@ -2,5 +2,23 @@
  * Funcionalidad de tu producto
  */
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
+// Pruebas en consola
+var lim20162 = data.LIM['2016-2'];
+var lim20171 = data.LIM['2017-1'];
+var lim20172 = data.LIM['2017-2'];
+var aqp20162 = data.AQP['2016-2'];
+var aqp20171 = data.AQP['2017-1'];
+var mex20171 = data.CDMX['2017-1'];
+var mex20172 = data.CDMX['2017-2'];
+var scl20162 = data.SCL['2016-2'];
+var scl20171 = data.SCL['2017-1'];
+var scl20172 = data.SCL['2017-2'];
+
+/*
+var lim20162students = Object.values(lim20162)[0];
+var lim20171students = Object.values(lim20171)[0];
+var lim20172students = Object.values(lim20172)[0];
+
 console.log(data);
+console.log(lim20172students);
+*/
