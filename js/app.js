712364bf19f1ed9d9df2d398366b50af99ea6bf0
Victoria <vtr.lung@outlook.es>
2017-11-29 13:42:50 -0500

    agregando eventos checkbox

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-window.addEventListener('load', function(event){
-    var displayIcon = document.getElementById('fa-sort-desc')
-  ;
-})
+window.addEventListener('load', function(event) {
+  var displayFilter = document.getElementById('fa-sort-desc');
+  displayFilter.addEventListener('click', function(event) {
+    var checkBoxSite = document.getElementById('checkbox-site');
+    var classCheckBoxSite = document.getElementsByClassName('checkbox-site')[0];
+    if (classCheckBoxSite.style.display = 'none') {
+      classCheckBoxSite.style.display = 'block';
+      classCheckBoxSite.style.left = '180px';
+      classCheckBoxSite.style.up = '30px';
+    } else classCheckBoxSite.style.display = 'none';
+    console.log(event.target);
+  });
+});
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
