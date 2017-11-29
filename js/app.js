df1f2a03a9edd8d65345bc4b5ade846c7552dabe
Isabel <ycnia_1403@hotmail.com>
2017-11-29 01:35:33 -0500

    Funcionalidad de los tabs Overviews, Students y Teachers

--- js/app.js
+++ js/app.js
@@ -3,14 +3,34 @@
  */
 // Funcionalidad de los tabs Overviews, Students y Teachers
 
-var showHide = function(e){
+var showHide = function(e){ 
     var tabSelector = e.target.dataset.tabSelector;
-    if (tabSelector=== 'tabOverviews'){
+    var overviews = document.getElementById('overviews');
+    var students = document.getElementById('students');
+    var teachers = document.getElementById('teachers');
+
+    if (tabSelector === 'tabOverviews'){
         console.log('mostrar overviews');
+        // ocultar students y teacher
+        students.style.display='none';
+        teachers.style.display='none';
+        // mostrar overviews
+        overviews.style.display='block';
+
     } else if (tabSelector === 'tabStudents'){
         console.log('Mostrar a los estudiantes');
+        // ocultar overviews y teacher
+        overviews.style.display = 'none';
+        teachers.style.display = 'none';
+        // mostrar students
+        students.style.display = 'block';
     } else if (tabSelector === 'tabTeachers'){
         console.log('mostrar a los profesores');
+        // mostrar overviews y students
+        students.style.display = 'none';
+        overviews.style.display = 'none';
+        // mostrar students
+        teachers.style.display = 'block';
     }
 }
 
@@ -21,9 +41,16 @@ var loadTabs = function(){
     }
 }
 
-loadTabs();
+loadTabs()
 
+// funcionalidades del menu desplegable de las sedes de laboratoria
 
+for(var sede in data){
+    console.log(sede)
+}
+
+var city = Object.keys(data);
+console.log(city[1])
 
 // // Puedes hacer uso de la base de datos a través de la variable `data`
 // console.log(data);
