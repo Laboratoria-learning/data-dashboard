97dacbc04bbd5f230dc1855ee289203d6af8613d
wendytamara <anacarlavegam@gmail.com>
2017-11-29 08:56:30 -0500

    añadiendo fotos de las estudiantes y nombres de cada una

--- js/images.js
+++ js/images.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function(){
 
-debugger
+//debugger
 var contentStudentsImg = document.getElementById("content-students-img");
 
 var aqp = data.AQP;
@@ -8,13 +8,25 @@ var students = aqp["2016-2"].students;
 
 for (var i = 0; i < students.length; i++) {
  var studentsPhoto = students[i].photo;
+ var nameStudents = students[i].name;
 
+var div = document.createElement("div");
 var img = document.createElement("img");
-img.setAttribute("src",studentsPhoto);
+var span = document.createElement("span");
 
-contentStudentsImg.appendChild(img);
+
+img.setAttribute("src",studentsPhoto);
+img.setAttribute("alt","foto de la estudiante");
+
+
+div.appendChild(img);
+div.appendChild(span);
+span.textContent = nameStudents;
+contentStudentsImg.appendChild(div);
 img.style.width = "200px";
 img.style.height = "200px";
+
+
 }
 
 })
