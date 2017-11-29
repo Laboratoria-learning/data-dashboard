0dbd33429fbd49f9fd5c213cadbc0eda97f4f662
SIlviaTivi <skfl.erg@hotmail.com>
2017-11-28 22:22:53 -0500

    numero de estudiantes que superan la meta

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ console.log(data);
 // total de estudiantes presentes por sede y generacion
 /* por generacion:--------------------------------------------------*/
 // CDMX
-var numEstudentsCDMX20171 = data.CDMX['2017-1'].students.length + 1;
+var numEstudentsCDMX20171 = data.CDMX['2017-1'].students.length;
 console.log('numero estudiantes CDMX (2017-1) : ' + numEstudentsCDMX20171);
-var numEstudentsCDMX20172 = data.CDMX['2017-2'].students.length + 1;
+var numEstudentsCDMX20172 = data.CDMX['2017-2'].students.length;
 console.log('numero estudiantes CDMX (2017-2) : ' + numEstudentsCDMX20172);
 // LIM
-var numEstudentsLIM20162 = data.LIM['2016-2'].students.length + 1;
+var numEstudentsLIM20162 = data.LIM['2016-2'].students.length;
 console.log('numero estudiantes LIM (2016-2) : ' + numEstudentsLIM20162);
-var numEstudentsLIM20171 = data.LIM['2017-1'].students.length + 1;
+var numEstudentsLIM20171 = data.LIM['2017-1'].students.length;
 console.log('numero estudiantes LIM (2017-1) : ' + numEstudentsLIM20171);
-var numEstudentsLIM20172 = data.LIM['2017-2'].students.length + 1;
+var numEstudentsLIM20172 = data.LIM['2017-2'].students.length;
 console.log('numero estudiantes LIM (2017-2) : ' + numEstudentsLIM20172);
 
 /* por sede:------------------------------------------------------*/
@@ -119,3 +119,46 @@ console.log('% estudiantes Desertaron LIM20171 : ' + percDesertedLIM20171);
 // % estudiantes desertaron LIM 2017-2
 var percDesertedLIM20172 = (numdesertedLIM20172 / numEstudentsLIM20172) * 100;
 console.log('% estudiantes Desertaron LIM20172 : ' + percDesertedLIM20172);
+
+
+/* Cantidad de estudiantes que superan la meta*/
+
+/* ratings de estudiantes---------------------------------------------*/
+// estudiantes CDMX
+var ratingsCDMX20171 = data.CDMX['2017-1'].ratings;// Array
+var ratingsCDMX20172 = data.CDMX['2017-2'].ratings;
+// estudiantes LIM
+var ratingsLIM20162 = data.LIM['2016-2'].ratings;
+var ratingsLIM20171 = data.LIM['2017-1'].ratings;
+var ratingsLIM20172 = data.LIM['2017-2'].ratings;
+
+
+/* porcentajes de estudiantes superaron la meta CDMX 2017-1 por sprint--------------------------*/
+percesExceedGoalCDMX20171 = [];
+for (var i = 0; i < ratingsCDMX20171.length; i++) {
+  percesExceedGoalCDMX20171.push(ratingsCDMX20171[i].student.supera);
+}
+console.log('% estudiantes exceden meta en CDMX20171 : ' + percesExceedGoalCDMX20171);
+
+// numero estudiantes superaron la meta CDMX 2017-1 por sprint
+
+var sprint1numExceedGoalCDMX20171 = percesExceedGoalCDMX20171[0] * numEstudentsCDMX20171 / 100; // <-- linea 9
+console.log('numero alumnas exceden meta CDMX20171-sprint1 : ' + sprint1numExceedGoalCDMX20171);
+var sprint2numExceedGoalCDMX20171 = percesExceedGoalCDMX20171[1] * numEstudentsCDMX20171 / 100;
+console.log('numero alumnas exceden meta CDMX20171-sprint2 : ' + sprint2numExceedGoalCDMX20171);
+var sprint3numExceedGoalCDMX20171 = percesExceedGoalCDMX20171[2] * numEstudentsCDMX20171 / 100;
+console.log('numero alumnas exceden meta CDMX20171-sprint3 : ' + sprint3numExceedGoalCDMX20171);
+
+/* porcentajes de estudiantes superaron la meta CDMX 2017-2 por sprint--------------------------*/
+percesExceedGoalCDMX20172 = [];
+for (var i = 0; i < ratingsCDMX20172.length; i++) {
+  percesExceedGoalCDMX20172.push(ratingsCDMX20172[i].student.supera);
+}
+console.log('% estudiantes exceden meta en CDMX20172 : ' + percesExceedGoalCDMX20172);
+
+// numero estudiantes superaron la meta CDMX 2017-1 por sprint
+
+var sprint1numExceedGoalCDMX20172 = percesExceedGoalCDMX20172[0] * numEstudentsCDMX20172 / 100; // <-- linea 9
+console.log('numero alumnas exceden meta CDMX20172-sprint1 : ' + sprint1numExceedGoalCDMX20172);
+var sprint2numExceedGoalCDMX20172 = percesExceedGoalCDMX20172[1] * numEstudentsCDMX20172 / 100;
+console.log('numero alumnas exceden meta CDMX20172-sprint2 : ' + sprint2numExceedGoalCDMX20172);
