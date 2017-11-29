be8f0cbc71d1316fa27c1204730ec4f63290f0ad
MarinaRH <l_marina1717@hotmail.com>
2017-11-29 09:40:13 -0500

    agregando la funcion para mostrar contenedores

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 /*
  * Funcionalidad de tu producto
  */
+window.addEventListener('load', function() {
+  var tabs = document.querySelectorAll('.tab');
+  var contenidos = document.querySelectorAll('.inform');
+  
+  function toggleTab(elem) {
+    for (var i = 0; i < elem.length; i++) {
+      elem[i].addEventListener('click', showContainers);
+    }
+  }
+  function showContainers() {
+    for (var j = 0; j < contenidos.length; j++) {
+      if (this.dataset.text === contenidos[j].dataset.text) {
+        if (contenidos[j].classList.contains('hiden')) {
+          contenidos[j].classList.remove('hiden');
+        }
+      } else {
+        contenidos[j].classList.add('hiden');
+      }
+      /* this.classList.add('red');*/
+    }
+  }
+  
+  // window.onload = function() {
+  //   toggleTab(tabs);
+  // };
+  toggleTab(tabs);
+});
