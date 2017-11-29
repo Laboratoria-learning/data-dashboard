940623b20ae5110f79feddbdf0702f8c5f4b6863
LeslyN <lnombertoc@gmail.com>
2017-11-29 12:55:03 -0500

    Cambiando estructura y agregado funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 /*
  * Funcionalidad de tu producto
  */
+window.addEventListener('load', function() {  
+  var listMenu = document.querySelectorAll('.general-menu');
+  for (var i = 0; i < listMenu.length; i++) {
+    listMenu[i].addEventListener('click', showMenu);
+  }
+  
+  function showMenu(event) {
+    console.log(event.target);
+  }
+});
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
