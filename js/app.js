28ba585302520f9dfc14aec6ccec960af9313a00
Nefelijm <sistemasnik20@gmail.com>
2017-11-29 11:29:55 -0500

    modificacion de css y html y jss 1

--- js/app.js
+++ js/app.js
@@ -4,90 +4,21 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 // console.log(data);
 
-// window.addEventListener .....
-var mostraOcultar = function(event) {
-  // console.log(event.target.dataset.tabSelection);
-  var tabSelection = event.target.dataset.tabSelection;
-  var overview = document.getElementById('overview');
-  var students = document.getElementById('students');
-  /* ********************************************** */
-  // var teachers = document.getElementById('teachers');
-  // var allGenerations = document.getElementById('allGeneration');
-  /* ********************************************** */
-  if (tabSelection === 'tabOverview') {
-    console.log('Dashboard-Overview');
-    // ocultar students, teachers, AllGenerations
-    students.style.display = none;
-    teachers.style.display = none;
-    allGenerations.style.display = none;
-    // mostrarOverview
-    overview.style.display = block;
-  } else if (tabSelection === 'tabStudents') {
-    console.log('Dashboard-Students');
-    // ocultar overview, teachers, AllGenerations
-    overview.style.display = none;
-    teachers.style.display = none;
-    allGenerations.style.display = none;
-    // mostrarStudents
-    students.style.display = block;
-  } /* else if (tabSelection === 'tabTeachers') {
-    console.log('Dashboard-Teachers');
-    // ocultar overview, students, allGenerations
-    overview.style.display = none;
-    students.style.display = none;
-    allGenerations.style.display = none;
-    // mostrarTeachers
-    teachers.style.display = block;
-  } else if (tabSelection === 'tabAllGenerations') {
-    console.log('Dashboard-AllGenerations');
-    // ocultar overview, students, Teachers
-    overview.style.display = none;
-    students.style.display = none;
-    teachers.style.display = none;
-    // mostrar allGenerations
-    allGenerations.style.display = block;
-  }*/
-};
-var loadPage = function() {
-  var overview = document.getElementById('overview');
-  var students = document.getElementById('students');
-  /* ************************************************* */
-  // var teachers = document.getElementById('teachers');
-  // var allGenerations = document.getElementById('allGeneration');
-  overview.style.display = none;
-  students.style.display = none;
-  /* ************************************************ */
-  // teachers.style.display = none;
-  // allGenerations.style.display = none;
-  var elementsTab = document.getElementsByClassName('tab');
-  for (var i = 0; i < elementsTab.length;i++) {
-    elementsTab[i].addEventListener('click', mostraOcultar);
-  }
-};
-
-loadPage();
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
-<<<<<<< HEAD
-// console.log(data);
-// var alert = ('Hola mundo');
-// var prompt = ('Hola mundo');
-=======
-console.log(data);
-
-window.addEventListener('load', eventos);
-
-
-<<<<<<< HEAD
-
-
-
-
-
-
-function eventos (){
+window.addEventListener('load', select);
+function create() {       
+  var sede = document.getElementById('sede').value;   
+  document.getElementById('text').innerHTML = sede ;   
+    var datos = (data["AQP"]["2016-2"]["students"][0]["active"]);
+    
+    if (datos == true ) {
+     document.getElementById('one').innerHTML = datos.length; 
+ }
+ 
 
 }
-=======
->>>>>>> ccab019fddbcd3327abdf678fd4208e95a26c898
->>>>>>> 32cd494796c7967208e30d6d088aaae8bf35ec03
+
+
+function select() {
+  sede.addEventListener('change', create);
+};
+
