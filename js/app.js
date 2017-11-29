e51af4b83271c1ab1ae5e35ff59e86d83cf0b050
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-29 13:56:07 -0500

    implementado los filtros

--- js/app.js
+++ js/app.js
@@ -1,21 +1,60 @@
 // agregar el evento load
 window.addEventListener('load', function() {
-  var lima = data.LIM;
-  // console.log(lima);
-  var arequipa = data.AQP;
-  // console.log(arequipa);
-  var santiagoChile = data.SCL;
-  // console.log(santiagoChile);
-  var cMexico = data.CDMX;
-  // console.log(cMexico);
   var selection = document.getElementById('selection');
   // console.log(selection);
   var contenidoSede = document.getElementById('contenido-sede');
   // console.log(contenidoSede);
   var tabs = document.getElementsByClassName('tab');
   var contents = document.getElementsByClassName('content');
-  var filtrosSede = document.getElementById('filtros-sede');
 
+  var studentsInscribed = document.getElementById('students-inscribed');
+  var studentsDeserted = document.getElementById('students-deserted');
+
+  var studentsApproved = document.getElementById('students-approved');
+  var totalApproved = document.getElementById('total-approved');
+
+  var averageNps = document.getElementById('average');
+
+  var approvedTech = document.getElementById('approved-tech');
+  var averageTech = document.getElementById('average-tech');
+
+  var approvedHse = document.getElementById('approved-hse');
+  var averageHse = document.getElementById('average-hse');
+
+  var averageSatisfied = document.getElementById('average-satisfied');
+
+  var scoresTeacher = document.getElementById('scores-teacher');
+
+  var scoresJedi = document.getElementById('scores-jedi');
+
+  function mostrarInfo() {
+  };
+
+  selection.addEventListener('change', function(e) {
+  //  console.log(e.target.value);
+    var value = e.target.value; 
+    var aux = value.split('-');
+    var sedeName = aux.shift();
+    var generation = aux.join('-');
+
+    var generationData = data[sedeName][generation];
+    var totalStudents = generationData.students.length;
+    // Datos de estudiantes inscritas
+    studentsInscribed.textContent = totalStudents;
+    // var div = document.createElement('div');
+
+    var counter = 0;
+    generationData.students.forEach(function(student) {
+      // console.log(student);
+      if (student.active === false) {
+        counter ++;
+      }
+    });
+    // Datos de estudiantes que desartaron
+    studentsDeserted.textContent = Math.floor((counter * 100) / totalStudents) + '%';
+
+  });
+ 
   // agregar el evento click a todos los tabs
   for (var i = 0; i < tabs.length; i++) {
     // y dentro del click para cada tab
@@ -36,5 +75,4 @@ window.addEventListener('load', function() {
       contents[event.target.dataset.content].classList.add('active');
     });
   }
-
 });
