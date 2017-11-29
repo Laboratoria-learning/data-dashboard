4b1c308c0e99772c025efa5ccbc2c203bdcc489b
elenaJO <ele.lucero1993@gmail.com>
2017-11-29 08:37:45 -0500

    añadiendo eventos, maquetacion y estilos

--- js/app.js
+++ js/app.js
@@ -3,4 +3,168 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+
+console.log(data['SCL']);
+console.log(data['AQP']['2016-2']['students'][1]['active']);
+console.log(data['AQP']['2016-2']['ratings'][1]['nps']['promoters']);
+console.log(data['AQP']['2016-2']['ratings'][2]['teacher']);
+// cambiar pestañas
+
+var list = document.getElementsByTagName('ul')[0].children;
+var elementTab = document.getElementsByClassName('tab');
+var containerOverview = document.getElementById('container-overview');
+var containesStudents = document.getElementById('container-students');
+var comboBox = document.getElementById('combo-box');
+var studentCurrently = document.getElementById('student-currently');
+var dropout = document.getElementById('dropout');
+var cumulativeNps = document.getElementById('cumulative-nps');
+var percentage = document.getElementById('percentage');
+function show(event) {
+  var tabSelection = event.target.dataset.tabSelection;
+  if (tabSelection === 'overview') {
+    containesStudents.classList.remove('block');
+    containesStudents.classList.add('none');
+    containerOverview.classList.remove('none');
+    containerOverview.classList.add('block');
+    list[1].classList.remove('hover');
+    list[0].classList.add('hover');
+  } else {
+    containerOverview.classList.remove('block');
+    containerOverview.classList.add('none');
+    containesStudents.classList.remove('none');
+    containesStudents.classList.add('block');
+    containesStudents.classList.add('block');
+    list[0].classList.remove('hover');
+    list[1].classList.add('hover');
+  }
+}
+
+function changeTitle() {
+  var titleSelector = document.getElementById('title-selector');
+  var valueSelector = comboBox.value;
+  
+  var returnCurrently;
+  switch (valueSelector) {
+  case 'lim2016II':
+    titleSelector.textContent = 'Lima 2016-II';
+    calculateCurrently('LIM', 35, '2016-2');
+    calculatePromoter('LIM', 2, '2016-2');
+    var attend = parseInt(calculateCurrently('LIM', 35, '2016-2'));
+    drawCurrently(attend, 35 - attend);
+    break;
+  case 'lim2017I':
+    titleSelector.textContent = 'Lima 2017-I';
+    calculateCurrently('LIM', 17, '2017-1');
+    calculatePromoter('LIM', 4, '2017-1');
+    var attend = parseInt(calculateCurrently('LIM', 17, '2017-1'));
+    drawCurrently(attend, 17 - attend);
+    break;
+  case 'lim2017II':
+    titleSelector.textContent = 'Lima 2017-II';
+    calculateCurrently('LIM', 14, '2017-2');
+    calculatePromoter('LIM', 2, '2017-2');
+    var attend = parseInt(calculateCurrently('LIM', 14, '2017-2'));
+    drawCurrently(attend, 14 - attend);
+    break;
+  case 'are2016II':
+    titleSelector.textContent = 'Arequipa 2016-II';
+    calculateCurrently('AQP', 15, '2016-2');
+    calculatePromoter('AQP', 4,'2016-2');
+    var attend = parseInt(calculateCurrently('AQP', 15, '2016-2'));
+    drawCurrently(attend, 15 - attend);
+    break;
+  case 'are2017I':
+    titleSelector.textContent = 'Arequipa 2017-I';
+    calculateCurrently('AQP', 15, '2017-1');
+    calculatePromoter('AQP', 3, '2017-1');
+    var attend = parseInt(calculateCurrently('AQP', 15, '2017-1'));
+    drawCurrently(attend, 15 - attend);
+    break;
+  case 'chi2016II':
+    titleSelector.textContent = 'Chile 2016-II';
+    calculateCurrently('SCL', 11, '2016-2');
+    calculatePromoter('SCL', 4, '2016-2');
+    var attend = parseInt(calculateCurrently('SCL', 11, '2016-2'));
+    drawCurrently(attend, 11 - attend);
+    break;
+  case 'chi2017I':
+    titleSelector.textContent = 'Chile 2017-I';
+    calculateCurrently('SCL', 23, '2017-1');
+    calculatePromoter('SCL', 3, '2017-1');
+    var attend = parseInt(calculateCurrently('SCL', 23, '2017-1'));
+    drawCurrently(attend, 23 - attend);
+    break;
+  case 'chi2017II':
+    titleSelector.textContent = 'Chile 2017-II';
+    calculateCurrently('SCL', 61, '2017-2');
+    calculatePromoter('SCL', 2, '2017-2');
+    var attend = parseInt(calculateCurrently('SCL', 61, '2017-2'));
+    drawCurrently(attend, 61 - attend);
+    break;
+  case 'mex2017I':
+    titleSelector.textContent = 'Mexico 2017-I';
+    calculateCurrently('CDMX', 24, '2017-1');
+    calculatePromoter('CDMX', 3, '2017-1');
+    var attend = parseInt(calculateCurrently('CDMX', 24, '2017-1'));
+    drawCurrently(attend, 24 - attend);
+    break;
+  case 'mex2017II':
+    titleSelector.textContent = 'Mexico 2017-II';
+    calculateCurrently('CDMX', 46, '2017-2');
+    calculatePromoter('CDMX', 2,'2017-2');
+    var attend = parseInt(calculateCurrently('CDMX', 46, '2017-2'));
+    drawCurrently(attend, 46 - attend);
+    break;
+  }
+}
+
+function calculateCurrently(sede, numEstudents, year) {
+  var cont = 0;
+  for (var i = 0 ; i < numEstudents;i++) {
+    var asisten = data[sede][year]['students'][i]['active'];
+    if (asisten === true) {
+      cont++;
+    }
+  }
+  studentCurrently.textContent = numEstudents + ' students currently enrolled';
+  dropout.textContent = ((100 * (numEstudents - cont)) / numEstudents).toFixed(0) + ' % dropout';
+  return cont;
+}
+
+function calculatePromoter(sede, numSprint, year) {
+  var contPromoters = 0;
+  var contDetractors = 0;
+  var contPassive = 0;
+  for (var i = 0;i < numSprint;i++) {
+    contPromoters = data[sede][year]['ratings'][i]['nps']['promoters'] + contPromoters;
+    contDetractors = data[sede][year]['ratings'][i]['nps']['detractors'] + contDetractors;
+    contPassive = data[sede][year]['ratings'][i]['nps']['passive'] + contDetractors;
+  }
+  cumulativeNps.innerHTML = ((contPromoters / numSprint).toFixed(0) - (contDetractors / numSprint).toFixed(0)) + ' % cumulative NPS';
+  percentage.innerHTML = (contPromoters / numSprint).toFixed(0) + ' % Promoters <br>' + (contPassive / numSprint).toFixed(0) + ' % Passive </br>' + (contDetractors / numSprint).toFixed(0) + ' % Detractors';
+}
+
+function calculateTeacherRating() {
+
+}
+
+function drawCurrently(attend, noAttend) {
+  var data = google.visualization.arrayToDataTable([
+    ['Currently', 'Number Student'],
+    ['Asisten', attend],
+    ['No Asisten', noAttend],
+  ]);
+  var options = {
+    'width': 300,
+    'height': 250};
+
+  var chart = new google.visualization.PieChart(document.getElementById('grafy-enrollment'));
+  chart.draw(data, options);
+}
+
+window.onload = function() {
+  elementTab[0].addEventListener('click', show);
+  elementTab[1].addEventListener('click', show);
+  containesStudents.classList.add('none');
+  comboBox.addEventListener('change',changeTitle)
+}
