49755e216e3598ceb9a2111828427f8abd7afdea
NishiAicchi <ai.espichan@gmail.com>
2017-11-29 16:55:15 -0500

    version MPV 2.1 - Añadiendo funcionalidades al 5%

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 /*
  * Funcionalidad de tu producto
  */
+var showHide = function(e) {
+    var tabs = e.target.dataset.selecttab;
+    var overContent = document.getElementById('overview-tab');
+    var studentsTab = document.getElementById('students-tab');
+    if(tabs === 'overview'){
+      console.log('bien hecho');
+      //ocultar students y teachers
+      //mostrar info de overview
+      var changeOne = overContent.style.display = 'block';
+      change.toggle('overview-tab');
+    } else if (tabs === 'students'){
+      console.log('vas bien');
+      //ocultar overview y teachers
+      changeOne.toggle('overview-tab');
+      //mostrar students
+      var changeTwo = studentsTab.style.display = 'block';
+      
+    changeTwo.toggle('students-tab');
+    } if (tabs === 'teachers')
+      console.log('lo lograste');
+      //ocultar students y overview
+      //mostrar teachers
+  }
+  var chargePage = function() {
+    var tabElements = document.getElementsByClassName('tabBtn');
+    for(var i = 0; i < tabElements.length; i++) {
+      tabElements[i].addEventListener('click', showHide);
+    }
+  }
+  
+  chargePage();
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
