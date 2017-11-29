9c489dd60264e82163a585c65fb5863424b0f993
Cintia Tahirih <tahysluana@gmail.com>
2017-11-29 17:48:43 -0500

    extrae data

--- js/app.js
+++ js/app.js
@@ -1,77 +1,73 @@
-/*
- * Funcionalidad de tu producto
- */
-var studentsContainer = document.getElementById('coders-container');
-var selectGenerationLima = document.getElementsByClassName('generacion')[0];
-var selectGenerationAqp = document.getElementsByClassName('generacion')[1];
-var selectGenerationSch = document.getElementsByClassName('generacion')[2];
-var selectGenerationDf = document.getElementsByClassName('generacion')[3];
-var selectGenerationG = document.getElementsByClassName('generacion')[4];
-var subImagesContainer = null;
-
-
-window.addEventListener('load', function() {  
+window.addEventListener('load', function() {
   var listMenu = document.querySelectorAll('.general-menu');
+  var studentsContainer = document.getElementById('coders-container');
+  var selectGenerationLima = document.getElementsByClassName('generacion')[0];
+  var selectGenerationAqp = document.getElementsByClassName('generacion')[1];
+  var selectGenerationSch = document.getElementsByClassName('generacion')[2];
+  var selectGenerationDf = document.getElementsByClassName('generacion')[3];
+  var selectGenerationG = document.getElementsByClassName('generacion')[4];
+  var infoCoder = document.getElementsByClassName('student');
+  var subInfoContainer = null;
+
   for (var i = 0; i < listMenu.length; i++) {
     listMenu[i].addEventListener('click', showMenu);
   }
-  
+
   function showMenu(event) {
     console.log(event.target);
   }
+
+  // constantes
+  var FIRST15 = 'first-15';
+  var SECOND15 = 'second-15';
+  var FIRST16 = 'first-16';
+  var SECOND16 = 'second-16';
+  var FIRST17 = 'first-17';
+  var SECOND17 = 'second-17';
+
+  selectGenerationLima.addEventListener('change', selectionGeneration);
+  selectGenerationAqp.addEventListener('change', selectionGeneration);
+  selectGenerationSch.addEventListener('change', selectionGeneration);
+  selectGenerationDf.addEventListener('change', selectionGeneration);
+  selectGenerationG.addEventListener('change', selectionGeneration);
+
+  showAll();
+
+  function selectionGeneration(event) {
+    for (var j = 0; j < data.LIM["2017-2"].students.length; j++) {
+        var newCoder = document.createElement('div');
+        newCoder.setAttribute('class', 'student');
+        newCoder.textContent = data.LIM["2017-2"].students[j].name;
+        infoCoder = studentsContainer.appendChild(newCoder);
+      }
+    switch (true) {
+    case event.target.value === '':
+      showAll();
+      break;
+    case event.target.value === 'first-15':
+      showAll();
+      break;
+    case event.target.value === 'second-15':
+      showAll();
+      break;
+    case event.target.value === 'first-16':
+      showAll();
+      break;
+    case event.target.value === 'second-16':
+      addImages(infoCoder, SECOND16);
+      break;
+    case event.target.value === 'first-17':
+      addImages(infoCoder, FIRST15);
+      break;
+    case event.target.value === 'second-17':
+      addImages(infoCoder, SECOND15);
+      break;
+    }
+    console.log('hola');
+  }
+  function showAll() {
+console.log('hola2');
+  }
+
+
 });
-
-// constantes
-var FIRST15 = 'first-15';
-var SECOND15 = 'second-15';
-var FIRST16 = 'first-16';
-var SECOND16 = 'second-16';
-var FIRST17 = 'first-17';
-var SECOND17 = 'second-17';
-
-selectGenerationLima.addEventListener('change', selectionGeneration);
-selectGenerationAqp.addEventListener('change', selectionGeneration);
-selectGenerationSch.addEventListener('change', selectionGeneration);
-selectGenerationDf.addEventListener('change', selectionGeneration);
-selectGenerationG.addEventListener('change', selectionGeneration);
-
-var df;
-
-showAll();
-
-
-function selectionGeneration(event) {
-  switch (true) {
-  case event.target.value === '':
-    showAll();
-    break;
-  case event.target.value === 'first-15':
-    addImages(datos, FIRST15);
-    break;
-  case event.target.value === 'second-15':
-    addImages(datos, SECOND15);
-    break;
-  case event.target.value === 'first-16':
-    addImages(datos, FIRST16);
-    break;
-  case event.target.value === 'second-16':
-    addImages(datos, SECOND16);
-    break;
-  case event.target.value === 'first-17':
-    addImages(datos, FIRST15);
-    break;
-  case event.target.value === 'second-17':
-    addImages(datos, SECOND15);
-    break;
-  }
-}
-
-function addImages() {
-  for (var key in data) {
-    console.log(arrayProductoInd[key]);
-  }
-}
-
-function showAll() {
-
-}
