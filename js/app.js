0b092af315e4056bacb33b252e90277e88d61717
adayc <adayc@hotmail.com>
2017-11-29 04:44:13 -0500

    Calculando NPS

--- js/app.js
+++ js/app.js
@@ -4,8 +4,57 @@
 // Declarando variables
 optionSprint = document.getElementById('sprint');
 optionSpecialization = document.getElementById('specialization');
-
+divNps = document.getElementById('kpi-nps');
+divPromoters = document.getElementById('promoters');
+divPassive = document.getElementById('passive');
+divDetractors = document.getElementById('detractors');
 // Puedes hacer uso de la base de datos a través de la variable `data`
+
+window.addEventListener('load', function(event) {
+  // Ocultando especializacion de las estudiantes
+  optionSpecialization.classList.add('hidden');
+  
+  var generationx = data['LIM']['2016-2'];
+
+  // Cargando las opciones de sprint segun generation 
+  for (i = 0;i < generationx['ratings'].length;i++) {
+    var option = document.createElement('option');
+    option.text = 'Sprint ' + generationx['ratings'][i]['sprint'];
+    option.value = generationx['ratings'][i]['sprint']-1;
+    option.id = generationx['ratings'][i]['sprint']-1;
+    optionSprint.add(option);
+  }
+
+  console.log(optionSprint);
+  // Evento para elegir el Sprint
+  optionSprint.addEventListener('change', function(event) {
+    // Mostramos especializaciones en el caso que el sprnt sea mayor o igual a 5
+    if (optionSprint.value >= 5) {
+      optionSpecialization.classList.add('show');
+      optionSpecialization.classList.remove('hidden');
+    } else {
+      optionSpecialization.classList.add('hidden');
+      optionSpecialization.classList.remove('show');
+    }
+
+    var promoters = generationx['ratings'][event.target.value]['nps']['promoters'];
+    var passive = generationx['ratings'][event.target.value]['nps']['passive'];
+    var detractors = generationx['ratings'][event.target.value]['nps']['detractors'];
+    
+    var nps = promoters - detractors;
+    console.log(nps);
+    divNps.innerHTML = nps + '%';
+    divPromoters.innerHTML = 'Promoters ' + promoters + '%';
+    divPassive.innerHTML = 'Passive ' + passive + '%';
+    divDetractors.innerHTML = 'Detractores ' + detractors + '%';
+  });
+
+ 
+/*
+
+ console.log(data['LIM']['2016-2']['ratings'][0]); // sprint 1 
+  console.log(data['LIM']['2016-2']['ratings'][1]); // sprint 2 
+  
 console.log(data['LIM']['2016-2']['students']);
 console.log(data['LIM']['2016-2']['students'][0])
 console.log(data['LIM']['2016-2']['students'][0]['name']);
@@ -15,42 +64,6 @@ console.log(data['LIM']['2016-2']['students'][0]['sprints'][0]['number']);
 console.log(data['LIM']['2016-2']['students'][0]['sprints'][0]['score']);
 console.log(data['LIM']['2016-2']['students'][0]['sprints'][0]['score']['tech']);
 console.log(data['LIM']['2016-2']['students'][0]['sprints'][0]['score']['hse']);
-window.addEventListener('load', function(event) {
-  // Ocultando especializacion de las estudiantes
-  optionSpecialization.classList.add('hidden');
-  
-  var generationx = data['LIM']['2016-2'];
-
-  // Cargando las opciones segun generation 
-  for (i = 0;i < generationx['ratings'].length;i++) {
-    var option = document.createElement('option');
-    option.text = 'Sprint ' + generationx['ratings'][i]['sprint'];
-    option.value = generationx['ratings'][i]['sprint'];
-    option.id = generationx['ratings'][i]['sprint'];
-    optionSprint.add(option);
-  }
-  console.log(data);
-  
-  // Evento para elegir el Sprint
-  optionSprint.addEventListener('change', function(event) {
-    if (optionSprint.value >= 5) {
-      optionSpecialization.classList.add('show');
-      optionSpecialization.classList.remove('hidden');
-    } else {
-      optionSpecialization.classList.add('hidden');
-      optionSpecialization.classList.remove('show');
-    }
-  });
-
- 
-/*
-
- console.log(data['LIM']['2016-2']['ratings'][0]); // sprint 1 
-  console.log(data['LIM']['2016-2']['ratings'][1]); // sprint 2 
-  
-
-  console.log(data);
-
 
  
   /*
