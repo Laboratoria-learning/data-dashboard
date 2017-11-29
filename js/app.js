1d040e7a4d7dd2b9b6cfa73e534df4e9c446d68a
adayc <adayc@hotmail.com>
2017-11-29 01:34:15 -0500

    Obteniendo la data

--- js/app.js
+++ js/app.js
@@ -22,4 +22,28 @@ window.addEventListener('load', function(event) {
       optionSpecialization.classList.remove('show');
     }
   });
+  // obteniendo sedes
+
+  var city = Object.keys(data);
+  console.log(city);
+  console.log(data[city[0]]);// arequipa
+
+  var generations = Object.keys(data[city[0]]); 
+  console.log(generations); // generaciones de arequipa
+  console.log(data[city[0]][generations[0]]); // datos de la generacion 2016-2 de arequipa
+
+  var students = Object.keys(data[city[0]][generations[0]]);
+  console.log(data[city[0]][generations[0]][students[0]]);
+  console.log(data);
+  
+ 
+  /*
+  console.log(data['AQP']);
+  console.log(data['AQP']['2016-2'])
+  console.log(data['AQP']['2016-2']['ratings'])
+  console.log(data['AQP']['2016-2']['ratings'][0]) //sprint
+  console.log(data['AQP']['2016-2']['ratings'][0]['jedi']) //puntaje dl jedi
+  console.log(data['AQP']['2016-2']['ratings'][0]['nps']) //nps
+  console.log(data['AQP']['2016-2']['ratings'][0]['nps']['promoters'])
+  console.log(data);*/
 });
