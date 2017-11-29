aa6f962b24bc949dc252d1f37726778415554e34
AnaisOlivares <anaisolivareshilario@gmail.com>
2017-11-29 11:04:32 -0500

    añadiendo javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 console.log(data);
 //Llamar sede
 var aqp = data.AQP;
@@ -18,19 +17,36 @@ register.textContent = students.length;
 var desertStudents = document.getElementById('desertStudents');
 desertStudents.textContent = desert();
 
-function desert () {
-    var arrActive = [];
+function desert() {
+  var arrActive = [];
 
-    for (i = 0; i< students.length; i++) {
-        arrActive.push(students[i].active);
-        }
+  for (i = 0; i < students.length; i++) {
+    arrActive.push(students[i].active);
+  }
 
-        var countDesert = 0;
-        for (j = 0; j < arrActive.length; j++){
-            if (arrActive[j] === false){
-                countDesert++;
-            }
+  var countDesert = 0;
+  for (j = 0; j < arrActive.length; j++) {
+    if (arrActive[j] === false) {
+      countDesert++;
     }
+  }
 
-    return parseInt(countDesert * 100/ students.length) + '%'
+  return parseInt(countDesert * 100 / students.length) + '%'
+}
+
+var submenu = document.getElementById('span');
+  submenu.addEventListener('click', showMenu);
+
+
+function showMenu() {
+  var listMenu = document.getElementsByClassName('item-list')[0];
+  console.log('item-list');
+  if (listMenu.classList.contains('hide')) {
+    listMenu.classList.remove('hide');
+    listMenu.classList.add('show');
+
+  } else {
+    listMenu.classList.remove('show');
+    listMenu.classList.add('hide');
+  }
 }
