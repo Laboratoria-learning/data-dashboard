bcf60100d9ee80dc7fcdf02ea5196c132919d6f9
Yelitza3125 <deyel_99@hotmail.com>
2017-11-29 17:10:02 -0500

    Implementando la etiqueta select y cambiando sus estilos con css

--- js/app.js
+++ js/app.js
@@ -5,14 +5,11 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
 
-window.addEventListener('load', function() {  
-  arrow.addEventListener('click', function showSedes(event) {
-    var arrow = document.getElementById('arrow');  
-    var sedes = document.getElementById('sedes-container');
-    sedes.classList.toggle('hidden');
-    sedes.style.marginTop = '170px';
-    sedes.style.marginLeft = '170px';
-    // ventana.style.left = ((document.body.clientWidth - 350) / 2) + 'px';
+window.addEventListener('load', function() { 
+  var select = document.getElementById('select');
+  select.addEventListener('change', function (event) {
+    
+    });   
   });
 
   teachers.addEventListener('click', function showData(event) {
