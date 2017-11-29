6e3d6a48563b0d5d86a7b7059db5308c9863f089
ambar13 <ambar13_9@hotmail.com>
2017-11-29 08:11:38 -0500

    asd

--- js/app.js
+++ js/app.js
@@ -3,4 +3,37 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+window.onload = function () {
+}
+console.log(data['AQP']['2017-1']['students'].length);
+
+var sede1 = data['AQP']['2017-1']['students']['active'];
+var sede2 = data['AQP']['2016-2']['students'];
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable ([
+    ['Generacion','Porcentaje'],
+    ['2017', sede1],
+    ['2016', sede2]]);
+
+var options = {
+  title: 'Alumnas'
+};
+var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
+ 
+}     
+var retiradas = data['AQP']['2017-1']['students'] 
+// function begin () {
+//   var selectSede= document.getElementsByClassName('sede');
+//   var selectProm= document.getElementsByClassName('promocion')
+//   var selectSprint= document.getElementsByClassName('sprint')
+// }
+
+// var students = ['anais-araya', 'andrea-miranda', 'berenisse-rios', 'caterina-da-silva', 'daniela-sanchez', 'francisca-ojeda', 'katerine-sandoval'];
+
+console.log(data.AQP["2016-2"].students[1].active);
