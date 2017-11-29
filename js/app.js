afd53595fb379c7a76c0a446088340eb0ad0e4f0
NatalyOC <noteroc@hotmail.com>
2017-11-29 14:11:50 -0500

    modificando listas desplegables

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+window.addEventListener('load',function() {
+    var places = document.getElementById('place');
+    var placeSelect = places.value;
+    console.log(placeSelect);
+})
