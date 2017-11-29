e078cba0e162fc73a963288501ca7e4bd2d82044
NelidaSh <nelida.quispetacuri@gmail.com>
2017-11-29 03:28:06 -0500

    añadiendo funciones para desplegar generaciones segun sede y para cambiar pestañas segun el boton

--- js/app.js
+++ js/app.js
@@ -1,26 +1,50 @@
+/*mediante esta función cambiaremos el contenido segun boton*/
+var addAndHide = function (event) {
+  var tabSeleccionado = event.target.dataset.tabSelect;
+  var overview = document.getElementById("overview");
+  var students = document.getElementById("students");
+  var teachers = document.getElementById("teachers");
+
+  if (tabSeleccionado === "tabOverview") {
+    students.style.display = "none";
+    teachers.style.display = "none";
+    overview.style.display = "block";
+  } else if (tabSeleccionado === "tabStudents") {
+    students.style.display = "block";
+    teachers.style.display = "none";
+    overview.style.display = "none";
+  } else if (tabSeleccionado === "tabTeachers") {
+    students.style.display = "none";
+    teachers.style.display = "block";
+    overview.style.display = "none";
+  }
+}
+/*En esta función obtenemos los botones y le agregamos un evento a cada uno mediante un for, el evento se desencadena mediante un click y permitira que aparezca el contenido solicitado*/
+var changeArticle = function () {
+  var btns = document.getElementsByClassName("tab");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", addAndHide);
+  }
+}
+changeArticle();
+
+/*Crearemos una función que nos permita obtener las generaciones por sede*/
 var sedes = document.querySelector('.sedes');
-var sedesOptions = document.getElementsByTagName('option');
-var generations = document.querySelector('.generations')
+var sedesOptions = sedes.getElementsByTagName('option');
+var generations = document.querySelector('.generations');
+var sprints = document.getElementById('sprints');
 
-/*Podemos desde Js agregar atributos a nuestras opciones mediante la siguiente función o tbm en nuestro HTML podemos colocar ese atributo*/
-/* Recorremos las opciones con un for para agregarle un atributo
-for (var i = 0; i < sedesOptions.length; i++) {
-  sedesOptions[i].setAttribute('value', Object.keys(data)[i]);
-  /* Recuerda que data es un objeto, y de esta manera a cada opcion se le agregara el atributo value con su valor correspondiente en la data (Arequipa = AQP, Ciudad de México = CDMX, Lima = LIM y Santiago de Chile = SCL)
-}*/
-
-/*Hacemos uso del evento onchange, que ocurre cuando el valor de un elemento(select) ha sido cambiado.*/
-sedes.addEventListener("change", function(event){
-  addGen(event.target.value);
-});
-
-/* Creamos la función que va a agregar las generaciones que le corresponden al momento de seleccionar una sede*/
-function addGen(sede) {
-  var genSede = Object.Keys(data[sede]); /*almacena las generaciones en un array debido al Object.Keys*/
-  generations.textContent = '';
-  for (var i = 0; i<genSede.length; i++){
+/*el evento onchange ocurre cuando el valor de un elemento <select> ha sido cambiado*/
+sedes.onchange = function (event) {
+  genSelect(event.target.value);/*event.target.value obtiene el valor de la sede*/
+};
+/*Funcion para que al seleccionar una sede cambie las generaciones en base a cada sede*/
+function genSelect(sede) {
+  var datagene = Object.keys(data[sede]);
+  generations.textContent = "";
+  for (var i = 0; i < datagene.length; i++) {
     var option = document.createElement('option');
-    option.textContent = genSede[i];
+    option.textContent= datagene[i];
     generations.appendChild(option);
   }
 }
