fb24a7f5a6f97889bcc83c911750d4643b30f315
cellay <camposellay@gmail.com>
2017-11-29 12:49:07 -0500

    Creando variables globales, modificando html

--- js/app.js
+++ js/app.js
@@ -1,52 +1,60 @@
-console.log(data);
-//Llamar sede
-var aqp = data.AQP;
-console.log(aqp);
-//Llamar generación
-console.log(aqp["2016-2"]);
-//Llamar studiantes
-var students = aqp["2016-2"].students;
-console.log(students);
-//Llamar studiante
-console.log(aqp["2016-2"].students[0]);
-//Llamar estudiante activas
-console.log(aqp["2016-2"].students[0].active);
+var selectSede = document.getElementById('select-sede')
+var selectGen = document.getElementById('select-generation');
+var SEDE = '';
+var GEN = '';
+var students = {};
 
-var register = document.getElementById('register');
-register.textContent = students.length;
-var desertStudents = document.getElementById('desertStudents');
-desertStudents.textContent = desert();
+selectGen.addEventListener('change', generation);
 
-function desert() {
-  var arrActive = [];
+function generation(event) {
+    SEDE = selectSede.value;
+    GEN = selectGen.value;
+    
+    students = data[SEDE][GEN].students;
 
-  for (i = 0; i < students.length; i++) {
-    arrActive.push(students[i].active);
-  }
+    var register = document.getElementById('register');
+    register.textContent = students.length;
 
-  var countDesert = 0;
-  for (j = 0; j < arrActive.length; j++) {
-    if (arrActive[j] === false) {
-      countDesert++;
+    var desertStudents = document.getElementById('desertStudents');
+    desertStudents.textContent = desert();
+    function desert() {
+      var arrActive = [];
+    
+      for (i = 0; i < students.length; i++) {
+        arrActive.push(students[i].active);
+      }
+    
+      var countDesert = 0;
+      for (j = 0; j < arrActive.length; j++) {
+        if (arrActive[j] === false) {
+          countDesert++;
+        }
+      }
+    
+      return parseInt(countDesert * 100 / students.length) + '%'
     }
-  }
+    
+};
 
-  return parseInt(countDesert * 100 / students.length) + '%'
-}
+
+//return students = data[SEDE][GEN].students;
+
+
+//Submenu
 
 var submenu = document.getElementById('span');
-  submenu.addEventListener('click', showMenu);
+submenu.addEventListener('click', showMenu);
 
 
 function showMenu() {
-  var listMenu = document.getElementsByClassName('item-list')[0];
-  console.log('item-list');
-  if (listMenu.classList.contains('hide')) {
-    listMenu.classList.remove('hide');
-    listMenu.classList.add('show');
+var listMenu = document.getElementsByClassName('item-list')[0];
+console.log('item-list');
+if (listMenu.classList.contains('hide')) {
+  listMenu.classList.remove('hide');
+  listMenu.classList.add('show');
 
-  } else {
-    listMenu.classList.remove('show');
-    listMenu.classList.add('hide');
-  }
+} else {
+  listMenu.classList.remove('show');
+  listMenu.classList.add('hide');
+}
 }
