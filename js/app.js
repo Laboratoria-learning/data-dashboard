038c73484161ad0ed0683430432dc9b27cb5fab2
llandreall <andrea.telleria@gmail.com>
2017-11-29 18:26:50 -0500

    Pruebas en JS y consola

--- js/app.js
+++ js/app.js
@@ -1,6 +1,52 @@
-/*
- * Funcionalidad de tu producto
- */
+/* Funciones Maribel */
+// agregar el evento load
+window.addEventListener('load', function() {
+  var selection = document.getElementById('selection');
+  // console.log(selection);
+  var contenidoSede = document.getElementById('contenido-sede');
+  // console.log(contenidoSede);
+  var tabs = document.getElementsByClassName('tab');
+  var contents = document.getElementsByClassName('content');
+
+  function mostrarInfo() {
+  };
+
+  selection.addEventListener('change', function(e) {
+  // console.log(e.target.value);
+    var value = e.target.value;
+    var aux = value.split('-');
+    var sedeName = aux.shift();
+    var generation = aux.join('-');
+
+    var generationData = data[sedeName][generation];
+    var totalStudents = generationData.students.length;
+
+    console.log(generationData);
+    });
+  });
+
+  // agregar el evento click a todos los tabs
+  for (var i = 0; i < tabs.length; i++) {
+    // y dentro del click para cada tab
+    tabs[i].addEventListener('click', function(event) {
+      for (var j = 0; j < tabs.length; j++) {
+        // quitar la clase active a todos los tabs
+        tabs[j].classList.remove('active');
+      }
+
+      for (var k = 0; k < contents.length; k++) {
+        // quitar la clase active a todos los contents
+        contents[k].classList.remove('active');
+      }
+
+      // agregar la clase active solo a este tab que se le dio click
+      event.target.classList.add('active');
+      // agregar la clase active solo al content correspondiente (data-content
+      contents[event.target.dataset.content].classList.add('active');
+    });
+  }
+  /* Fin */
+});
 
 // Pruebas en consola
 var lim20162 = data.LIM['2016-2'];
@@ -19,6 +65,11 @@ var lim20162students = Object.values(lim20162)[0];
 var lim20171students = Object.values(lim20171)[0];
 var lim20172students = Object.values(lim20172)[0];
 
-console.log(data);
 console.log(lim20172students);
 */
+
+function showProfiles () {
+
+}
+
+lim20162.students
