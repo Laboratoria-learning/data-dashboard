e0b6dbe5b3ae6347c3cf96df9264d3ec0ba97959
Victoria <vtr.lung@outlook.es>
2017-11-29 03:01:57 -0500

    editando y agregando funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function(event){
-console.log("hola");
+    var displayIcon = document.getElementById('fa-sort-desc')
+  ;
 })
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
