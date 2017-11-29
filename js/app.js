ffa710dcc84c575401463d5f8f561ee1d3960619
lizncq <lizxd10@gmail.com>
2017-11-29 18:27:27 -0500

    actualizacion funcionalidad

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ var allSedes = document.querySelectorAll('.sede');
 console.log(allSedes);
 
 // sedes y prefijos
-var arequipa = 'AQP' ;
-var mexico = 'CDMX' ;
-var lima = 'LIM' ;
-var chile = 'SCL' ;
+var arequipa = 'AQP';
+var mexico = 'CDMX';
+var lima = 'LIM';
+var chile = 'SCL';
 
 // variable que almacena  el texto junto de sede y generacion
 var sedeGeneration = document.getElementById('sede_generation');
@@ -20,29 +20,6 @@ var arequipaGenerations = document.getElementById('arequipa_generations');
 var mexicoGenerations = document.getElementById('mexico_generations');
 var limaGenerations = document.getElementById('lima_generations');
 var chileGenerations = document.getElementById('chile_generations');
-
-
-sedeGeneration.addEventListener('click', showListSedes);
-function showListSedes(event) {
-
-};
-// data.AQP["2016-2"].students.length --> me da el total de estudiantes de arequipa 2016-2
-
-// total promoters
-var promoters = 0;
-for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
-  var promoters = promoters + data.AQP['2016-2'].ratings[i].nps.promoters;
-};
-// total detractors
-var detractors = 0;
-for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
-  var detractors = detractors + data.AQP['2016-2'].ratings[i].nps.detractors;
-};
-
-// total nps
-var nps = promoters - detractors;
-console.log(nps);
-
 
 // variables que alamcenan cada sede del html en el dom
 var sede0 = document.getElementById('sede_0');
@@ -74,3 +51,66 @@ for (var i = 0; i < allSedes.length; i++) {
 
   }
 }
+
+console.log(data);
+/* var promoters = 0;
+var detractors = 0;
+
+function nps(sede, generacion) {
+  for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
+    var promoters = promoters + data.AQP['2016-2'].ratings[i].nps.promoters;
+  };
+  var detractors = 0;
+  for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
+    var detractors = detractors + data.AQP['2016-2'].ratings[i].nps.detractors;
+  };
+  return promoters - detractors;
+}
+
+nps(sede, generation);*/
+
+// puntuacion promedio de los profes
+var promTeacher = 0;
+for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
+  var promTeacher = promTeacher + data.AQP['2016-2'].ratings[i].teacher;
+};
+console.log(promTeacher);
+
+// puntuacion promedio de los jedis
+var promJedi = 0;
+for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
+  var promJedi = promJedi + data.AQP['2016-2'].ratings[i].jedi;
+};
+console.log(promJedi);
+
+// porcentaje de estudiantes satisfechas
+var satisfiedStudents = 0;
+for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
+  var satisfiedStudents = satisfiedStudents + data.AQP['2016-2'].ratings[i].student.cumple;
+};
+console.log(satisfiedStudents);
+
+// cantidad y el porcentaje que representa el total de estudiantes que superan la meta de puntos tecnicos en promedio y por sprint
+var studentsSuperan = 0;
+for (i = 0; i < data.AQP['2016-2'].students.length; i++) {
+  if (data.AQP['2016-2'].students[i].sprints.score.tech > ((70 / 100) * 1800)) {
+    studentsSuperan ++;
+  } else {
+    studentsSuperan = studentsSuperan;
+  }
+};
+console.log(studentsSuperan);
+/* total promoters
+var promoters = 0;
+for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
+  var promoters = promoters + data.AQP['2016-2'].ratings[i].nps.promoters;
+};
+// total detractors
+var detractors = 0;
+for (i = 0; i < data.AQP['2016-2'].ratings.length; i++) {
+  var detractors = detractors + data.AQP['2016-2'].ratings[i].nps.detractors;
+};
+
+// total nps
+var nps = promoters - detractors;
+console.log(nps);*/
