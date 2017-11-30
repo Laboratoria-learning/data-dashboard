6a24bfe10f7bc594cdde1b7888dab72e7492b332
Danielapariona <daniela.parionac@gmail.com>
2017-11-30 11:57:27 -0500

    ratings

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ console.log(data);
 var menuPrincipal = document.getElementById('menu-principal');
 var content = document.getElementById('content-menu');
 var studentsCurrentlyEnrolled = document.getElementById('students-currently');
+var studentSatisfaction = document.getElementById('student-satisfaction');
+var teacherRating = document.getElementById('teacher-rating');
+var jediRating = document.getElementById('jedi-rating');
+
 
 function viewMenu() {
   content.classList.toggle('hidden');
@@ -19,6 +23,9 @@ function valueOption(event) {
   var generation = event.target.value;
   var place = event.target.getAttribute('data-place');
   studentsCurrentlyEnrolled.textContent = totalStudents(place, generation);
+  studentSatisfaction.textContent = studentSatisfactionTotal(place, generation);
+  teacherRating.textContent = rating(place, generation, 'teacher');
+  jediRating.textContent = rating(place, generation, 'jedi');
 };
 
 function totalStudents(place, generation) {
@@ -36,3 +43,23 @@ function totalStudents(place, generation) {
   return totalStudents;
 }
 
+function studentSatisfactionTotal(place, generation) {
+  var totalPromoters = 0;
+  var nps = data[place][generation]['ratings'];
+  for (var i = 0; i < nps.length; i++) {
+    var current = nps[i]['nps']['promoters']
+    totalPromoters = totalPromoters + current;
+  }
+  var averagePromoters = totalPromoters / nps.length;
+  return averagePromoters;
+}
+
+function rating(place, generation, nameRating) {
+  var totalRating = 0;
+  var nps = data[place][generation]['ratings'];
+  for (var i = 0; i < nps.length; i++) {
+    totalRating = totalRating + nps[i][nameRating];
+  }
+  var rating = (totalRating / nps.length).toFixed(1);
+  return rating;
+}
