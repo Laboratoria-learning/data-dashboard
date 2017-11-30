1ff2380bacd8b0044e489d5971e1a761471af6b8
VaneQiuta777 <vanessa.colsan@gmail.com>
2017-11-30 00:49:20 -0500

    añadiendo evento tab

--- js/app.js
+++ js/app.js
@@ -35,3 +35,42 @@ window.addEventListener('load', function(event) {
 });
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+/*event tab*/
+var mostrarOcultar = function(e) {
+  var tabSeleccionado = e.target.dataset.tabSeleccionado;
+  var overview = document.getElementById('overview');
+  var students = document.getElementById('students');
+  var teachers = document.getElementById('teachers');
+  if (tabSeleccionado === 'tabOverview') {
+    console.log('mostrar overview');
+    /* hide students y teachers*/
+    students.style.display = 'none';
+    teachers.style.display = 'none';
+    /* show overview*/
+    overview.style.display = 'block';
+  } else if (tabSeleccionado === 'tabStudents') {
+    console.log('mostrar students');
+    /* hide overview y teachers*/
+    overview.style.display = 'none';
+    teachers.style.display = 'none';
+    /* show overview*/
+    students.style.display = 'block';
+  } else if (tabSeleccionado === 'tabTeachers') {
+    console.log('mostrar teachers');
+    /* hide overview y students*/
+    overview.style.display = 'none';
+    students.style.display = 'none';
+    /* show teachers*/
+    teachers.style.display = 'block';
+  }
+};
+var cargarPagina = function() {
+  students.style.display = 'none';
+  teachers.style.display = 'none';
+  var elementosTab = document.getElementsByClassName('tab');
+  for (var i = 0; i < elementosTab.length; i++) {
+    elementosTab[i].addEventListener('click', mostrarOcultar);
+  }
+};    
+cargarPagina();
