ae9b4a55a2ebe0b053ab1a71523fc01a84fac3c6
jessia2011 <jessicamezarojas2011@gmail.com>
2017-11-30 03:12:01 -0500

    grafico1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
-/*
- * Funcionalidad de tu producto
- */
-
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+// var dropout = Math.round(inactive * 100 / students.length);
+
+
+function studentSedes(sede, generation) {
+  var active = 0;
+  var inactive = 0;
+  var students = data[sede][generation]['students'];
+  var array = [];
+  for (var i = 0; i < students.length; i++) {
+    students[i].active ? active++ : inactive++;
+  }
+  array.push(active);
+  array.push(inactive);
+  return array;
+}
+//studentSedes('AQP', '2016-2');
---
+++ js/graficas.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function() {
+// cambiar el nombre de sede, seleccionando la generacion
+    var selectSprint = document.getElementById('select-sprint');
+    selectSprint.addEventListener('change', function() {
+    var site = selectSprint.options[selectSprint.selectedIndex].getAttribute('data-site');
+    var sede = selectSprint.options[selectSprint.selectedIndex].getAttribute('data-sede');
+    var generation = selectSprint.value;  
+    document.getElementById('site').textContent = site;
+
+    var inscritas = document.querySelector('.total-enrollment');
+    
+
+
+
+    // GOOGLE-CHARTS
+    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(drawChartEnrollment);
+    // 1era gráfica de alumnas inscritas
+    function drawChartEnrollment() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['active', studentSedes(sede, generation)[0]],
+        ['inactive', studentSedes(sede, generation)[1]],
+      ]);
+      var options = {'title': 'Students',
+        'width': 250,
+        'height': 150,
+        'is3D': false
+      };
+      var chart = new google.visualization.PieChart(document.getElementById('enrollment-graph'));
+      chart.draw(data, options);
+    }
+  });
+});
