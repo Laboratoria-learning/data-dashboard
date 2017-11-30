964b55b4996faa3f24ca27a0821d51df1814c0ce
Danielapariona <daniela.parionac@gmail.com>
2017-11-30 09:31:06 -0500

    Change structure selection

--- js/app.js
+++ js/app.js
@@ -1,16 +1,11 @@
-/*
- * Funcionalidad de tu producto
- */
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
 
 var menuPrincipal = document.getElementById('menu-principal');
 var content = document.getElementById('content-menu');
+var studentsCurrentlyEnrolled = document.getElementById('students-currently');
 
 function viewMenu() {
   content.classList.toggle('hidden');
-}
-
+};
 
 menuPrincipal.addEventListener('click', viewMenu);
