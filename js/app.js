8e5610bf8d25c76ccebd436dca248d6e66b30e74
AlejandraCP <alejandracp710@gmail.com>
2017-11-30 12:58:46 -0500

    Mejorando indentación

--- js/app.js
+++ js/app.js
@@ -106,27 +106,26 @@ function inscription(event) {
   npsElement.textContent =  nps();
   npsElement.setAttribute('class', 'number');
 
-    // función que devuelve la cantidad de estudiantes que superan la meta de puntos en promedio de todos los sprints
+  // función que devuelve la cantidad de estudiantes que superan la meta de puntos en promedio de todos los sprints
 
-    var studentsActive = 0;
-    var count = 0;
-    function achievement(){
-      students = data[sede][gen].students;
-      for(var i = 0; i < students.length; i++){
-        if(students[i].active){
-          studentsActive++;
-          for (var j = 0; j < students[i]['sprints'].length; j++){
-            var sum = students[i]['sprints'][j]['score']['tech'] + students[i]['sprints'][j]['score']['hse'];
-            // console.log(students[i]['sprints'][j]['score']['tech']);
-            // console.log(students[i]['sprints'][j]['score']['hse']);
-            if(sum > 2100 ){
-              count++;
-            }
+  var studentsActive = 0;
+  var count = 0;
+  function achievement() {
+    students = data[sede][gen].students;
+    for (var i = 0; i < students.length; i++) {
+      if (students[i].active) {
+        studentsActive++;
+        for (var j = 0; j < students[i]['sprints'].length; j++) {
+          var sum = students[i]['sprints'][j]['score']['tech'] + students[i]['sprints'][j]['score']['hse'];
+          if (sum > 2100) {
+            count++;
           }
         }
       }
-      console.log(count);
     }
 
-    achievement();
-  };
+    console.log(count);
+  }
+
+  achievement();
+};
