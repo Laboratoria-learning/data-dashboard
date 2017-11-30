ad29f8a854c0e2b285208f3fbfcd754a5e190132
rocioqm <rocio.quilla@aiesec.net>
2017-11-30 02:12:51 -0500

    funcion click para los dos divs del enrollment segun sede y generacion

--- js/eventos.js
+++ js/eventos.js
@@ -24,6 +24,13 @@ var enrollmentDiv2 = document.getElementById('enrollmentDiv2');
 var aqp20162 = document.getElementById('AQP20162');
 var aqp20171 = document.getElementById('AQP20171');
 var cdmx20171 = document.getElementById('CDMX20171');
+var cdmx20172 = document.getElementById('CDMX20172');
+var lim20162 = document.getElementById('LIM20162');
+var lim20171 = document.getElementById('LIM20171');
+var lim20172 = document.getElementById('LIM20172');
+var scl20162 = document.getElementById('SCL20162');
+var scl20171 = document.getElementById('SCL20171');
+var scl20172 = document.getElementById('SCL20172');
 // funcionalidad Generaciones en el menu desplegable
 function faqp20162() {
   enrollmentDiv1.textContent = numEstudentsAQP20162;
@@ -37,7 +44,42 @@ function fcdmx20171() {
   enrollmentDiv1.textContent = numEstudentsCDMX20171;
   enrollmentDiv2.textContent = percDesertedCDMX20171;
 }
+function fcdmx20172() {
+  enrollmentDiv1.textContent = numEstudentsCDMX20172;
+  enrollmentDiv2.textContent = percDesertedCDMX20172;
+}
+function flim20162() {
+  enrollmentDiv1.textContent = numEstudentsLIM20162;
+  enrollmentDiv2.textContent = percDesertedLIM20162;
+}
+function flim20171() {
+  enrollmentDiv1.textContent = numEstudentsLIM20171;
+  enrollmentDiv2.textContent = percDesertedLIM20171;
+}
+function flim20172() {
+  enrollmentDiv1.textContent = numEstudentsLIM20172;
+  enrollmentDiv2.textContent = percDesertedLIM20172;
+}
+function fscl20162() {
+  enrollmentDiv1.textContent = numEstudentsSCL20162;
+  enrollmentDiv2.textContent = percDesertedSCL20162;
+}
+function fscl20171() {
+  enrollmentDiv1.textContent = numEstudentsSCL20171;
+  enrollmentDiv2.textContent = percDesertedSCL20171;
+}
+function fscl20172() {
+  enrollmentDiv1.textContent = numEstudentsSCL20172;
+  enrollmentDiv2.textContent = percDesertedSCL20172;
+}
 // eventos Generaciones en el menu desplegable
 aqp20162 .addEventListener('click', faqp20162);
 aqp20171 .addEventListener('click', faqp20171);
 cdmx20171 .addEventListener('click', fcdmx20171);
+cdmx20172 .addEventListener('click', fcdmx20172);
+lim20162 .addEventListener('click', flim20162);
+lim20171 .addEventListener('click', flim20171);
+lim20172 .addEventListener('click', flim20172);
+scl20162 .addEventListener('click', fscl20162);
+scl20171 .addEventListener('click', fscl20171);
+scl20172 .addEventListener('click', fscl20172);
