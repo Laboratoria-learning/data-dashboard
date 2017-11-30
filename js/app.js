3c04fa8885f2ff04983b31abf4e33eb5e1f41d9b
Angelicavc21 <angelik_m21@hotmal.com>
2017-11-30 08:51:21 -0500

    subiendo avance

--- js/app.js
+++ js/app.js
@@ -3,4 +3,20 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// console.log(data);
+
+
+function mostrar() {
+    document.getElementById("sidebar").style.width = "300px";
+    document.getElementById("content").style.marginLeft = "300px";
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "inline";
+  }
+  
+  function ocultar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+    document.getElementById("open").style.display = "inline";
+    document.getElementById("close").style.display = "none";
+  }
+  
