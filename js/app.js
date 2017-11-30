cb12520b0b041e8fc31fc22d8120cc547130e2c7
melissayauri <yaurimelissa@gmail.com>
2017-11-30 04:31:08 -0500

    mejorando el filtro para el total de alumnas por sede y generacion

--- js/app.js
+++ js/app.js
@@ -1,67 +1,53 @@
-
 window.addEventListener('load', function() {
+  /* funcion para mostrar las alumas por sede-generacion*/
   var selection = document.getElementById('selection');
-  var container = document.getElementById('container');
-  selection.addEventListener('change', showsGeneration);
-  var sedeArequipa = data.AQP;
-  var count = 0;
+  var containerStudents = document.getElementById('total-students');
+  selection.addEventListener('change', showGenerations);
+  function showGenerations() {
+    /* seleccionando cada option de lista despegable*/
+    var options = selection.value;
+    /* se convierte a un array para obtener la data de sede y generaciones*/
+    var array = options.split('-');/* [lima,2017,2]*/
+    console.log(array);
+    /* obteniendo la generacion*/
+    var array1 = array.splice(1, 2);
+    console.log(array1);
+    /* uniendo los dos elementos de la generacion*/
+    var generation = array1[0] + '-' + array1[1];
+    /* obteniendo la sede*/
+    var site = array.splice(0, 1);
+    console.log(site);
+    /* obteniendo datos por sede-generacion*/
+    var siteGeneration = data[site][generation];
+    console.log(siteGeneration);
+    /* creando un texto*/
+    var totalStudents = siteGeneration.students.length;
+    containerStudents.innerHTML = '# de studiantes inscritos' + '</br>' + totalStudents;
 
-  function showsGeneration(event) {
-    /* sede arequipa*/
-    if (selection.value === 'AQP-2016-2') {
-      var generationAqp1 = data.AQP['2016-2']['students'];
-      for (var i = 0; i < generationAqp1.length; i++) {
-        if (generationAqp1[i].active === true) {
-          count++;
-          container.innerHTML = count;
-        }
-      }
-    }
-    if (selection.value === 'AQP-2017-1') {
-      var generationAqp2 = data.AQP['2017-1']['students'];
-      for (var i = 0; i < generationAqp2.length; i++) {
-        if (generationAqp2[i].active === true) {
-          count++;
-          container.innerHTML = count;
-        }
-      }
-    }
-    /* por cada generacion de mexico*/
-    if (selection.value === 'CDMX-2017-1') {
-      var generationCdmx1 = data.CDMX['2017-1']['students'];
-      for (var i = 0; i < generationCdmx1.length; i++) {
-        if (generationCdmx1[i].active === true) {
-          count++;
-          container.innerHTML = count;
-        }
-      }
-    }
-    if (selection.value === 'CDMX-2017-2') {
-      var generationCdmx2 = data.CDMX['2017-2']['students'];
-      for (var i = 0; i < generationCdmx2.length; i++) {
-        if (generationCdmx2[i].active === true) {
-          count++;
-          container.innerHTML = count;
-        }
-      }
-    }
-    if (selection.value === 'LIM-2016-2') {
-      var generationLim1 = data.LIM['2016-2']['students'];
-      for (var i = 0; i < generationLim1.length; i++) {
-        if (generationLim1[i].active === true) {
-          count++;
-          container.innerHTML = count;
-        }
-      }
-    }
-    if (selection.value === 'LIM-2017-1') {
-      var generationLim2 = data.LIM['2017-1']['students'];
-      for (var i = 0; i < generationLim2.length; i++) {
-        if (generationLim2[i].active === true) {
-          count++;
-          container.innerHTML = count;
-        }
-      }
-    }
   }
- });
+
+
+
+
+
+
+
+
+  /*menu derecho*/
+var bars = document.getElementById('bars');
+bars.addEventListener('click', showMenu);
+function showMenu() {
+var options = document.getElementById('options-menu');
+if(options.classList.contains('disabled-menu')){
+  options.classList.remove('disabled-menu');
+  options.classList.add('enabled-menu');
+}
+else{
+  options.classList.remove('enabled-menu');
+  options.classList.add('disabled-menu');
+}
+}
+bars.addEventListener('click', showMenu);
+// Puedes hacer uso de la base de datos a través de la variable `data`
+console.log(data);
+});
