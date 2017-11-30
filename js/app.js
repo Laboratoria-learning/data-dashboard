51ce41fc56197e204ea15e6537e27f37b5146ba8
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2017-11-30 08:58:30 -0500

    Añadiendo for para La puntuación promedio de l@s jedi y profesores en el overview

--- js/app.js
+++ js/app.js
@@ -46,6 +46,37 @@ overviewBtn.addEventListener('click', function(){
     var y2 = x2.nps.promoters;
     var z2 = x2.nps.passive;
     var w2 = x2.nps.detractors;
+    var sumXZW = (y2 + z2 + w2)
+    var promoters = y2/sumXZW*100
+    var detractors = w2/sumXZW*100
+    var nps = promoters - detractors
+    var p3 = document.createElement('p')
+    p3.textContent = 'NPS' + ':' + nps;
+    div2.appendChild(p3)
+  /*  [Promoters] = [Respuestas 9 o 10] / [Total respuestas] * 100
+[Passive] = [Respuestas 7 u 8] / [Total respuestas] * 100
+[Detractors] = [Respuestas entre 1 y 6] / [Total respuestas] * 100
+[NPS] = [Promoters] - [Detractors] */
+  }
+
+  //for para La puntuación promedio de l@s profesores.
+  for (var i = 0; i < intento2.length; i++){
+
+    var x3 = intento2[i];
+    var y3 = x3.teacher;
+    var p4 = document.createElement('p')
+    p4.textContent = 'PUNTACIÓN PROMEDIO PROFESORES' + ':' + y3
+    div2.appendChild(p4)
+  }
+  //for para La puntuación promedio de l@s jedi.
+  for (var i = 0; i < intento2.length; i++){
+
+    var x4 = intento2[i];
+    var y4 = x4.jedi;
+    var p5 = document.createElement('p')
+    p5.textContent = 'PUNTACIÓN PROMEDIO JEDI' + ':' + y4
+    div2.appendChild(p5)
   }
   });
 });
+console.log (data)
