622d80074a66a28b91610c0237d638587f12ebe9
Cintia Tahirih <tahysluana@gmail.com>
2017-11-30 01:37:09 -0500

    selecciona estudiantes de Lima

--- js/app.js
+++ js/app.js
@@ -1,73 +1,45 @@
-window.addEventListener('load', function() {
-  var listMenu = document.querySelectorAll('.general-menu');
-  var studentsContainer = document.getElementById('coders-container');
-  var selectGenerationLima = document.getElementsByClassName('generacion')[0];
-  var selectGenerationAqp = document.getElementsByClassName('generacion')[1];
-  var selectGenerationSch = document.getElementsByClassName('generacion')[2];
-  var selectGenerationDf = document.getElementsByClassName('generacion')[3];
-  var selectGenerationG = document.getElementsByClassName('generacion')[4];
-  var infoCoder = document.getElementsByClassName('student');
-  var subInfoContainer = null;
+var studentsContainer = document.getElementById('coders-container');
+var selectGenerationLima = document.getElementsByClassName('generacion')[0];
+var selectGenerationAqp = document.getElementsByClassName('generacion')[1];
+var selectGenerationSch = document.getElementsByClassName('generacion')[2];
+var selectGenerationDf = document.getElementsByClassName('generacion')[3];
+var selectGenerationG = document.getElementsByClassName('generacion')[4];
+var subImagesContainer = null;
 
-  for (var i = 0; i < listMenu.length; i++) {
-    listMenu[i].addEventListener('click', showMenu);
+selectGenerationLima.addEventListener('change', selectionGeneration);
+selectGenerationAqp.addEventListener('change', selectionGeneration);
+selectGenerationSch.addEventListener('change', selectionGeneration);
+selectGenerationDf.addEventListener('change', selectionGeneration);
+selectGenerationG.addEventListener('change', selectionGeneration);
+
+function selectionGeneration(event) {
+  var filtro = event.target.value;
+  cargarCoders(filtro.substr(0, 3), filtro.substr(3, 6));
+}
+
+function cargarCoders(ciudad, periodo) {
+  for (var i = 0 ; i < data[ciudad][periodo].students.length; i++) {
+    var estudiante = document.createElement('div');
+    var infStuden = document.createElement('p');
+    var photoStudent = document.createElement('img');
+
+    photoStudent.classList.add('student-img');
+    estudiante.setAttribute('class', 'student-inf');
+
+    photoStudent.src = data[ciudad][periodo].students[i].photo;
+    infStuden.textContent = data[ciudad][periodo].students[i].name;
+    photoStudent.textContent = data[ciudad][periodo].students[i].photo;
+
+    estudiante.appendChild(infStuden);
+    estudiante.appendChild(photoStudent);
+    studentsContainer.appendChild(estudiante);
   }
+}
 
-  function showMenu(event) {
-    console.log(event.target);
-  }
+function techSkills() {
+  var techPoints;
+}
 
-  // constantes
-  var FIRST15 = 'first-15';
-  var SECOND15 = 'second-15';
-  var FIRST16 = 'first-16';
-  var SECOND16 = 'second-16';
-  var FIRST17 = 'first-17';
-  var SECOND17 = 'second-17';
+function lifeSkills() {
 
-  selectGenerationLima.addEventListener('change', selectionGeneration);
-  selectGenerationAqp.addEventListener('change', selectionGeneration);
-  selectGenerationSch.addEventListener('change', selectionGeneration);
-  selectGenerationDf.addEventListener('change', selectionGeneration);
-  selectGenerationG.addEventListener('change', selectionGeneration);
-
-  showAll();
-
-  function selectionGeneration(event) {
-    for (var j = 0; j < data.LIM["2017-2"].students.length; j++) {
-        var newCoder = document.createElement('div');
-        newCoder.setAttribute('class', 'student');
-        newCoder.textContent = data.LIM["2017-2"].students[j].name;
-        infoCoder = studentsContainer.appendChild(newCoder);
-      }
-    switch (true) {
-    case event.target.value === '':
-      showAll();
-      break;
-    case event.target.value === 'first-15':
-      showAll();
-      break;
-    case event.target.value === 'second-15':
-      showAll();
-      break;
-    case event.target.value === 'first-16':
-      showAll();
-      break;
-    case event.target.value === 'second-16':
-      addImages(infoCoder, SECOND16);
-      break;
-    case event.target.value === 'first-17':
-      addImages(infoCoder, FIRST15);
-      break;
-    case event.target.value === 'second-17':
-      addImages(infoCoder, SECOND15);
-      break;
-    }
-    console.log('hola');
-  }
-  function showAll() {
-console.log('hola2');
-  }
-
-
-});
+}
