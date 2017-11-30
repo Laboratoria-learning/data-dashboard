2733248ec786bc5f4774a576373d9a5771a60c02
alejandraHoces <hocesalejandra@gmail.com>
2017-11-30 09:08:41 -0500

    Añadiendo estilos js

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+/*var selectData = document.getElementById('select-data');
+var content = document.getElementById('number').innher*/
+
+
+
+/*Opcion que accede a el numero de alumnas de AQP*/
+var objetoAQP = data.AQP
+var ageGeneracion = objetoAQP['2016-2'];
+var student = ageGeneracion.students
+console.log(student.length);
