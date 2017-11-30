85353dbaeca38b6961aa5265a2eba6ff9f446403
pameam <pamela.anayhuaman@gmail.com>
2017-11-30 11:38:44 -0500

    añadiendo nuevos estilos y funcionalidad a overview

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 /*
  * Funcionalidad de tu producto
  */
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
-
+// para habilitar y desabilitar el boton
 var boton = document.getElementById('btn');
 
 function showMenu() {
@@ -17,7 +15,7 @@ function showMenu() {
     menu.classList.add('disabled-menu');
   }
 }
-
+// Para que el boton menu se habilite cuando se haga click
 boton.addEventListener('click', showMenu);
 
 var mostrarOcultar = function(event) {
@@ -57,12 +55,37 @@ var cargarPagina = function() {
 
 cargarPagina();
 //
-// Puedes hacer uso de la base de datos a través de la variable `data`
-var sitie = Object.keys(data);
+// Haciendo uso de la variable data
+/* var sitie = Object.keys(data);// [AQP,CDMX,LIM,SCL]
 for (var i = 0; i < sitie.length; i++) {
   sitie[i];
 }
-
+sitie[0] = document.getElementById('aqp');
+sitie[1] = document.getElementById('cdmx');
+sitie[2] = document.getElementById('lim');
+sitie[3] = document.getElementById('cl');
+// var arrsitie = data[sitie[0]];
+// console.log(arrsitie);
+function showPromo(event) {
+  switch (true) {
+  case sitie[0]:
+    var arrsitie = data[sitie[0]];
+    var arrGeneration = Object.keys(data[sitie[0]]);
+    for (var i = 0; i < arrGeneration.length; i++) {
+    }
+    arrGeneration[0] = document.getElementById('2016-II');
+    arrGeneration[1] = document.getElementById('2017-I');
+    break;
+  case sitie[1]:
+    break;
+  case sitie[2]:
+    break;
+  case sitie[3]:
+    break;
+  default:
+  }
+}*/
+// Para Arequipa generacion 2016-2
 selectionLocated = document.getElementById('selection-located');
 selectionLocated.addEventListener('change', showPromo);
 var studentaqp = data.AQP['2016-2'].students.length;
@@ -75,22 +98,53 @@ for (var i = 0; i < studentaqp; i++) {
     coninactivo ++;
   }
 }
+// Para Arequipa generacion 2017-1
+var studentaqp = data.AQP['2017-1'].students.length;
+contactivo71 = 0;
+coninactivo71 = 0;
+for (var i = 0; i < studentaqp; i++) {
+  if ((data.AQP['2017-1'].students[i]['active']) === true) {
+    contactivo71 ++;
+  } else {
+    coninactivo71 ++;
+  }
+}
 // desersion en porcentaje
 var desertion = Math.floor((coninactivo * 100) / (coninactivo + contactivo)) + '%';
-function showPromo(event) { // si no se 'e' o 'event' como paramentro dentro de la funcion se debe usar this pero aún no se lo que es pe'
-//  console.log(event.target.value); //target es el que ejecutra el evento o el que dispara el evento-- esto con console.log sirve para comprobar que esta ocurriendo lo que queremos en la consola
+var desertion71 = Math.floor((coninactivo71 * 100) / (coninactivo71 + contactivo71)) + '%';
+
+// ver el total que pasaron el 70% (del total de puntos de hse y tecnico)
+var point = 0;
+var tScore = 0;
+for (var i = 0; i < data.AQP['2016-2'].students.length ; i++) {
+  for (var j = 0; j < data.AQP['2016-2'].students[i].sprints.length ; j++) {
+    var scoreHse = data.AQP['2016-2'].students[i].sprints[j].score.hse;
+    // console.log(scoreHse);
+    var scoreTech = data.AQP['2016-2'].students[i].sprints[j].score.tech;
+    // console.log(scoreTech)
+    var sumPointSprint = scoreHse + scoreTech;
+    // console.log(sumPointSprint)
+    tScore += sumPointSprint;
+    if (tScore >= 8400) {
+      point += 1;
+    }
+  }
+  tScore = 0;
+}
+
+function showPromo(event) { //  si no se 'e' o 'event' como paramentro dentro de la funcion se debe usar this pero aún no se lo que es pe'
   switch (true) {
   case event.target.value === 'lim':
-
-
     break;
-  case event.target.value === 'aqp':
+  case event.target.value === '2016-II':
     document.getElementById('container-enrolled').innerHTML = contactivo;
     document.getElementById('container-deserte').innerHTML = desertion;
+    document.getElementById('point').innerHTML = point;
     break;
-  case event.target.value === 'cl':
-
+  case event.target.value === '2017-I':
+    document.getElementById('container-enrolled').innerHTML = contactivo71;
+    document.getElementById('container-deserte').innerHTML = desertion71;
     break;
   default:
   }
-} 
+}
