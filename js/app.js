a278dd0152007dd976413a72335e7ec0632a2883
Nefelijm <sistemasnik20@gmail.com>
2017-11-30 01:58:40 -0500

    modifcando js

--- js/app.js
+++ js/app.js
@@ -9,13 +9,12 @@ var contentOverview = document.getElementById('contentOverview');// lo que quier
 var students = document.getElementById('tab-students');// lo que quiero hacer click
 var contentStudents = document.getElementById('contentStudents');// lo que quiero que aparezca
 
-function estudentcont () {
+function estudentcont() {
   contentOverview.style.display = 'none';
   contentStudents.style.display = 'block';
 }
- function overcont () {
+function overcont() {
   contentOverview.style.display = 'block';
-
 };
 
 function create() {
@@ -23,69 +22,61 @@ function create() {
   document.getElementById('text').innerHTML = sede ;
 
 
-switch(sede) {
+  switch (sede) {
   case 'LIMA2016-II':
-  var estudiantelim1 = (data["LIM"]["2016-2"]["students"].length);
-  console.log( document.getElementById('one').innerHTML =  estudiantelim1  + " # estudents currentl enrrolled" );  
-      break;
+    var estudiantelim1 = (data['LIM']['2016-2']['students'].length);
+    document.getElementById('one').innerHTML = estudiantelim1 + ' # estudents currentl enrrolled' ;  
+    break;
   case 'LIMA2017-I':
-  var  estudiantelim2 = (data["LIM"]["2017-1"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim2);
-      break;
+    var estudiantelim2 = (data['LIM']['2017-1']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim2);
+    break;
   case 'LIMA2017-II':
-  var  estudiantelim3 = (data["LIM"]["2017-1"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim3);
-      break;
+    var estudiantelim3 = (data['LIM']['2017-1']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim3);
+    break;
   case 'AREQUIPA2016-II':
-  var  estudiantelim4 = (data["AQP"]["2016-2"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim4);
-       break;
+    var estudiantelim4 = (data['AQP']['2016-2']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim4);
+    break;
   case 'AREQUIPA2017-I':
-  var  estudiantelim5 = (data["AQP"]["2017-1"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim5);
-      break;
+    var estudiantelim5 = (data['AQP']['2017-1']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim5);
+    break;
   case 'SantiagodeChile2016-II':
-  var  estudiantelim6 = (data["SCL"]["2017-1"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim6);
-      break;
+    var estudiantelim6 = (data['SCL']['2017-1']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim6);
+    break;
   case 'SantiagodeChile2017-I':
-  var  estudiantelim7 = (data["SCL"]["2017-1"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim7);
-      break;
+    var estudiantelim7 = (data['SCL']['2017-1']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim7);
+    break;
   case 'SantiagodeChile2017-II':
-  var  estudiantelim8 = (data["SCL"]["2017-2"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim8);
-      break;
+    var estudiantelim8 = (data['SCL']['2017-2']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim8);
+    break;
   case 'CiudaddeMexico2017-I':
-  var  estudiantelim9 = (data["CDMX"]["2017-1"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim9);
-     break;
+    var estudiantelim9 = (data['CDMX']['2017-1']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim9);
+    break;
   case 'CiudaddeMexico2017-II':
-  var  estudiantelim10 = (data["CDMX"]["2017-2"]["students"].length);
-  console.log( document.getElementById('one').innerHTML = estudiantelim10);
-  porcentaje();
+    var estudiantelim10 = (data['CDMX']['2017-2']['students'].length);
+    console.log(document.getElementById('one').innerHTML = estudiantelim10);
+    porcentaje();
     break;
   default:
-      alert('seleccione una opcion');
-}
+    alert('seleccione una opcion');
+  }
 
 
-
- function porcentaje(){
-
-      alert('gb');
-}
-
-
-
+  function porcentaje() {
+    alert('gb');
+  }
 }
 
 function select() {
-
   sede.addEventListener('change', create);
 
   students.addEventListener('click', estudentcont);
   overview.addEventListener('click', overcont);
-
-
 };
