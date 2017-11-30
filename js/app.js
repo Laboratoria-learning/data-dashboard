6528e2a9ffbc3e75c0851527aa2574e52c19ac83
alejandraHoces <hocesalejandra@gmail.com>
2017-11-30 11:28:58 -0500

    Añadiendo estilos js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,36 @@ var content = document.getElementById('number').innher*/
 
 
 /*Opcion que accede a el numero de alumnas de AQP*/
-var objetoAQP = data.AQP
+var objetoAQP = data.AQP;
 var ageGeneracion = objetoAQP['2016-2'];
 var student = ageGeneracion.students
-console.log(student.length);
+console.log(arrSede);//15
+console.log(data);
+
+/*creando menu acordion de año y generacion*/
+var arrSede = data.LIM;
+var sedeLima = Object.keys(arrSede);
+console.log(sedeLima);
+
+var sedes = {};
+sedes['slm'] = sedeLima
+sedes['saqp'] = []
+sedes['ssdl'] = []
+sedes['scmx'] = []
+
+var dataSelect = document.getElementById('select-data');
+dataSelect.addEventListener('change', dataBase);
+
+function dataBase(event) {
+
+}
+
+
+
+
+
+
+/*var baseGeneration = {};
+baseGeneration['slm'] =['2016-2', '2017-1', '2017-2'];
+baseGeneration['saqp'] =['2016-2', '2017-1'];
+baseGeneration['slm'] =['2016-2', '2017-1', '2017-2'];*/
