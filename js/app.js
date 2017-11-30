f5aac010fea3a24e81faceacfc45149c0db514d3
Danielapariona <daniela.parionac@gmail.com>
2017-11-30 09:32:13 -0500

    Add functionality in select principal

--- js/app.js
+++ js/app.js
@@ -9,3 +9,30 @@ function viewMenu() {
 };
 
 menuPrincipal.addEventListener('click', viewMenu);
+
+var options = document.getElementsByName('option');
+for (var i = 0; i < options.length; i++) {
+  options[i].addEventListener('click', valueOption);
+};
+
+function valueOption(event) {
+  var generation = event.target.value;
+  var place = event.target.getAttribute('data-place');
+  studentsCurrentlyEnrolled.textContent = totalStudents(place, generation);
+};
+
+function totalStudents(place, generation) {
+  var totalInactive = 0;
+  var totalActive = 0;
+  var dataStudents = data[place][generation]['students'];
+  for (var i = 0; i < dataStudents.length; i++) {
+    if (dataStudents[i]['active'] === true) {
+      totalActive++;
+    } else if (dataStudents[i]['active'] === false) {
+      totalInactive++;
+    }
+  }
+  var totalStudents = totalActive + totalInactive;
+  return totalStudents;
+}
+
