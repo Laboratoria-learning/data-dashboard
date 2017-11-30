3f85d01069e2d7a57ecd39a1e8f7c92a92bb235f
Pamelaleo96 <delacruzlozano.p@gmail.com>
2017-11-30 02:41:21 -0500

    Actualizando archivo css navegador y archivo html von javascript

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 /* Funcionalidad de tu producto*/
 window.addEventListener('load', function() {
-    var currentStudents = document.getElementById('currentStudents');
-    var sede =
-function totalStudents (sede, generacion) {
-    if (sede =='Lima' && generacion == '2017-II'){
-    }
-totalStudents('Lima', '2017-II');
+  var currentStudents = document.getElementById('currentStudents');
+  var sede = document.getElementsByClassName('sede');
+  var promocion = document.getElementsByClassName('promocion');
+
+  function studentsLaboratoria(sede, generacion) {
+    
+  }  
+  studentsLaboratoria('Lima', '2017-II');
 });
+
      
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
