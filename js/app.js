3bfbcab2d0911442ee0358bfc7b240d6c2b003bd
Danielapariona <daniela.parionac@gmail.com>
2017-11-29 21:48:09 -0500

    Add effects toggle

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+var menuPrincipal = document.getElementById('menu-principal');
+var content = document.getElementById('content-menu');
+
+function viewMenu() {
+  content.classList.toggle('hidden');
+}
+
+
+menuPrincipal.addEventListener('click', viewMenu);
