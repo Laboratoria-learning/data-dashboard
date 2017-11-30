4f3edb00d3f9fe0c7970943b97de7dcecede7bd5
YosseLine <yoseline_96@hotmail.com>
2017-11-30 01:48:38 -0500

    Modificando css y resolviendo conflictos

--- js/app.js
+++ js/app.js
@@ -1,31 +1,25 @@
 var ulRegion = document.getElementById('ul-region');
 var ulLim = document.getElementById('ul-lim');
-
 var divRegion = document.getElementById('divRegion');
 var lima = document.getElementById('lim');
 var ulLim = document.getElementById('ul-lim');
 var lim172 = document.getElementById('lim172');
 var lim171 = document.getElementById('lim171');
 var lim162 = document.getElementById('lim162');
-
 var arequipa = document.getElementById('aqp');
 var ulAqp = document.getElementById('ul-aqp');
 var aqp171 = document.getElementById('aqp171');
 var aqp162 = document.getElementById('aqp162');
-
 var chile = document.getElementById('scl');
 var ulScl = document.getElementById('ul-scl');
 var scl172 = document.getElementById('scl172');
 var scl171 = document.getElementById('scl171');
 var scl162 = document.getElementById('scl162');
-
 var mexico = document.getElementById('cdmx');
 var ulCdmx = document.getElementById('ul-cdmx');
 var cdmx172 = document.getElementById('cdmx172');
 var cdmx171 = document.getElementById('cdmx171');
-
 var previous = divRegion;
-
 window.addEventListener('load', function() {
   divRegion.addEventListener('click', showAllRegions);
   lima.addEventListener('click', optionsLima);
@@ -40,7 +34,6 @@ window.addEventListener('load', function() {
   var promotion = lim172.dataset.promotion;
   showMain(region, promotion);
 });
-
 function showAllRegions() {
   // ulRegion.classList.toggle('hidden');
   ulRegion.classList.toggle('hidden');
@@ -50,7 +43,6 @@ function showAllRegions() {
   ulScl.classList.toggle('hidden');
   ulCdmx.classList.toggle('hidden');
 };
-
 function paintOptions(option, previous) {
   option.classList.add('yellow');
   if (option !== previous)
@@ -79,6 +71,9 @@ function optionslim172() {
 function optionslim171() {
   paintOptions(lim171, previous);
   previous = lim171;
+  var region = lim171.dataset.region;
+  var promotion = lim171.dataset.promotion;
+  showMain(region, promotion);
 }
 function optionslim162() {
   paintOptions(lim162, previous);
@@ -99,39 +94,42 @@ function optionsMexico() {
   previous = mexico;
   ulCdmx.classList.toggle('hidden');
 }
-
-function showMain(promotion) {
-  showTotalStudents(promotion);
+function showMain(region, promotion) {
+  showTotalStudents(region, promotion);
 }
-
 function showTotalStudents(region, promotion) {
-  drawTotalStudents(6, 7);
+  var current = 0;
+  var deserted = 0;
+  console.log('region '+region+' promotion '+promotion );
+  var sedeRegion = region;
+  var generation = promotion;
   /*
-  console.log(data.LIM);
-  for(var regionData in data) {
-    if(region === regionData)
-      console.log(regionData);
-  }
-  */
   var sedeRegion = 'LIM';
-  var generacion = '2017-2';
+  var generation = '2017-2';
+  */
   for (var regionData in data) {
     if (regionData === sedeRegion) {
-      for (var semestre in data [regionData]) {
-        if (semestre === generacion) {
-          console.log(data[regionData][semestre].students);
+      for (var promo in data [regionData]) {
+        if (promo === generation) {
+          for (var students in data [regionData][promo]) {
+            if (students === 'students') {
+              for (var eachStudent in data [regionData][promo][students]) {
+                var active = data[sedeRegion][promo][students][eachStudent]['active'];
+                (active) ? current++ : deserted++;
+              }
+            }
+          }
         }
       }
     }
   }
+  console.log("current "+current+" deserted "+deserted);
+  drawTotalStudents(current, deserted);
 }
 // Obtener a quien elijo
 // LIM172 = data.LIM['2017-2'];
-// console.log(LIM172);
 // Pasar parametro para obtener ya datos estadisticos
-
 /*************************GRAFICOS************/
-
 function drawTotalStudents(current, deserted) {
   google.charts.load('current', {'packages':['corechart']});
   google.charts.setOnLoadCallback(drawChart);
@@ -145,8 +143,8 @@ function drawTotalStudents(current, deserted) {
     ]);
     var options = {
       'colors': ['#109618', '#dc3912'],
-      'width': 300,
-      'height': 300 };
+      'width': 320,
+      'height': 150 };
     var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
     chart.draw(dataTest, options);
   }
