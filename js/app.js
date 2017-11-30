0b9bcad4b12e8b670f01034df31bc243ae325505
dayamll <dayamllerenacastro@gmail.com>
2017-11-29 20:30:34 -0500

    Subiendo primera seccion

--- js/app.js
+++ js/app.js
@@ -3,15 +3,25 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-var expanded = false;
+// var expanded = false;
 
-function showCheckboxes() {
-  var checkboxes = document.getElementById("checkboxes");
-  if (!expanded) {
-    checkboxes.style.display = "block";
-    expanded = true;
-  } else {
-    checkboxes.style.display = "none";
-    expanded = false;
-  }
+// function showCheckboxes() {
+//   var checkboxes = document.getElementById("checkboxes");
+//   if (!expanded) {
+//     checkboxes.style.display = "block";
+//     expanded = true;
+//   } else {
+//     checkboxes.style.display = "none";
+//     expanded = false;
+//   }
+// }
+
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
 }
