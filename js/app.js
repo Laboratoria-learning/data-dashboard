ce9f049f29b1607f2a060df15ac624eb0b8673c0
Rocciescobar <roccisen.88@gmail.com>
2017-11-29 23:03:54 -0500

    agregando identificador a p

--- js/app.js
+++ js/app.js
@@ -14,13 +14,18 @@ window.addEventListener('load', function() {
 
   // ------------> Crea función para seleccionar sede y promoción, y generar datos <----------
   var select = document.getElementById('promo-filter');
-  select.addEventListener('change', function(event) {
+
+  select.addEventListener('change', promFilter);
+  
+  function promFilter() {
     var city = select.value; // AQP SCL CDMX LIM
     var prom = select.options[select.selectedIndex].dataset.year; // 2016-2, 2017-1, etc.
-    var totalStudents = data[city][prom].students.length; // Cantidad total de estudiantes por sede y promoción
-
+    var totalStudents = data[city][prom]['students'].length; // Cantidad total de estudiantes por sede y promoción
+    var dataRatings = data[city][prom]['ratings'];
+    var arrayStudents = data[city][prom]['students'];
+/*
     // ------------> ENROLLMENT - arequipa 2016-II <-----------------
-    var arrayStudents = data[city][prom].students;
+    
     // recorre la longitud de las estudiantes y muestra a las que dropout en porcentaje
     var dropout = 0;
     for (var i = 0; i < totalStudents; i++) {
@@ -47,24 +52,31 @@ window.addEventListener('load', function() {
     leyendadropoutPorcentaje.textContent = '% DROPOUT';
     dropoutPorcentaje.appendChild(textPorcentaje);
     dropoutPorcentaje.appendChild(leyendadropoutPorcentaje);
-
+*/
     // ------------> ACHIEVEMENT <--------------------
-    var dataRatings = data[city][prom]['ratings'];
-    var meetsRange = 0;
-    var exceedsRange = 0;
-    var totalStudentsOther = (dataRatings.length) * 100;
+    
+    // data['AQP']['2016-2']['students'][0]['sprints'][0]['score'] // {tech: 1213, hse: 854}
+    // data['AQP']['2016-2']['students'][0]['sprints'][0]['score']['tech'] // 1213
 
-    for (var i = 0; i < dataRatings.length; i++) {
-      meetsRange = meetsRange + dataRatings[i]['student']['cumple'];
+    var scoreTech = 0;
+    var scoreHSE = 0;
+
+    for (var i = 0; i < totalStudents; i++) {
+      for (var j = 0; j < arrayStudents[i]['sprints'].length; j++) {
+        scoreTech += arrayStudents[i]['sprints'][j]['score']['tech'];
+      }      
     }
+    console.log(scoreTech);
+/*
+    var achievement = document.getElementById('box-achievement');
+    achievement.textContent = sumAchievement;
 
-    for (var i = 0; i < dataRatings.length; i++) {
-      exceedsRange = exceedsRange + dataRatings[i]['student']['supera'];
-    }
+    var percentOfTotal = document.getElementById('percent-achievement');
+    percentOfTotal.textContent = percentAchievement.toFixed(2);
 
-    var percent = ((meetsRange + exceedsRange) / totalStudentsOther) * 100;
-
-    console.log(percent);
+    var totalAchievement = document.getElementById('total-achievement');
+    totalAchievement.textContent = '% OF TOTAL (' + totalStudentsOther + ')';
+*/
 /*
     // ------------> NPS - arequipa 2016-II <-----------------
     var box2 = document.getElementById('aqp-nps-sprint1');
@@ -81,58 +93,68 @@ window.addEventListener('load', function() {
     parag2.appendChild(text2);
     box2.appendChild(parag2);
 */
-  });
 
-// ------------> Crea función para generar lista de estudiantes <-----------------
+    // -----------> TEACHER RATING <-----------
+    var sumTeacherRating = 0;
 
-  var studentsTab = document.getElementById('studentsTab');
-  studentsTab.addEventListener('click', showSectionStudents);
+    for (var i = 0; i < dataRatings.length; i++) {
+      sumTeacherRating += dataRatings[i]['teacher'];          
+    }   
 
-  var sectionOverview = document.getElementById('section-overview');
-  var sectionStudents = document.getElementById('section-students');
-
-  function showSectionStudents(event) {
-    console.log(event.target);
-    sectionOverview.classList.add('hide');
-    sectionStudents.classList.remove('hide');
-    sectionStudents.style.cssText = 'display:inline-block; background:white;';
+    var overallTeacherRating = sumTeacherRating / dataRatings.length;
+    var teacherRating = document.getElementById('teacher-rating');
+    teacherRating.textContent = overallTeacherRating.toFixed(1);
   }
+/*
+    // ------------> Crea función para generar lista de estudiantes <-----------------
+    var studentsTab = document.getElementById('studentsTab');
+    studentsTab.addEventListener('click', showSectionStudents);
 
-  // ruta para hallar el value del filtro
-  var promoFilter = document.getElementById('promo-filter');
+    var sectionOverview = document.getElementById('section-overview');
+    var sectionStudents = document.getElementById('section-students');
 
-  if (promoFilter.children[1].children[0].value === 'aqp-16-2') {
-    students = data.AQP['2016-2'].students;
-    //console.log(students);
-    showStudents();
-  } else if (promoFilter.children[1].children[1].value === 'aqp-17-1') {
-    students = data.AQP['2017-1'].students;
-    console.log(students);
-    showStudents();
-  }
+    function showSectionStudents(event) {
+      console.log(event.target);
+      sectionOverview.classList.add('hide');
+      sectionStudents.classList.remove('hide');
+      sectionStudents.style.cssText = 'display:inline-block; background:white;';
+    }
 
-  function showStudents() {
+    // ruta para hallar el value del filtro
+    var promoFilter = document.getElementById('promo-filter');
+
+    if (promoFilter.children[1].children[0].value === 'aqp-16-2') {
+      students = data.AQP['2016-2'].students;
+      //console.log(students);
+      showStudents();
+    } else if (promoFilter.children[1].children[1].value === 'aqp-17-1') {
+      students = data.AQP['2017-1'].students;
+      console.log(students);
+      showStudents();
+    }
+  
+  function showStudents()
     var container = document.getElementById('students-hidden');
     for (var i = 0; i < students.length; i++) {
-    //crear un div para una estudiante
-   var profilestudent = document.createElement('div');
-   container.appendChild(profilestudent);
+      //crear un div para una estudiante
+    var profilestudent = document.createElement('div');
+    container.appendChild(profilestudent);
 
-   //crear la imagen para la foto de perfil
-   var imgprofile= document.createElement('img');
-   imgprofile.classList.add('img-student');
-   imgprofile.setAttribute('src', students[i].photo);
+    //crear la imagen para la foto de perfil
+    var imgprofile= document.createElement('img');
+    imgprofile.classList.add('img-student');
+    imgprofile.setAttribute('src', students[i].photo);
 
-   profilestudent.appendChild(imgprofile);
+    profilestudent.appendChild(imgprofile);
 
-   //agregar nombre de estudiante
-   var information  = document.createElement('p');
-    information.classList.add('datos-studen');
-   var otherName = document.createElement('h5');
-   otherName.textContent=(students[i].name);
+    //agregar nombre de estudiante
+    var information  = document.createElement('p');
+      information.classList.add('datos-studen');
+    var otherName = document.createElement('h5');
+    otherName.textContent=(students[i].name);
 
-   information.appendChild(otherName);
-   profilestudent.appendChild(information);
-    }
-  }
+    information.appendChild(otherName);
+    profilestudent.appendChild(information);
+   }
+*/
 });
