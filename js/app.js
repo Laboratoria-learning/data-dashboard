ecd1429ff3d24665542afb3fb9d33acda239902f
llandreall <andrea.telleria@gmail.com>
2017-11-30 13:07:48 -0500

    Pruebas de funcionalidad para tab Students

--- js/app.js
+++ js/app.js
@@ -1,19 +1,13 @@
-/* Funciones Maribel */
-// agregar el evento load
-window.addEventListener('load', function() {
+/* -------Funciones Maribel-------
+window.addEventListener('load', function () {
   var selection = document.getElementById('selection');
-  // console.log(selection);
   var contenidoSede = document.getElementById('contenido-sede');
-  // console.log(contenidoSede);
   var tabs = document.getElementsByClassName('tab');
   var contents = document.getElementsByClassName('content');
 
   function mostrarInfo() {
-  };
-
-  selection.addEventListener('change', function(e) {
-  // console.log(e.target.value);
-    var value = e.target.value;
+    //  console.log(e.target.value);
+    var value = selection.value;
     var aux = value.split('-');
     var sedeName = aux.shift();
     var generation = aux.join('-');
@@ -21,34 +15,52 @@ window.addEventListener('load', function() {
     var generationData = data[sedeName][generation];
     var totalStudents = generationData.students.length;
 
-    console.log(generationData);
+    var counter = 0;
+
+    generationData.students.forEach(function (student) {
+      if (student.active === false) {
+        counter++;
+      }
     });
+
+    selection.addEventListener('change', function (e) {
+      var value = e.target.value;
+      var aux = value.split('-');
+      var sedeName = aux.shift();
+      var generation = aux.join('-');
+
+      var generationData = data[sedeName][generation];
+      var totalStudents = generationData.students.length;
+    });
+
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener('click', function (event) {
+        for (var j = 0; j < tabs.length; j++) {
+          tabs[j].classList.remove('active');
+        }
+
+        for (var k = 0; k < contents.length; k++) {
+          contents[k].classList.remove('active');
+        }
+
+        event.target.classList.add('active');
+        contents[event.target.dataset.content].classList.add('active');
+      });
+    }
+  }
+});
+*/
+
+window.addEventListener ('load', function () {
+  var studentButton = document.getElementById('studentButton');
+  studentButton.addEventListener('click', function () {
+    document.getElementById('studentsTab').style.display = 'block';
   });
 
-  // agregar el evento click a todos los tabs
-  for (var i = 0; i < tabs.length; i++) {
-    // y dentro del click para cada tab
-    tabs[i].addEventListener('click', function(event) {
-      for (var j = 0; j < tabs.length; j++) {
-        // quitar la clase active a todos los tabs
-        tabs[j].classList.remove('active');
-      }
-
-      for (var k = 0; k < contents.length; k++) {
-        // quitar la clase active a todos los contents
-        contents[k].classList.remove('active');
-      }
-
-      // agregar la clase active solo a este tab que se le dio click
-      event.target.classList.add('active');
-      // agregar la clase active solo al content correspondiente (data-content
-      contents[event.target.dataset.content].classList.add('active');
-    });
-  }
-  /* Fin */
+  
 });
 
-// Pruebas en consola
+/* Pruebas en consola
 var lim20162 = data.LIM['2016-2'];
 var lim20171 = data.LIM['2017-1'];
 var lim20172 = data.LIM['2017-2'];
@@ -60,16 +72,9 @@ var scl20162 = data.SCL['2016-2'];
 var scl20171 = data.SCL['2017-1'];
 var scl20172 = data.SCL['2017-2'];
 
-/*
 var lim20162students = Object.values(lim20162)[0];
 var lim20171students = Object.values(lim20171)[0];
 var lim20172students = Object.values(lim20172)[0];
 
 console.log(lim20172students);
 */
-
-function showProfiles () {
-
-}
-
-lim20162.students
