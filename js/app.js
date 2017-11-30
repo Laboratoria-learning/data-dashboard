21c55c2bbffed2bcfd6a6e296c162cbd96cd4c1a
MYTH-INF <maytheinfante96@gmail.com>
2017-11-30 10:49:22 -0500

    maquetado1

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
-// solo para estudiantes AQP 2016-2
 
-var students = data.AQP['2016-2'].students;
+window.addEventListener('load', function() {
+  // variable que almacena  el texto junto de sede y generacion, el cual se muestra en la pantalla
+  var sedeGeneration = document.getElementById('sede_generation');
+  // console.log(sedeGeneration); // <a href="#" id="sede_generation">LIMA 2017-2</a>
 
-// calculando el total de estudiantes ACTIVAS AQP
-var activeAqp = 0;
-for (i = 0; i < students.length; i++) {
-  if (students[i].active === true) {
-    var activeAqp = (activeAqp + 1);
+  // variable que almacena la lista de sedes
+  var listSedes = document.getElementById('listSedes');
+  // console.log(listSedes); // <div id="listSedes" class="sedes_hiden"></div>
+
+  var listGenerations = document.querySelectorAll('.listGenerations');
+  // console.log(listGenerations); // todos los div con las generaciones
+  // console.log(listGenerations.children);
+
+  var generations = document.querySelectorAll('.generation');
+  // console.log(generations); // (10) [a.generation, a.generation, a.generation, a.generation, a.generation, a.generation, a.generation, a.generation, a.generation, a.generation]
+
+
+  sedeGeneration.addEventListener('click', showListSedes);
+  function showListSedes(event) {
+    listSedes.classList.toggle('show');
+  };
+
+  var sedes = document.querySelectorAll('.sede');
+  // console.log(sedes); // [a.sede =arequipa, a.sede =mexico, a.sede = lima, a.sede = chile] // lista de nodos
+  for (var i = 0; i < sedes.length; i++) {
+    sedes[i].addEventListener('click', function(event) {
+      // console.log(event.target); //  <a href="#" class="sede" data-sede ="AQP">Arequipa</a>
+      var sede = event.target.dataset.sede;
+      // console.log(sede); // AQP
+
+      for (var i = 0; i < listGenerations.length; i++) {
+        listGenerations[i].addEventListener('click', function(event) {
+          // sconsole.log(event.target);
+          var generation = event.target.textContent;
+          // console.log(generation);// 2016-2
+          var students = data[sede][generation].students.length ;
+          // console.log(students);// 15
+          alert('el número de estudiantes de la sede ' + sede + ' y de la generación ' + generation + ' es: ' + students);
+        });
+      }
+    });
   }
-};
-// calculando el total de estudiantes INACTIVAS AQP
-var inactiveAqp = students.length - activeAqp;
-
-// calculado la suma de scores(HSE Y TECH) por sprints de cada estudiante
-
-//  suma score hse
-
-for (i = 0; i < students.length; i++) {
-  var studentsSprints = students[i].sprints;
-  console.log(studentsSprints);
-};
+});
