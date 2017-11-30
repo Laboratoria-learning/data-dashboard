40e2205a3a8f3f6d259845f3ee55d694ea42e0c2
YoannaHS <yoannahernandez.s@gmail.com>
2017-11-30 15:48:57 -0500

    Añadiendo data a los gráficos

--- js/grafhic.js
+++ js/grafhic.js
@@ -1,4 +1,4 @@
- 
+
   google.charts.load('current', {'packages':['corechart']});
   google.charts.setOnLoadCallback(drawChart);
 
@@ -6,10 +6,10 @@
 
     var data = google.visualization.arrayToDataTable([
       ['Sedes', 'Total de alumnas'],
-      ['Arequipa',     11],
-      ['Santiago de Chile',      2],
-      ['Ciudad de México',  2],
-      ['Lima', 2],
+      ['Arequipa',     30],
+      ['Santiago de Chile',      95],
+      ['Ciudad de México',  70],
+      ['Lima', 66],
     ]);
 
     var options = {
@@ -19,4 +19,24 @@
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
     chart.draw(data, options);
-  }
+
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Desercion', 'Total y porcentaje'],
+        ['Arequipa',     51],
+        ['Santiago de Chile',      27],
+        ['Ciudad de México',  18],
+        ['Lima', 59],
+      ]);
+
+      var options = {
+        title: 'Total de alumnas que desertaron por sede',
+        pieHole: 0.4,
+      };
+
+      var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+      chart.draw(data, options);
+    };
+  };
