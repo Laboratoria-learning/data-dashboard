2c0cc341c75f73ff916606e73207804164bcd598
PaoSil <pshuaman@outlook.com>
2017-11-30 17:16:03 -0500

    actualizacion 2

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ function create() {
   case 'LIMA2016-II':
     var estudiantelim1 = (data['LIM']['2016-2']['students'].length);
     console.log(document.getElementById('one').innerHTML = estudiantelim1 + ' # estudents currentl enrrolled');
-    
+
     // creando desercion
     var totalStudent = data['LIM']['2016-2']['students'];
     var activeStudent = [];
@@ -52,7 +52,7 @@ function create() {
 
     // creando promedio de los profesores
     var teacheone = data['LIM']['2016-2']['ratings'][0]['teacher'];
-    var teachetwo = data['LIM']['2016-2']['ratings'][1]['teacher'];    
+    var teachetwo = data['LIM']['2016-2']['ratings'][1]['teacher'];
     var total = teacheone + teachetwo ;
     var promedio = total / 2;
 
@@ -74,12 +74,12 @@ function create() {
     var supera = supera1 + supera2;
     var satisfecho = (cumple + supera) / 2;
     document.getElementById('satisfecho').innerHTML = satisfecho;
-    
-    
+
+
     break;
 
   case 'LIMA2017-I':
-    var estudiantelim2 = (data['LIM']['2017-1']['students'].length);   
+    var estudiantelim2 = (data['LIM']['2017-1']['students'].length);
     console.log(document.getElementById('one').innerHTML = estudiantelim2);
     // creando desercion
     var totalStudent = data['LIM']['2017-1']['students'];
@@ -92,7 +92,7 @@ function create() {
         inactiveStudent.push(i);
       }
     }
-    document.getElementById('two').innerHTML = (inactiveStudent.length / estudiantelim2) * 100 + '%'; 
+    document.getElementById('two').innerHTML = (inactiveStudent.length / estudiantelim2) * 100 + '%';
 
     // creando NPS;
     var ratings = data['LIM']['2017-1']['ratings'][0];
@@ -105,7 +105,7 @@ function create() {
 
     console.log(document.getElementById('six-1').innerHTML = (promoters) + '%' + 'promoters');
     console.log(document.getElementById('six-2').innerHTML = (passive) + '%' + 'passives');
-    console.log(document.getElementById('six-3').innerHTML = (detractors) + '%' + 'detractors'); 
+    console.log(document.getElementById('six-3').innerHTML = (detractors) + '%' + 'detractors');
 
     // creando promedio de los profesores
     var teacheone = data['LIM']['2017-1']['ratings'][0]['teacher'];
@@ -113,7 +113,7 @@ function create() {
     var teachetree = data['LIM']['2017-1']['ratings'][2]['teacher'];
     var teachefour = data['LIM']['2017-1']['ratings'][3]['teacher'];
     var total = teacheone + teachetwo + teachetree + teachefour;
-    var promedio = total / 4;    
+    var promedio = total / 4;
     document.getElementById('teacher').innerHTML = promedio;
 
     // creando promedio de jedis
@@ -172,7 +172,7 @@ function create() {
     var teacheone = data['LIM']['2017-2']['ratings'][0]['teacher'];
     var teachetwo = data['LIM']['2017-2']['ratings'][1]['teacher'];
     var total = teacheone + teachetwo;
-    var promedio = total / 2; 
+    var promedio = total / 2;
     document.getElementById('teacher').innerHTML = promedio;
     // creando promedio para jedis
     var teacheone = data['LIM']['2017-2']['ratings'][0]['jedi'];
@@ -180,7 +180,7 @@ function create() {
     var total = teacheone + teachetwo;
     var promedio = total / 2;
     document.getElementById('jedi').innerHTML = promedio;
-    
+
     // creando funcion de satisfaccion
     var cumple1 = data['LIM']['2017-2']['ratings'][0]['student']['cumple'];
     var cumple2 = data['LIM']['2017-2']['ratings'][1]['student']['cumple'];
@@ -195,7 +195,7 @@ function create() {
   case 'AREQUIPA2016-II':
     var estudiantelim4 = (data['AQP']['2016-2']['students'].length);
     console.log(document.getElementById('one').innerHTML = estudiantelim4);
-    
+
     // creando desercion
     var totalStudent = data['AQP']['2016-2']['students'];
     var activeStudent = [];
@@ -226,7 +226,7 @@ function create() {
     var teachetree = data['AQP']['2016-2']['ratings'][2]['teacher'];
     var teachefour = data['AQP']['2016-2']['ratings'][3]['teacher'];
     var total = teacheone + teachetwo + teachetree + teachefour;
-    var promedio = total / 4;      
+    var promedio = total / 4;
     document.getElementById('teacher').innerHTML = promedio;
     // creando promedio de los jedis
     var teacheone = data['AQP']['2016-2']['ratings'][0]['jedi'];
@@ -250,7 +250,7 @@ function create() {
     var supera = supera1 + supera2 + supera3 + supera4;
     var satisfecho = (cumple + supera) / 2;
     document.getElementById('satisfecho').innerHTML = satisfecho;
-      
+
     break;
   case 'AREQUIPA2017-I':
     var estudiantelim5 = (data['AQP']['2017-1']['students'].length);
@@ -282,7 +282,7 @@ function create() {
     // creando promedio de los profesores
     var teacheone = data['AQP']['2017-1']['ratings'][0]['teacher'];
     var teachetwo = data['AQP']['2017-1']['ratings'][1]['teacher'];
-    var teachetree = data['AQP']['2017-1']['ratings'][2]['teacher'];          
+    var teachetree = data['AQP']['2017-1']['ratings'][2]['teacher'];
     var total = teacheone + teachetwo + teachetree ;
     var promedio = total / 3;
     document.getElementById('teacher').innerHTML = promedio;
@@ -292,19 +292,19 @@ function create() {
     var teachetree = data['AQP']['2017-1']['ratings'][2]['jedi'];
     var total = teacheone + teachetwo + teachetree;
     var promedio = total / 3;
-    document.getElementById('jedi').innerHTML = promedio; 
+    document.getElementById('jedi').innerHTML = promedio;
 
     // creando funcion de satisfaccion
     var cumple1 = data['AQP']['2017-1']['ratings'][0]['student']['cumple'];
     var cumple2 = data['AQP']['2017-1']['ratings'][1]['student']['cumple'];
-    var cumple3 = data['AQP']['2017-1']['ratings'][2]['student']['cumple'];    
+    var cumple3 = data['AQP']['2017-1']['ratings'][2]['student']['cumple'];
     var cumple = cumple1 + cumple2 + cumple3 ;
     var supera1 = data['AQP']['2017-1']['ratings'][0]['student']['supera'];
     var supera2 = data['AQP']['2017-1']['ratings'][1]['student']['supera'];
-    var supera3 = data['AQP']['2017-1']['ratings'][2]['student']['supera'];   
+    var supera3 = data['AQP']['2017-1']['ratings'][2]['student']['supera'];
     var supera = supera1 + supera2 + supera3 ;
     var satisfecho = (cumple + supera) / 2;
-    document.getElementById('satisfecho').innerHTML = satisfecho; 
+    document.getElementById('satisfecho').innerHTML = satisfecho;
 
     break;
   case 'SantiagodeChile2016-II':
@@ -321,7 +321,7 @@ function create() {
         inactiveStudent.push(i);
       }
     }
-    
+
     document.getElementById('two').innerHTML = (inactiveStudent.length / estudiantelim6) * 100 + '%';
 
     // creando NPS
@@ -366,7 +366,7 @@ function create() {
     var supera4 = data['SCL']['2016-2']['ratings'][3]['student']['supera'];
     var supera = supera1 + supera2 + supera3 + supera4;
     var satisfecho = (cumple + supera) / 2;
-    document.getElementById('satisfecho').innerHTML = satisfecho;    
+    document.getElementById('satisfecho').innerHTML = satisfecho;
 
     break;
   case 'SantiagodeChile2017-I':
@@ -422,9 +422,9 @@ function create() {
     var supera3 = data['SCL']['2017-1']['ratings'][2]['student']['supera'];
     var supera = supera1 + supera2 + supera3 ;
     var satisfecho = (cumple + supera) / 2;
-    document.getElementById('satisfecho').innerHTML = satisfecho;  
-    
-    
+    document.getElementById('satisfecho').innerHTML = satisfecho;
+
+
     break;
   case 'SantiagodeChile2017-II':
     var estudiantelim8 = (data['SCL']['2017-2']['students'].length);
@@ -456,10 +456,10 @@ function create() {
 
     // creando promedio de los profesores
     var teacheone = data['SCL']['2017-2']['ratings'][0]['teacher'];
-    var teachetwo = data['SCL']['2017-2']['ratings'][1]['teacher'];    
+    var teachetwo = data['SCL']['2017-2']['ratings'][1]['teacher'];
     var total = teacheone + teachetwo ;
     var promedio = total / 2;
-    document.getElementById('teacher').innerHTML = promedio; 
+    document.getElementById('teacher').innerHTML = promedio;
 
     // creando promedio de los jedis
     var teacheone = data['SCL']['2017-2']['ratings'][0]['jedi'];
@@ -507,12 +507,12 @@ function create() {
     console.log(document.getElementById('six-1').innerHTML = (promoters) + '%' + 'promoters');
     console.log(document.getElementById('six-2').innerHTML = (passive) + '%' + 'passives');
     console.log(document.getElementById('six-3').innerHTML = (detractors) + '%' + 'detractors');
-    
+
     // creando promedio de los profesores
     var teacheone = data['CDMX']['2017-1']['ratings'][0]['teacher'];
     var teachetwo = data['CDMX']['2017-1']['ratings'][1]['teacher'];
     var teachetree = data['CDMX']['2017-1']['ratings'][2]['teacher'];
-          
+
     var total = teacheone + teachetwo + teachetree;
     var promedio = total / 3;
 
@@ -536,7 +536,7 @@ function create() {
     var supera3 = data['CDMX']['2017-1']['ratings'][2]['student']['supera'];
     var supera = supera1 + supera2 + supera3 ;
     var satisfecho = (cumple + supera) / 2;
-    document.getElementById('satisfecho').innerHTML = satisfecho;  
+    document.getElementById('satisfecho').innerHTML = satisfecho;
     break;
   case 'CiudaddeMexico2017-II':
     var estudiantelim10 = (data['CDMX']['2017-2']['students'].length);
@@ -567,7 +567,7 @@ function create() {
     console.log(document.getElementById('six-3').innerHTML = (detractors) + '%' + 'detractors');
     // creando promedio de los profesores
     var teacheone = data['CDMX']['2017-2']['ratings'][0]['teacher'];
-    var teachetwo = data['CDMX']['2017-2']['ratings'][1]['teacher'];    
+    var teachetwo = data['CDMX']['2017-2']['ratings'][1]['teacher'];
     var total = teacheone + teachetwo ;
     var promedio = total / 2;
     document.getElementById('teacher').innerHTML = promedio;
@@ -586,7 +586,7 @@ function create() {
     var supera = supera1 + supera2;
     var satisfecho = (cumple + supera) / 2;
     document.getElementById('satisfecho').innerHTML = satisfecho;
-    
+
     break;
   default:
     alert('seleccione una opcion');
@@ -594,7 +594,6 @@ function create() {
 }
 
 function select() {
-  
   sede.addEventListener('change', create);
   students.addEventListener('click', estudentcont);
   overview.addEventListener('click', overcont);
