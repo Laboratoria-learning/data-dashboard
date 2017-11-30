e46949ea2ed7f02e4245942d7cbabee053b7fb6d
brendamesias <brendamesiasvillar@gmail.com>
2017-11-30 01:42:29 -0500

    comentando los console.log

--- js/app.js
+++ js/app.js
@@ -26,15 +26,15 @@ window.addEventListener('load', function() {
     sedes[i].addEventListener('click', function(event) {
       // console.log(event.target); //  <a href="#" class="sede" data-sede ="AQP">Arequipa</a>
       var sede = event.target.dataset.sede;
-      console.log(sede); // AQP
+      // console.log(sede); // AQP
 
       for (var i = 0; i < listGenerations.length; i++) {
         listGenerations[i].addEventListener('click', function(event) {
-          console.log(event.target);
+          // sconsole.log(event.target);
           var generation = event.target.textContent;
-          console.log(generation);// 2016-2
+          // console.log(generation);// 2016-2
           var students = data[sede][generation].students.length ;
-          console.log(students);// 15
+          // console.log(students);// 15
           alert('el número de estudiantes de la sede ' + sede + ' y de la generación ' + generation + ' es: ' + students);
         });
       }
