90990cd3935830b3780c2e5adba175fc30fed791
melissayauri <yaurimelissa@gmail.com>
2017-11-30 16:56:42 -0500

    incorporando item satifaccion de estudiantes

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ window.addEventListener('load', function() {
   var selection = document.getElementById('selection');
   var containerStudents = document.getElementById('total-students');
   var totalNps = document.getElementById('total-nps');
+  var satisfaction = document.getElementById('satisfaction');
   selection.addEventListener('change', showGenerations);
   function showGenerations(event) {
     /* seleccionando cada option de lista despegable*/
     var options = selection.value;
     /* se convierte a un array para obtener la data de sede y generaciones*/
     var array = options.split('-');/* [lima,2017,2]*/
-    console.log(array);
+    /* console.log(array);*/
     /* obteniendo la generacion*/
     var array1 = array.splice(1, 2);
     console.log(array1);
@@ -17,30 +18,47 @@ window.addEventListener('load', function() {
     var generation = array1[0] + '-' + array1[1];
     /* obteniendo la sede*/
     var site = array.splice(0, 1);
-    console.log(site);
+    /* console.log(site);*/
     /* obteniendo datos por sede-generacion*/
     var siteGeneration = data[site][generation];
-    console.log(siteGeneration);
+    /* console.log(siteGeneration);*/
     /* analizando el total de estudiantes*/
     var totalStudents = siteGeneration.students.length;
     containerStudents.innerHTML = '# de studiantes inscritos' + '</br>' + totalStudents;
     /* analizando el NPS*/
-    var result = 0;
+    var nps = 0;
     /* recorriendo el elemento ratings para obtner el nps*/
     for (i = 0; i < siteGeneration.ratings.length;i++) {
       var promoters = siteGeneration.ratings[i].nps.promoters;
+      var percentagePromotrs = (promoters / siteGeneration.ratings.length) * 100 / 100;
       var passive = siteGeneration.ratings[i].nps.passive;
+      var percentagePassive = (passive / siteGeneration.ratings.length) * 100 / 100;
       var detractors = siteGeneration.ratings[i].nps.detractors;
-      result = result + (promoters - detractors);
+      var percentageDetractors = (detractors / siteGeneration.ratings.length) * 100 / 100;
+      nps = nps + (promoters - detractors);
       /* console.log(result)*/
-      var percentage = ((result / siteGeneration.ratings.length) * 100) / 100 ;
-      /* considerando solo dos decimales */
-      percentage.toFixed(2) + ' % ';
-      /* console.log(percentage);*/
+      /* resultado del porcentaje del nps*/
+      var percentageNps = Math.round(((nps / siteGeneration.ratings.length) * 100) / 100) + ' \n' + '%';
     }
     /* incorporando el porcentaje del nps*/
-    totalNps.innerHTML = '% NPS' + '<br>' + percentage ;
-    /*analizando el */
+    totalNps.innerHTML = '% NPS' + '<br>' + percentageNps ;
+    /* analizando el nivel de satifaccion de la estudiantes*/
+    /* resultado de satisgacción*/
+    var satisfactionResult = 0;
+    /* recorriendo el elemento ratings*/
+    for (var j = 0; j < siteGeneration.ratings.length; j++) {
+      /* analizando el elemento ratings-student-cumple*/
+      var satisfactionCumple = siteGeneration.ratings[j]['student']['cumple'];
+      /* console.log(satisfactionCumple);*/
+      /* analizando el elemento ratings-student-supera*/
+      var satisfactionSupera = siteGeneration.ratings[j]['student']['supera'];
+      satisfactionResult = satisfactionResult + (satisfactionCumple + satisfactionSupera);
+    }
+    /* porcentaje de satisfaciión*/
+    var percentageSatisfaction = Math.round(satisfactionResult / siteGeneration.ratings.length) * 100 / 100 + '%';
+    /* percentageSatisfaction.toFixed(1);*/
+    /* console.log(percentageSatisfaction);*/
+    satisfaction.innerHTML = ' % de estudiantes satisfechos' + '<br>' + percentageSatisfaction;
   }
 
   /* menu derecho*/
