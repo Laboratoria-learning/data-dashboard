5ec09ad75bb182c6076f7e711872182692dbf2fb
AraceliGS <aragutarras@gmail.com>
2017-12-03 12:59:40 -0500

    añadiendo archivo app.js comentado

--- js/app-1.js
+++ js/app-1.js
@@ -7,13 +7,12 @@
 console.log(data);
 
 window.addEventListener('load', function(event) {
-
 /* evento para botón de especialización javascript */
   var javascriptButton = document.getElementById('javascript-button');
   javascriptButton.addEventListener('click', function(event) {
-  alert('es click');
-  button.classList.add('desactive');
- })
+    alert('es click');
+    button.classList.add('desactive');
+  });
 
   /* evento para botón de especialización uxdesing */
   var uxdesignButton = document.getElementById('uxdesign-button');
@@ -25,111 +24,111 @@ window.addEventListener('load', function(event) {
   /* evento para botón de especialización front end designer */
   var frontEndDesignerButton = document.getElementById('front-end-designer-button');
   frontEndDesignerButton.addEventListener('click', function(event) {
-  alert('es click en front end desing');
+    alert('es click en front end desing');
 
-  var students= [];
-  var studentsData = [];
-  var baseTech = 1800;
-  var baseHse = 1200;
-  var acumTech = 0;
-  var acumHse = 0;
-  var ind = 0;
-   
-  /* Sedes - Array */
-  var sedes = Object.keys(data);
-  var sedesArray=[];
-  var studentGeneratiosArray=[];
-    for ( var i=0 ; i< 4 ; i++) {
-          sedesArray[i] = sedes[i];
+    var students = [];
+    var studentsData = [];
+    var baseTech = 1800;
+    var baseHse = 1200;
+    var acumTech = 0;
+    var acumHse = 0;
+    var ind = 0;
+
+    /* Sedes - Array */
+    var sedes = Object.keys(data);
+    var sedesArray = [];
+    var studentGeneratiosArray = [];
+    for (var i = 0 ; i < 4 ; i++) {
+      sedesArray[i] = sedes[i];
     }
 
     /* Generaciones- Array */
     var generations = Object.values(data);
 
     /* sede */
-    var generationsArray= Object.keys(generations[0]); /* AQP-GEN-POS 0 , hacer for para 2 */
-  
-    /*encuentro lista de alumnos*/
-    var generationsValues = Object.values(generations[0]); 
-    var generationsValuesStudents= generationsValues[0];
-    var generationsValuesStudentsData = Object.values(generationsValuesStudents);
-    var gen= generationsValuesStudentsData[0];
-    
-    /* extrayendo alumnas de sede arequipa */
-   for( var a= 0; a<15 ; a++){
-       studentsData = Object.values(gen[a]); /*extrae alumnos*/
-       students[[a,0]] = sedesArray[0];   /*asigna sede arequipa */
-       students[[a,1]] = generationsArray[0]; /*asigna generation 1 */
-       students[[a,2]] = studentsData[0]; /*asigna nombre*/
-       students[[a,3]] = studentsData[1]; /*asigna foto*/
-       students[[a,4]] = studentsData[2]; /*asigna active*/
-       students[[a,5]] = studentsData[3]; /*asigna score objeto*/
-       ind = 0;
+    var generationsArray = Object.keys(generations[0]); /* AQP-GEN-POS 0 , hacer for para 2 */
 
-     /* extrayendo puntaje de sprints */     
-     for (var b= 0; b <4 ; b++) {
-        var tech =Object.values(studentsData[3]);
+    /* encuentro lista de alumnos */
+    var generationsValues = Object.values(generations[0]);
+    var generationsValuesStudents = generationsValues[0];
+    var generationsValuesStudentsData = Object.values(generationsValuesStudents);
+    var gen = generationsValuesStudentsData[0];
+
+    /* extrayendo alumnas de sede arequipa */
+    for (var a = 0; a < 15 ; a++) {
+      studentsData = Object.values(gen[a]); /* extrae alumnos */
+      students[[a, 0]] = sedesArray[0]; /* asigna sede arequipa */
+      students[[a, 1]] = generationsArray[0]; /* asigna generation 1 */
+      students[[a, 2]] = studentsData[0]; /* asigna nombre */
+      students[[a, 3]] = studentsData[1]; /* asigna foto */
+      students[[a, 4]] = studentsData[2]; /* asigna active */
+      students[[a, 5]] = studentsData[3]; /* asigna score objeto */
+      ind = 0;
+
+      /* extrayendo puntaje de sprints */
+      for (var b = 0; b < 4 ; b++) {
+        var tech = Object.values(studentsData[3]);
         var tec = Object.values(tech[b]);
-        var tePoint= Object.values(tec[1]);
-         
+        var tePoint = Object.values(tec[1]);
+
         /* recorre las notas por print 4 por alumna */
-        /*sprint 1*/
+        /* sprint 1 */
         ind = 0;
-        for (var c=0; c<2 ;c++) {
-          students[[a,6+b+ind]] =((tePoint[c]/1800)*100).toFixed(1) ; /*tech*/
-          students[[a,7+b]] =((tePoint[c]/1200)*100).toFixed(1) ; /*hse*/   
-          ind = ind+1;
+        for (var c = 0; c < 2 ;c++) {
+          students[[a, 6 + b + ind]] = ((tePoint[c] / 1800) * 100).toFixed(1) ; /* tech */
+          students[[a, 7 + b]] = ((tePoint[c] / 1200) * 100).toFixed(1) ; /* hse */
+          ind = ind + 1;
         }
       }
     }
-    
+
     var acumTech = 0;
     var acumHse = 0;
 
     /* para recorrer el array students */
-    for (var z= 0 ; z<15 ; z++){
+    for (var z = 0 ; z < 15 ; z++) {
       /* ubico área para crear elementos */
       var visualizationSection = document.getElementById('visualization');
-      
-       /* creaación de elementos divs */
 
-       /* photo del estudiante */
-       var divStudent = document.createElement('div'); /* div con id-student */
-       var imgPhoto = document.createElement('img');  /* img con propiedad src*/
-       imgPhoto.setAttribute('src',students[[z,3]]);
-       imgPhoto.classList.add('photo-standart');
-       visualizationSection.appendChild(divStudent);
-       divStudent.appendChild(imgPhoto);
+      /* creaación de elementos divs */
 
-       /*datos de estudiante */
-       var divDate = document.createElement('div'); /* div con id-date */
-       var divDateStudent = document.createElement('div'); /* div con id-name */ 
-       var divName = document.createElement('div'); /* div con id-date */
-       divName.classList.add('big-letter');
-       var divEspecializationStudent = document.createElement('div'); /* div con id-name */ 
-       
-       divStudent.appendChild(divDate);
-       divDate.appendChild(divDateStudent);
+      /* photo del estudiante */
+      var divStudent = document.createElement('div'); /* div con id-student */
+      var imgPhoto = document.createElement('img'); /* img con propiedad src */
+      imgPhoto.setAttribute('src', students[[z, 3]]);
+      imgPhoto.classList.add('photo-standart');
+      visualizationSection.appendChild(divStudent);
+      divStudent.appendChild(imgPhoto);
 
-       divDateStudent.appendChild(divName);
-       divName.textContent = students[[z,2]];
-       divDateStudent.appendChild(divEspecializationStudent);
-       divEspecializationStudent.textContent = 'Front End Developer';
-       
-       /* evaluación técnica */
-       var divTechSkills = document.createElement('div');
-       var divTech = document.createElement('div');
-       var labelPercentTech= document.createElement('label');
-       var labelNumberTech= document.createElement('label');
-       var brUp1 = document.createElement('br')
+      /* datos de estudiante */
+      var divDate = document.createElement('div'); /* div con id-date */
+      var divDateStudent = document.createElement('div'); /* div con id-name */
+      var divName = document.createElement('div'); /* div con id-date */
+      divName.classList.add('big-letter');
+      var divEspecializationStudent = document.createElement('div'); /* div con id-name */
 
-       divDate.appendChild(divTechSkills);
-       divTechSkills.appendChild(divTech);
-       divTech.appendChild(labelPercentTech);
-       labelPercentTech.textContent = students[[z,6]];
-       divTech.appendChild(brUp1);
-       divTech.appendChild(labelNumberTech);
-       labelNumberTech.textContent = students[[z,7]];
-     };
+      divStudent.appendChild(divDate);
+      divDate.appendChild(divDateStudent);
+
+      divDateStudent.appendChild(divName);
+      divName.textContent = students[[z, 2]];
+      divDateStudent.appendChild(divEspecializationStudent);
+      divEspecializationStudent.textContent = 'Front End Developer';
+
+      /* evaluación técnica */
+      var divTechSkills = document.createElement('div');
+      var divTech = document.createElement('div');
+      var labelPercentTech = document.createElement('label');
+      var labelNumberTech = document.createElement('label');
+      var brUp1 = document.createElement('br')
+
+      divDate.appendChild(divTechSkills);
+      divTechSkills.appendChild(divTech);
+      divTech.appendChild(labelPercentTech);
+      labelPercentTech.textContent = students[[z, 6]];
+      divTech.appendChild(brUp1);
+      divTech.appendChild(labelNumberTech);
+      labelNumberTech.textContent = students[[z, 7]];
+    };
   });
 });
