e4f27796855aefdc91c2783874ef97d82e720bb9
NatalyOC <noteroc@hotmail.com>
2017-12-03 18:21:58 -0500

    agregando funciones para nps y corrigiendo satisfaction

--- js/app.js
+++ js/app.js
@@ -41,48 +41,103 @@ window.addEventListener('load', function() {
   var btndash = document.getElementById('btndash');
   btndash.addEventListener('click', function() {
     /* Enveroment Static*/
-    console.log(sede);
-    console.log(generacion);
-    console.log('-------------');
-    var cantidad = data[sede][generacion].students.length;
-    console.log(cantidad);
-    var acum = 0;
-    var acumtrue = 0;
-    for (var i = 0; i < cantidad;i++) {
-    // console.log(data[sede][generacion].students[i].active);
-      if (data[sede][generacion].students[i].active == false) {
-        console.log(data[sede][generacion].students[i]);
-        acum++;
-}
-      else {
-        console.log(data[sede][generacion].students[i]);
-        acumtrue++;
-}
-    }
-    console.log(acum);
-    console.log(acumtrue);
-    var dropout = (acum / cantidad) * 100;
-    var dropoutPercentaje = (Math.round(dropout) + '%');
-    // inserta data de cantidad a los elementos
-    var spanCount = document.getElementById('count-overview');
-    var textCount = document.createTextNode(cantidad);
-    spanCount.appendChild(textCount);
-    // inserta data de desercion a los elementos
-    var spanPercentaje = document.getElementById('percentaje-overview');
-    var textPercentaje = document.createTextNode(dropoutPercentaje);
-    spanPercentaje.appendChild(textPercentaje);
-    ratingTeachers(sede,generacion)
+    enrollment(sede, generacion);
+    /* Promedio de puntuacion Teacher */
+    ratingTeachers(sede, generacion);
+    ratingJedi(sede, generacion);
+    studentSatisfation(sede, generacion);
+    achievement(sede, generacion);
+    nps(sede, generacion);
   });
-   
 });
-
-function ratingTeachers() {
-    var rating=0;
-    var countRating=data[sede][generacion].ratings.length-1;
-    for(var i=0;i<data[sede][generacion].ratings;i++){ 
-     rating+=data[sede][generacion].ratings[i].teacher;
-    
+/* Funciones */
+function enrollment(sede, generacion) {
+  var quantity = data[sede][generacion].students.length;
+  var acum = 0;
+  var acumtrue = 0;
+  for (var i = 0; i < quantity;i++) {
+    if (data[sede][generacion].students[i].active == false) {
+      acum++;
+    } else {
+      acumtrue++;
     }
-    var v=(rating/countRating)*100;
-    console.log(v);
+  }
+  var dropout = (acum / quantity) * 100;
+  var dropoutPercentaje = (Math.round(dropout) + '%');
+  // inserta data de cantidad a los elementos
+  var spanCount = document.getElementById('count-overview');
+  spanCount.textContent = quantity;
+  // inserta data de desercion a los elementos
+  var spanPercentaje = document.getElementById('percentaje-overview');
+  spanPercentaje.textContent = dropoutPercentaje;
+}
+// Funcion que obtiene el promedio de la puntuacion de los teachers
+function ratingTeachers(sede, generacion) {
+  var ratingTeacher = 0;
+  var countRatingT = data[sede][generacion].ratings.length;
+  for (var i = 0;i < data[sede][generacion].ratings.length;i++) { 
+    ratingTeacher += data[sede][generacion].ratings[i].teacher;
+  }
+  var averageTeacher = (ratingTeacher / countRatingT).toFixed(1);
+  // inserta data a los elementos
+  var spanRatingT = document.getElementById('t-rating-overview');
+  spanRatingT.textContent = averageTeacher;
+}
+// Funcion que obtiene el promedio de la puntuacion de los jedis
+function ratingJedi(sede, generacion) {
+  var ratingJedi = 0;
+  var countRatingJ = data[sede][generacion].ratings.length;
+  // Recorremos el array ratings 
+  for (var j = 0;j < data[sede][generacion].ratings.length;j++) { 
+    // Acumula los puntajes de los jedis
+    ratingJedi += data[sede][generacion].ratings[j].jedi;
+  }
+  // Declaro variable que almacene el promedio y con  función toFixed() poder limitar el número de decimales a través de su parámetr
+  var averageJedi = (ratingJedi / countRatingJ).toFixed(1);
+  // inserta data a los elementos
+  var spanRatingJ = document.getElementById('j-rating-overview');
+  spanRatingJ.textContent = averageJedi;
+}
+function studentSatisfation(sede, generacion) {
+  var count = data[sede][generacion].ratings.length;
+  var sum = 0;
+  for (var i = 0;i < data[sede][generacion].ratings.length;i++) { 
+    // Acumula los puntajes de los jedis
+    sum += data[sede][generacion].ratings[i].nps.promoters;
+  }
+
+  var satisfationPercentaje = sum / count;
+  // inserta data a los elementos
+  var spanNpsS = document.getElementById('s-nps-overview');
+  spanNpsS.textContent = satisfationPercentaje ;
+}
+function achievement(sede, generacion) {
+  var quantityStudents = data[sede][generacion].students.length;
+  var countAim = 0;
+  for (var i = 0; i < quantityStudents;i++) {
+    var quantitySprints = data[sede][generacion].students[i].sprints.length;
+    for (var j = 0; j < quantitySprints; j++) {
+      if ((data[sede][generacion].students[i].sprints[j].score.tech > 1260) && (data[sede][generacion].students[i].sprints[j].score.hse > 840)) {
+        if((data[sede][generacion].students[i].sprints[j].number)){
+        countAim++;
+        data[sede][generacion].students[i].sprints[j].number=data[sede][generacion].students[i].sprints[j].number+1;
+      console.log(data[sede][generacion].students[i].sprints[j].number)}
+      else{
+        data[sede][generacion].students[i].sprints[j].number=1;
+      }
+      }
+    } 
+  } 
+  console.log(countAim);
+}
+function nps(sede,generacion) {
+  var countRatings = data[sede][generacion].ratings.length;
+  var accumulatorNPS=0;
+  
+  for(var i=0; i<countRatings;i++){
+    accumulatorNPS+=data[sede][generacion].ratings[i].nps.promoters-data[sede][generacion].ratings[i].nps.detractors;
+  }
+  var averageNPS=accumulatorNPS/countRatings;
+  //inserta data a los elementos
+  
 }
