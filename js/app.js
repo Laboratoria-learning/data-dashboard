134566f796e4356dd4873735493f7599f590c978
Dani931 <yoyo931@live.com.mx>
2017-12-18 15:53:51 -0600

    Empezando Data

--- js/app.js
+++ js/app.js
@@ -2,5 +2,14 @@
  * Funcionalidad de tu producto
  */
 
+ function go(){
+if (document.getElementsByTagName('form').Password =='Password' && document.form.login =='E-mail'){
+        document.form.submit();
+    }
+    else{
+         alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
+    }
+}
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
