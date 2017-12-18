43f2f901bff9090810de2cdb1543ec1d4cd30359
CruzMayra <velazquez.leonora@gmail.com>
2017-12-18 12:21:17 -0600

    Creación de un menú desplegable

--- js/app.js
+++ js/app.js
@@ -5,19 +5,23 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 // console.log(data);
 var boton = document.getElementById("btn");
+
+boton.addEventListener("click", showMenu);
+
  function showMenu() {
    var menu = document.getElementById("opcs-menu");
-   if(menu.classList.contains("disabled-menu")){
 
+   if(menu.classList.contains("disabled-menu")){
      menu.classList.remove("disabled-menu");
      menu.classList.add("enabled-menu");
    }
    else{
      menu.classList.remove("enabled-menu");
      menu.classList.add("disabled-menu");
-
    }
 
  }
 
- boton.addEventListener("click", showMenu);
+
+// var selectSedes = document.getElementById('sedes');
+// selectSedes.addEventListener('change',selectGeneration())
