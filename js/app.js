113e6e8841e3b68f52292fdb8426de3396876e8e
CruzMayra <velazquez.leonora@gmail.com>
2017-12-18 13:57:42 -0600

    Creación de select dinámico

--- js/app.js
+++ js/app.js
@@ -19,9 +19,32 @@ boton.addEventListener("click", showMenu);
      menu.classList.remove("enabled-menu");
      menu.classList.add("disabled-menu");
    }
-
  }
 
+var sedeCdmx = ['2017-1','2017-2'];
+var sedeLima = ['2016-2','2017-1','2017-2'];
+var sedeAqp = ['2016-2','2017-1'];
+var sedeScl = ['2016-2','2017-1','2017-2'];
+var selectSedes = document.getElementById('sedes');
+var selectGeneration = document.getElementById('generation');
 
-// var selectSedes = document.getElementById('sedes');
-// selectSedes.addEventListener('change',selectGeneration())
+selectSedes.addEventListener('change', getGeneration)
+
+function getGeneration() {
+  if(selectSedes.value != 0){
+    var generation = eval('sede'+ selectSedes.value);
+    // console.log(eval('sede'+ selectSedes.value));
+    var numGeneration = generation.length;
+    selectGeneration.length = numGeneration;
+
+    for(var i = 0; i < numGeneration; i++){
+      selectGeneration.options[i].value = generation[i];
+      selectGeneration.options[i].text = generation[i];
+    }
+  } else {
+    selectGeneration.length = -1;
+    selectGeneration.options[0].value ='---';
+    selectGeneration.options[0].text = '---';
+  }
+    selectGeneration.options[0].selected = true;
+}
