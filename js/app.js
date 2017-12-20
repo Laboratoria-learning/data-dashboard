2148c08bd467146d8daea98ad15e14add0bca5b9
G4br13l4 <gabolf93@gmail.com>
2017-12-20 10:31:23 -0600

    grafica de barras avance

--- js/app.js
+++ js/app.js
@@ -199,7 +199,7 @@ var nps= (promotersPercent - detractorsPercent).toFixed(1) + '%';
     var satifactionResponses= (noCumple+cumple+supera);
     var satifactionPercent= (((cumple+supera)/satifactionResponses)*100).toFixed(1) +"%";
 
-// ---------------->#Puntos Promedio por Sprint <----------------
+// ---------------->#Puntos Promedio Totales <----------------
   var techPoints = 0; 
   var hsePoints= 0;
 
@@ -210,8 +210,55 @@ var nps= (promotersPercent - detractorsPercent).toFixed(1) + '%';
           }      
   }
   var totalPointsAvg = ((techPoints+hsePoints)/totalStudents).toFixed(0);
-  var techPointsAvg = (techPoints / totalStudents).toFixed(0);;
+  var techPointsAvg = (techPoints / totalStudents).toFixed(0);
   var hsePointsAvg = (hsePoints / totalStudents).toFixed(0);;
+
+// ---------------->#Puntos Promedio por Sprint <----------------
+
+//Totales de Sprints
+var totalHseSprint1 = 0;
+var totalHseSprint2 = 0;
+var totalHseSprint3 = 0;
+var totalHseSprint4 = 0;
+
+var totalTechSprint1 = 0;
+var totalTechSprint2 = 0;
+var totalTechSprint3 = 0;
+var totalTechSprint4 = 0;
+
+
+  for (var i = 0; i < totalStudents; i++) {
+          for (var j = 0; j < arrayStudents[i]['sprints'].length; j++) { //iterar sobre los sprints de cada estudiante
+            var sprint=arrayStudents[i]['sprints'][j]['number']; //identificar el el sprint por number
+            
+            if(sprint===1) { 
+              totalTechSprint1 += arrayStudents[i]['sprints'][j]['score']['tech'];
+              totalHseSprint1  += arrayStudents[i]['sprints'][j]['score']['hse'];
+            } else if (sprint===2) {
+              totalTechSprint2 += arrayStudents[i]['sprints'][j]['score']['tech'];
+              totalHseSprint2  += arrayStudents[i]['sprints'][j]['score']['hse'];
+            } else if (sprint===3) {
+              totalTechSprint3 += arrayStudents[i]['sprints'][j]['score']['tech'];
+              totalHseSprint3  += arrayStudents[i]['sprints'][j]['score']['hse'];
+            } else if (sprint===4) {
+              totalTechSprint4 += arrayStudents[i]['sprints'][j]['score']['tech'];
+              totalHseSprint4  += arrayStudents[i]['sprints'][j]['score']['hse'];
+            }
+          }      
+  }
+// AVERAGES PER SPRINT
+
+//HSE Sprints
+var avgHseSprint1 = (totalHseSprint1/totalStudents).toFixed(0);
+var avgHseSprint2 = (totalHseSprint2/totalStudents).toFixed(0);
+var avgHseSprint3 = (totalHseSprint3/totalStudents).toFixed(0);
+var avgHseSprint4 = (totalHseSprint4/totalStudents).toFixed(0);
+//TECH Sprints 
+var avgTechSprint1 = (totalTechSprint1/totalStudents).toFixed(0);
+var avgTechSprint2 = (totalTechSprint2/totalStudents).toFixed(0);
+var avgTechSprint3 = (totalTechSprint3/totalStudents).toFixed(0);
+var avgTechSprint4 = (totalTechSprint4/totalStudents).toFixed(0);
+
 
   // --------> Meter datos a sus cajas de Kpis<------------
 
@@ -306,18 +353,9 @@ google.charts.load('current', {'packages':['corechart']});
         chart.draw(data, options);
       }
 
-}
 //grafica de barras 
 
-
-/*html 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <button id="change-chart">Change to Classic</button>
-        <br><br>
-        <div id="chart_div" style="width: 800px; height: 500px;"></div>
-  js
-
-google.charts.load('current', {'packages':['corechart', 'bar']});
+      google.charts.load('current', {'packages':['corechart', 'bar']});
       google.charts.setOnLoadCallback(drawStuff);
 
       function drawStuff() {
@@ -326,18 +364,33 @@ google.charts.load('current', {'packages':['corechart', 'bar']});
         var chartDiv = document.getElementById('chart_div');
 
         var data = google.visualization.arrayToDataTable([
-          ['Sprint', 'HSE', 'TECH',"TOTAL"],
-          ['Sprint 1', 8000, 23.3, 50],
-          ['Sprint 2', 24000, 4.5,60],
-          ['Sprint 3', 30000, 14.3,70],
-  
+          ['SPRINTS', 'HSE', 'TECH'],
+          ['Sprint 1', avgHseSprint1 , avgTechSprint1],
+          ['Sprint 2', avgHseSprint2, avgTechSprint2],
+          ['Sprint 3', avgHseSprint3, avgTechSprint3],
+          ['Sprint 4', avgHseSprint4, avgTechSprint4 ]
+    
         ]);
 
+        /*var options = {
+          title: 'Total de Alumnas',
+          'is3D':true,
+          'width':500,
+        'height':400,
+        'pieSliceText':'percentage',
+        chartArea:{left:50,top:30,width:'60%',height:'75%'},
+        colors:['red','#004411','yellow'],
+        legend:{textStyle: {color: 'black', fontSize: 12}}
+
+        };*/
+
+
         var materialOptions = {
-          width: 900,
+          width: 500,
+
           chart: {
-            title: 'Nearby galaxies',
-            subtitle: 'distance on the left, brightness on the right'
+            title: 'Puntos Promedio por Sprint',
+            subtitle: 'HSE & Técnico'
           },
           series: {
             0: { axis: 'distance' }, // Bind series 0 to an axis named 'distance'.
@@ -379,7 +432,10 @@ google.charts.load('current', {'packages':['corechart', 'bar']});
           button.onclick = drawMaterialChart;
         }
 
-*/
+        drawMaterialChart();
+    };
+
+}
 
 
 
