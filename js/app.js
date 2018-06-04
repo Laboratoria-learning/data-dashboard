09903af6520f6943d790e14fe14d08e3be9cd2ff
navy1985 <jezreelsanz@gmail.com>
2018-06-04 13:48:34 -0500

    se agrega funcinalidad para cumplir requerimientos

--- js/app.js
+++ js/app.js
@@ -15,8 +15,18 @@ var containerForm = document.getElementById('container-form');
 var input = document.getElementById('formulario-input');
 var passw = document.getElementById('formulario-password');
 var containerActive = document.getElementById('container-actives');
+var containerJedis = document.getElementById('promedio-jedis');
+var containerTeachers = document.getElementById('promedio-teachers');
+var containerActivas = document.getElementById('activas');
+var containerInactivas = document.getElementById('inactivas');
+var containerTotalStudents = document.getElementById('total-students');
+var nameTotalInactivas = document.getElementById('name-inactivas');
+var cumplenMeta = document.getElementById('cumplen-meta');
+var superanMeta = document.getElementById('superan-meta');
+var satisfechas = document.getElementById('satisfechas');
 
-//----SE LE DA FUNFIONALIDA AL INGRESAR----Y VALIDACION AL FORMULARIO------//
+
+//----SE LE DA FUNCIONALIDAD AL INGRESAR----Y VALIDACION AL FORMULARIO------//
 
 button.addEventListener("click", function(){
 
@@ -49,34 +59,147 @@ function optionSelect(e) {
   var generation = select.options[select.selectedIndex].dataset.generation;
          //console.log(generation);
   var students = data[sede][generation]["students"];
-    //    console.log(students);
+    //  console.log(students);
 
+
+
+//----FOR QUE ITERA EN LAS ESTUDIANTES Y CUENTA LAS ACTIVAS Y NOS DA EL NOMBRE DE LAS INACTIVAS, SE UTILIZA CONTADOR---//
   contActivas = 0;
   contInactivas = 0;
-
+  nameInactivas = [];
  for (var i = 0; i < students.length; i++) {
-     if(students[i]["active"] == true){
-        contActivas ++;
-       console.log(contActivas);
-      //  console.log( students[i]["sprints"]);
-      }else{
-        contInactivas ++;
-      var inactivas= students[i];
-      console.log(inactivas["name"]);
-    }
+
+    if(students[i]["active"] == true){
+      contActivas ++;
+       var activas = contActivas;
+      // console.log(activas);
+
+
+//----- FOR PARA QUE NOS DE EL PUNTAJE DE CADA UNA DE LAS ESTUDIANTES TANTO EN TECH COMO EL HSE---//
+  scoreHse=0;
+  scoreTech = 0;
+
+
+
+for (var j = 0; j < students[i]["sprints"].length; j++) {
+  scoreTech += students[i]["sprints"][j]["score"]["tech"];
+  scoreHse += students[i]["sprints"][j]["score"]["hse"];
+   var hse= "puntaje HSE " + scoreHse;
+   console.log(hse);
+   var tech= "puntaje TECH " + scoreTech;
+   console.log(tech);
+
+  var totalScore = scoreHse +  scoreTech;
+ console.log(totalScore);
+
+
+
+   }
+
+
+ }else{
+    contInactivas ++;
+    var inactivas = contInactivas;
+
+    nameInactivas.push(students[i]["name"]);
+    //console.log(nameInactivas);
+  }
+
 }
 
   var totalStudents = contInactivas + contActivas;
-  containerStudents.innerHTML = "el total de alumnas de " + sede  +  generation + " es de: " + totalStudents;
 
   var porcentajeActivas = contActivas *100/ totalStudents;
-  console.log(porcentajeActivas.toFixed() + "%" );
+  var totalActivas = porcentajeActivas.toFixed() + "%" ;
 
   var porcentajeInactivas = contInactivas * 100/ totalStudents;
-  console.log(porcentajeInactivas.toFixed() + "%");
-//   var containerTotalStudents = document.createElement("div");
-//      containerTotalStudents.setAttribute("id", "container-Total")
-//      containerStudents.appendChild(containerTotalStudents);
-//      containerStudents.innerHTML = " El total de estudiantes activas es de: " + contActivas *100 / totalStudents;
+  var totalInactivas = porcentajeInactivas.toFixed() + "%";
 
+  containerTotalStudents.textContent = sede + " " + generation + " " + " Total de estudiantes : " + totalStudents;
+
+
+
+ containerActivas.textContent = "El porcentaje de estudiantes activas es del : " + totalActivas + " " +  " total : " + activas;
+ containerInactivas.textContent = "El porcentaje de estudiantes inactivas es del : " + totalInactivas + " " + " total : " + inactivas;
+ nameTotalInactivas.textContent = "Estudiantes que desertaron : " + nameInactivas;
+
+
+//-----PROMEDIO JEDIS Y TEACHERS---->
+
+var ratings = data[sede][generation]["ratings"];
+
+promJedi = 0;
+promTeacher = 0;
+jedi=[];
+teacher = [];
+
+for (var i = 0; i < ratings.length; i++) {
+   promJedi += (ratings[i]["jedi"])/ratings.length;
+  var totalJedi = promJedi.toFixed(1);
+
+  jedi.push(totalJedi);
+  //console.log(jedi);
+
+  promTeacher += (ratings[i]["teacher"])/ratings.length;
+  var totalTeachers = promTeacher.toFixed(1);
+
+  teacher.push(totalTeachers);
+  //console.log(teacher);
+
+
+containerJedis.textContent =  "El promedio de los Jedis es de : " + totalJedi;
+containerTeachers.textContent = "El promedio de los teachers es de : " + totalTeachers;
+
+
+}
+
+//-------ESTUDIANTES QUE CUMPLEN Y SUPERAN LA META---->
+
+cumpleTotal= 0;
+superaTotal =0;
+
+
+  for (var i = 0; i < ratings.length; i++){
+    var student = ratings[i]["student"];
+        //  console.log(student);
+    var totalCumple = cumpleTotal/ratings.length;
+     cumpleTotal += student["cumple"];
+    var totalMeta= superaTotal/ratings.length;
+     superaTotal += student["supera"];
+
+    var satisfechasTotal = totalCumple + totalMeta ;
+
+cumplenMeta.textContent = "El porcentaje de estudiantes que cumplen la meta es del: " + totalCumple.toFixed() + "%";
+superanMeta.textContent = "El porcentaje de estudiantes que superan la meta es del: " + totalMeta.toFixed() + "%";
+satisfechas.textContent = "El porcentaje de estudiantes satisfechas es del: " + satisfechasTotal.toFixed() + "%";
+
+}
+
+/*google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Create the data table.
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+      data.addColumn('string', 'promediotyj')//nombre d la tabla
+      data.addColumn('number', 'promedio'),// el promedio d teachers y jedis
+      data.addRows([
+          ['jedis', promJedi],
+          ['teacher', promTeacher]
+      ]);
+
+
+
+        var options = {
+          chart: {
+            title: 'Promedio t  y  jedis',
+            subtitle: 'Teachers, Jedis, y Sede: 2014-2017',
+          },
+          bars: 'horizontal' // Required for Material Bar Charts.
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('chart_div'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }*/
 };
