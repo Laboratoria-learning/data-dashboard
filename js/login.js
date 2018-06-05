fb741a431c9171e97648b88b154fa3cf84e33bd7
Orquídea Campos <37587788+Orkhydea@users.noreply.github.com>
2018-06-05 10:43:35 -0500

    propuestas orquidea

--- js/login.js
+++ js/login.js
@@ -1,17 +1,21 @@
+//alert("hola"); //
+// *se usa un alert para comprobar enlace con js*//
 
-// //validating the username and passwword
-// function validation() {
-//     var uname = document.login.username.value;
-//     var pwd = document.login.password.value;
+//validating the username and passwword//
 
-//     //check empty field
-//     if (uname == "" || pwd == "") {
-//         alert("Both fields are in blank, please add your username and password");
-//         return;
-//     }
-//     //check the password length
-//     if (pwd.lenght < 6 || pwd.lenght > 12) {
-//         alert("Password must be at least 6 characters");
-//         return;
-//     }
-// }
+ function validation() {
+     var uname = document.login.username.value;
+     var pwd = document.login.password.value;
+
+     //check empty field
+     if (uname == "" || pwd == "") {
+         alert("Both fields are in blank, please add your username and password");
+         return;
+     }
+     //check the password length
+     if (pwd.lenght < 6 || pwd.lenght > 12) {
+         alert("Password must be at least 6 characters");
+         return;
+     }
+     
+ }
