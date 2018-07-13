f56f3c940c902809841134d85096f41fe0388ce4
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-07-13 01:41:37 -0300

    primeiro grafico

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
-/*
- * Funcionalidad de tu producto
- */
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+var ativa = 0;
+var inativa = 0;
+for(var i = 0; i < 15; i++){
+  if(data.AQP["2016-2"].students[i].active == true){
+    ativa += 1;
+  }else{
+    inativa += 1;
+  }
+}
+
+function drawChart() {
+var data =  google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['ativas',   ativa],
+    ['invativas',  inativa]
+  ]);
+  var options = {
+    title: 'Turma 2016-2'
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
