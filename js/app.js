5ac7b129081b0b56903ce3f0e085b56e98da5a34
Pamela Rozolem <pamelarozolem12@live.com>
2018-07-16 14:44:37 -0300

    card 1 completo, iniciando card 3

--- js/app.js
+++ js/app.js
@@ -44,9 +44,9 @@ function graficoAlunasInativas(){
 }
 
 
-// Função que retorna total de alunas ativas por sede e geração
+// AAA função 
 function alunasAtivasSedeGeracao(){
-    const grafico1 = [
+    const graficos = [
     ];
     for (sede in data){
         for (geracao in data[sede]){
@@ -54,20 +54,22 @@ function alunasAtivasSedeGeracao(){
             item['sede'] = sede;
             item['geracao'] = geracao;
             item['quantidade'] = data[sede][geracao].students.filter(alunas=>alunas.active).length;
-            item['quantidadeInativas']= data[sede][geracao].students.filter(alunas=> !alunas.active).length;
-            item['mediaTechSprint']= data[sede][geracao].students.filter(alunas => {
+            item['quantidadeInativas']= data[sede][geracao].students.filter(alunas=> !alunas.tech).length;
+            item['totalScore']= data[sede][geracao].students.filter(alunas => alunas.score.tech).length;
+            item['excedeTech']= data[sede][geracao].students.filter(alunas => {
                 if (alunas.sprints)
                     return alunas.sprints.filter(media => media.score.tech > 1260);
             }).length;
+            console.log(excedeTech);
             item['cor'] = '#40' + (Math.random().toString(16) + '0000000').slice(2, 8); 
-            grafico1.push(item);
+            graficos.push(item);
         }
     }
-    return grafico1;
+    return graficos;
 }
 
 // Função que soma o total de todas as sedes e turmas
-document.querySelector(".totalAtivas").innerHTML = totalGeralAtivas();
+document.querySelector(".totalAtivasTodasSedes").innerHTML = totalGeralAtivas();
 function totalGeralAtivas(){
     let alunas = alunasAtivasSedeGeracao();
     return alunas.reduce(function(prev,element){
@@ -76,4 +78,49 @@ function totalGeralAtivas(){
 }
 
 
+////////////////////////////////////////////////////////////////////////////
+//Função do grafico meta de pontos técnicos em média e sprint.
+/*graficoMediaTech();
+function graficoMediaTech(){
+    let dados = alunasAtivasSedeGeracao();
+    let ctx = document.getElementById("tech").getContext("2d");
+    let labels = dados.map(item => item.sede + ' (' + item.geracao + ')' ); 
+    let mediaTechSprint = dados.map(item => item.mediaTechSprint);
+    let colors = dados.map(item => item.cor);
+    let myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [
+                {data: mediaTechSprint, label: 'Alunas na média por sede e gerção',  borderWidth: 1, backgroundColor: colors}
+            ]
+        }
+    });
+}
+*/
 
+//Função do grafico de alunas excedem a meta tech
+graficoExcedemTech();
+function graficoExcedemTech(){
+    let dados = alunasAtivasSedeGeracao();
+    let ctx = document.getElementById("tech").getContext("2d");
+    
+    let inativas = dados.map(item => item.quantidadeInativas).reduce ( (prev, item)=> prev + item, 0);
+    let ativas = dados.map(item => item.quantidade).reduce( (prev, item) => prev + item, 0 );
+    let totalGeral =  inativas + ativas;
+    let percentualAtivas = Math.round((ativas * 100) / totalGeral);
+    let percentualInativas = Math.round((inativas  * 100) / totalGeral);
+    document.querySelector(".totalDesistentes").innerHTML = percentualInativas;
+    let dataset = [percentualAtivas, percentualInativas];
+    let labels = ["Ativas", "Inativas"];
+
+     let myBarChart = new Chart(ctx, {
+         type: 'doughnut',
+         data: {
+             labels: labels,
+             datasets: [
+                 {data: dataset, label: labels,  borderWidth: 1, backgroundColor: ['#5032CD32', '#FF0000']}
+             ]
+         }
+     });
+}
